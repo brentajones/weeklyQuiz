811e49c80fe2c33ea5f7a8e14725cbfbfa7691da
brentajones <bjones@bjones.net>
2014-03-13 14:14:59 -0500

    update weekly questions

---
+++ js/20140314.js
@@ -0,0 +1,123 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for the week of Mar 10, 2014",
+        "main":    "<p>State government, education and even that pesky time change find their way into this week’s quiz. Were you paying attention? Find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "As part of his agreement to resign as superintendent of the Ferguson-Florissant schools, Art McCoy will receive what?",
+            "a": [
+                {"option": "Salary through the end of his contract in 2016",      "correct": false},
+                {"option": "An undisclosed lump sum settlement",     "correct": false},
+                {"option": "Payment for unused vacation",      "correct": true},
+                {"option": "All of the above",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/mccoy-resigns-ferguson-florissant-superintendent&quot;>The separation agreement released by the district said McCoy would get paid for 44 unused vacation days. As detailed by reporters Tim Lloyd and Dale Singer, charges against McCoy brought by the school board have not been disclosed.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/mccoy-resigns-ferguson-florissant-superintendent&quot;>The separation agreement released by the district said McCoy would get paid for 44 unused vacation days. As detailed by reporters Tim Lloyd and Dale Singer, charges against McCoy brought by the school board have not been disclosed.</a></p>" // no comma here
+        },
+        { 
+            "q": "An appellate court ruling has resurrected an initiative petition campaign in Missouri on what topic?",
+            "a": [
+                {"option": "Campaign contributions",      "correct": true},
+                {"option": "Teacher tenure",     "correct": false},
+                {"option": "Medicaid",      "correct": false},
+                {"option": "Medical marijuana",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/appeals-court-resurrects-initiative-petition-drive-campaign-donation-limits&quot;>The Western District Court of Appeals of Missouri has resurrected an initiative-petition effort to restore campaign donation limits in Missouri, but reporter Jo Mannies explained that it’s unclear if the proposal’s backers will have enough time to collect the signatures needed to get their proposal before voters.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/appeals-court-resurrects-initiative-petition-drive-campaign-donation-limits&quot;>The Western District Court of Appeals of Missouri has resurrected an initiative-petition effort to restore campaign donation limits in Missouri, but reporter Jo Mannies explained that it’s unclear if the proposal’s backers will have enough time to collect the signatures needed to get their proposal before voters.</a></p>" // no comma here
+        },
+        { 
+            "q": "A report from Missouri auditor Tom Schweich criticized what state program as inefficient?",
+            "a": [
+                {"option": "Fee offices",      "correct": false},
+                {"option": "Low-income housing tax credit",     "correct": true},
+                {"option": "Transportation development districts",      "correct": false},
+                {"option": "The lottery",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/schweichs-audit-questions-efficiency-impact-low-income-housing-tax-credit&quot;>Jason Rosenbaum reported that Schweich’s audit found that Missouri’s widely used low-income housing tax credit is inefficient and has a “very low” return on investment.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/schweichs-audit-questions-efficiency-impact-low-income-housing-tax-credit&quot;>Jason Rosenbaum reported that Schweich’s audit found that Missouri’s widely used low-income housing tax credit is inefficient and has a “very low” return on investment.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri House approved a state budget for fiscal 2015 of how much?",
+            "a": [
+                {"option": "$24 billion",      "correct": false},
+                {"option": "$26 billion",     "correct": false},
+                {"option": "$28 billion",      "correct": true},
+                {"option": "$30 billion",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/missouri-house-committee-passes-state-budget-fy2015&quot;>Capital correspondent Marshall Griffin reported that the spending plan includes an increase for K-12 education that could total between $122 million and $278 million, depending on the amount of revenue taken in by the state.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/missouri-house-committee-passes-state-budget-fy2015&quot;>Capital correspondent Marshall Griffin reported that the spending plan includes an increase for K-12 education that could total between $122 million and $278 million, depending on the amount of revenue taken in by the state.</a></p>" // no comma here
+        },
+        { 
+            "q": "Who chairs a newly named task force on the future of the Normandy school district?",
+            "a": [
+                {"option": "Commissioner of education",      "correct": false},
+                {"option": "Mayor of Normandy",     "correct": false},
+                {"option": "President of state board of education",      "correct": false},
+                {"option": "Dean of UMSL’s education school",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/panel-normandys-future-begins-meeting-private&quot;>Dean Carole Basile heads the 10-member panel, which is looking at various scenarios for Normandy schools. Reporter Dale Singer explains that the district’s finances have been drained by costs of the transfer program, and even if it survives the current school year, its future is in question. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/panel-normandys-future-begins-meeting-private&quot;>Dean Carole Basile heads the 10-member panel, which is looking at various scenarios for Normandy schools. Reporter Dale Singer explains that the district’s finances have been drained by costs of the transfer program, and even if it survives the current school year, its future is in question. </a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri Court of Appeals heard arguments in a case involving pensions for what group of St. Louis workers?",
+            "a": [
+                {"option": "Police officers",      "correct": false},
+                {"option": "Firefighters",     "correct": true},
+                {"option": "Teachers",      "correct": false},
+                {"option": "All of the above",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/legal-battle-resumes-over-changes-pensions-st-louis-city-firefighters&quot;>At issue, according to reporter Rachel Lippmann, is whether the city of St. Louis had the right to make changes to the pension benefits it offers its firefighters. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/legal-battle-resumes-over-changes-pensions-st-louis-city-firefighters&quot;>At issue, according to reporter Rachel Lippmann, is whether the city of St. Louis had the right to make changes to the pension benefits it offers its firefighters. </a></p>" // no comma here
+        },
+        { 
+            "q": "What symbols are popping up all over the area to make St. Louis’ 250th birthday?",
+            "a": [
+                {"option": "Gateway Arch replicas",      "correct": false},
+                {"option": "Birthday cakes",     "correct": true},
+                {"option": "Statues of Louis IX",      "correct": false},
+                {"option": "Cardinal baseball caps",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/stl250-cakes-are-popping-all-over&quot;>As reported by Donna Korando, the birthday cake theme is allowing artists and institutions to put their own unique stamp on the areawide celebration. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/stl250-cakes-are-popping-all-over&quot;>As reported by Donna Korando, the birthday cake theme is allowing artists and institutions to put their own unique stamp on the areawide celebration. </a></p>" // no comma here
+        },
+        { 
+            "q": "Which one of these people is NOT represented by a wax figure in the Griot Museum of Black History?",
+            "a": [
+                {"option": "Barack Obama",      "correct": true},
+                {"option": "Percy Green",     "correct": false},
+                {"option": "Carter Woodson",      "correct": false},
+                {"option": "Josephine Baker",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/storytelling-griot-has-personal-touch&quot;>The others are part of the display at the north St. Louis museum, which reporter Mary Delach Leonard said celebrates the accomplishments of African Americans and their connections to the region.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/storytelling-griot-has-personal-touch&quot;>The others are part of the display at the north St. Louis museum, which reporter Mary Delach Leonard said celebrates the accomplishments of African Americans and their connections to the region.</a></p>" // no comma here
+        },
+        { 
+            "q": "A researcher at what local university found that daylight saving time is more trouble than it’s worth?",
+            "a": [
+                {"option": "Saint Louis University",      "correct": false},
+                {"option": "Southern Illinois University-Edwardsville",     "correct": false},
+                {"option": "Washington University",      "correct": true},
+                {"option": "University of Missouri-St. Louis",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/daylight-saving-time-more-trouble-its-worth&quot;>Erik Herzog of Washington University told reporter Kelly MacNeil that the time change results in increased traffic accidents and heart attacks. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/daylight-saving-time-more-trouble-its-worth&quot;>Erik Herzog of Washington University told reporter Kelly MacNeil that the time change results in increased traffic accidents and heart attacks. </a></p>" // no comma here
+        },
+        { 
+            "q": "Missouri Democrats held their annual regional get-together where last weekend?",
+            "a": [
+                {"option": "St. Louis",      "correct": false},
+                {"option": "Poplar Bluff",     "correct": false},
+                {"option": "Columbia",      "correct": false},
+                {"option": "Hannibal",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/koster-calls-democrats-fight-medicaid-expansion-blasts-gops-angry-stubbornness&quot;>At the annual Democrat Days event in Hannibal, Attorney General Chris Koster –now running for governor -- attacked his former Republican colleagues for opposing Medicaid expansion and the Affordable Care Act, according to political reporter Jo Mannies. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target=&quot;_blank&quot; href=&quot;http://news.stlpublicradio.org/post/koster-calls-democrats-fight-medicaid-expansion-blasts-gops-angry-stubbornness&quot;>At the annual Democrat Days event in Hannibal, Attorney General Chris Koster &mdash; now running for governor &mdash; attacked his former Republican colleagues for opposing Medicaid expansion and the Affordable Care Act, according to political reporter Jo Mannies. </a></p>" // no comma here
+        }
+	
+]
+};

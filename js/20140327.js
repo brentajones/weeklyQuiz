235d9ec4b2a08d8689e278ea61c2da0e79078939
brentajones <bjones@bjones.net>
2014-03-27 10:50:50 -0500

    updated for March 27, 2014

---
+++ js/20140327.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for the week of Mar 24, 2014",
+        "main":    "<p>It’s a grab bag! We covered news all over the place this week. Were you paying attention?</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "The state of Missouri executed Jeffrey Ferguson on Wednesday for a murder he committed in what year?",
+            "a": [
+                {"option": "1989",      "correct": true},
+                {"option": "1992",     "correct": false},
+                {"option": "1995",      "correct": false},
+                {"option": "1999",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-executes-inmate-third-year'>Ferguson was convicted in the rape and killing of a St. Charles County teenager in 1989. Chris McDaniel reported that the execution was the fifth by Missouri in as many months, more than it had carried out in the preceding eight years.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-executes-inmate-third-year'>Ferguson was convicted in the rape and killing of a St. Charles County teenager in 1989. Chris McDaniel reported that the execution was the fifth by Missouri in as many months, more than it had carried out in the preceding eight years.</a></p>" // no comma here
+        },
+        { 
+            "q": "State Rep. Rick Stream, who entered the Republican primary for St. Louis County executive, is from what county municipality?",
+            "a": [
+                {"option": "Webster Groves",      "correct": false},
+                {"option": "Kirkwood",     "correct": true},
+                {"option": "Sunset Hills",      "correct": false},
+                {"option": "Crestwood",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stream-files-st-louis-county-executive'>Stream, who chairs the House budget committee, says he won’t spend much time on the county executive race until the legislative session ends in Jefferson City in May. Reporter Jo Mannies says the GOP hopes that Stream’s candidacy will help break a losing streak; Republicans haven’t held a countywide office since 1991.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stream-files-st-louis-county-executive'>Stream, who chairs the House budget committee, says he won’t spend much time on the county executive race until the legislative session ends in Jefferson City in May. Reporter Jo Mannies says the GOP hopes that Stream’s candidacy will help break a losing streak; Republicans haven’t held a countywide office since 1991.</a></p>" // no comma here
+        },
+        { 
+            "q": "Research shows that chronic absenteeism in schools often begins getting worse at what grade level?",
+            "a": [
+                {"option": "Fourth",      "correct": false},
+                {"option": "Fifth",     "correct": false},
+                {"option": "Sixth",      "correct": true},
+                {"option": "Seventh",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/accounted-where-are-you-starting-where-are-you-going-how-are-you-going-get-there'>As reporter Tim Lloyd explained in his series, “Accounted For,” chronic absenteeism typically follows the pattern of an inverted u-shape.  Attendance begins to improve through fifth grade, then begins to get worse starting with sixth grade. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/accounted-where-are-you-starting-where-are-you-going-how-are-you-going-get-there'>As reporter Tim Lloyd explained in his series, “Accounted For,” chronic absenteeism typically follows the pattern of an inverted u-shape.  Attendance begins to improve through fifth grade, then begins to get worse starting with sixth grade. </a></p>" // no comma here
+        },
+        { 
+            "q": "Startup businesses in the St. Louis area raised how much in capital last year?",
+            "a": [
+                {"option": "$320 million",      "correct": false},
+                {"option": "$360 million",     "correct": false},
+                {"option": "$380 million",      "correct": true},
+                {"option": "$410 million",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/2013-banner-year-stl-startups'>A new study from the St. Louis Regional Chamber finds area startups raised more than $380 million in equity investments in 2013, more than six times the amount raised in 2012. Reporter Maria Altman found that a changing attitude toward Midwestern startups from coastal venture capital firms is helping the growth.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/2013-banner-year-stl-startups'>A new study from the St. Louis Regional Chamber finds area startups raised more than $380 million in equity investments in 2013, more than six times the amount raised in 2012. Reporter Maria Altman found that a changing attitude toward Midwestern startups from coastal venture capital firms is helping the growth.</a></p>" // no comma here
+        },
+        { 
+            "q": "Missouri auditor Tom Schweich issued a report calling what program too expensive and inefficient?",
+            "a": [
+                {"option": "Medicaid",      "correct": false},
+                {"option": "Food stamps",     "correct": false},
+                {"option": "Higher education",      "correct": false},
+                {"option": "Historic preservation tax credit",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-casts-critical-eye-toward-historic-preservation-tax-credit'>Schweich’s audit, Jason Rosenbaum reported, said that the state’s historic preservation tax credit program is too expensive and structurally inefficient. At $1.1 billion over the past decade, it is the largest in the nation among states that have an annual limit, Schweich said.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-casts-critical-eye-toward-historic-preservation-tax-credit'>Schweich’s audit, Jason Rosenbaum reported, said that the state’s historic preservation tax credit program is too expensive and structurally inefficient. At $1.1 billion over the past decade, it is the largest in the nation among states that have an annual limit, Schweich said.</a></p>" // no comma here
+        },
+        { 
+            "q": "Fred Bronstein is leaving his job as head of the St. Louis Symphony to lead the Peabody Institute at what university?",
+            "a": [
+                {"option": "Harvard",      "correct": false},
+                {"option": "Stanford",     "correct": false},
+                {"option": "Princeton",      "correct": false},
+                {"option": "Johns Hopkins",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/symphony-chief-executive-going-peabody-institute-johns-hopkins'>During Bronstein's time in St. Louis, he helped revitalize the orchestra, bringing in more revenue and a larger audience, Donna Korando reported. The Peabody Institute at Johns Hopkins is the oldest and among the top conservatories in the United States.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/symphony-chief-executive-going-peabody-institute-johns-hopkins'>During Bronstein's time in St. Louis, he helped revitalize the orchestra, bringing in more revenue and a larger audience, Donna Korando reported. The Peabody Institute at Johns Hopkins is the oldest and among the top conservatories in the United States.</a></p>" // no comma here
+        },
+        { 
+            "q": "Eric Knost resigned from his position as superintendent of the Mehlville School District to become superintendent where?",
+            "a": [
+                {"option": "Affton",      "correct": false},
+                {"option": "Webster Groves",     "correct": false},
+                {"option": "Ferguson-Florissant",      "correct": false},
+                {"option": "Rockwood",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/knost-leaving-mehlville-become-rockwood-superintendent'>Knost’s tenure at Mehlville, which ends June 30, has been marked by the district’s acceptance of transfer students from Riverview Gardens. Dale Singer reported that at Rockwood, he will take over a district that has seen controversy with its superintendent and its school board.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/knost-leaving-mehlville-become-rockwood-superintendent'>Knost’s tenure at Mehlville, which ends June 30, has been marked by the district’s acceptance of transfer students from Riverview Gardens. Dale Singer reported that at Rockwood, he will take over a district that has seen controversy with its superintendent and its school board.</a></p>" // no comma here
+        },
+        { 
+            "q": "State Sen. Brian Nieves has filed to seek what office in Franklin County this fall?",
+            "a": [
+                {"option": "County commissioner",      "correct": false},
+                {"option": "Recorder of deeds",     "correct": true},
+                {"option": "Sheriff",      "correct": false},
+                {"option": "Treasurer",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nieves-files-franklin-county-recorder-deeds'>Nieves, R-Washington, decided to leave the General Assembly and enter the GOP primary for recorder of deeds in Franklin County. Jo Mannies reports he has represented the 26th senatorial district since 2011 and was eligible to seek another term but says he wanted to stay closer to home.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nieves-files-franklin-county-recorder-deeds'>Nieves, R-Washington, decided to leave the General Assembly and enter the GOP primary for recorder of deeds in Franklin County. Jo Mannies reports he has represented the 26th senatorial district since 2011 and was eligible to seek another term but says he wanted to stay closer to home.</a></p>" // no comma here
+        },
+        { 
+            "q": "Washington University Professor Murray Weidenbaum, who died last week, headed the Council of Economic Advisers for which president?",
+            "a": [
+                {"option": "Richard Nixon",      "correct": false},
+                {"option": "Gerald Ford",     "correct": false},
+                {"option": "Ronald Reagan",      "correct": true},
+                {"option": "George H.W. Bush",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/murray-l-weidenbaum-washington-university-economist-chief-architect-reaganomics'>Weidenbaum issued the first written version of Ronald Reagan’s economic plan in 1981, outlining the “four pillars” of the program: tax cuts, spending cuts, reduced government regulations and tightening the money supply. Gloria Ross wrote that he left the administration to return to Washington U., where he had founded the Center for the Study of American Business, which is now named after him.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/murray-l-weidenbaum-washington-university-economist-chief-architect-reaganomics'>Weidenbaum issued the first written version of Ronald Reagan’s economic plan in 1981, outlining the “four pillars” of the program: tax cuts, spending cuts, reduced government regulations and tightening the money supply. Gloria Ross wrote that he left the administration to return to Washington U., where he had founded the Center for the Study of American Business, which is now named after him.</a></p>" // no comma here
+        },
+        { 
+            "q": "Harris-Stowe State University has roots that go back to what year?",
+            "a": [
+                {"option": "1857",      "correct": true},
+                {"option": "1867",     "correct": false},
+                {"option": "1877",      "correct": false},
+                {"option": "1887",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/harris-stowe-s-long-winding-road'>The school’s first predecessor was a teachers training institution for whites that became Harris Teachers College. As education reporter Dale Singer recounted, it merged with a similar institution for blacks, Stowe Teachers College. The school eventually joined the state higher education system and became a university in 2005.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/harris-stowe-s-long-winding-road'>The school’s first predecessor was a teachers training institution for whites that became Harris Teachers College. As education reporter Dale Singer recounted, it merged with a similar institution for blacks, Stowe Teachers College. The school eventually joined the state higher education system and became a university in 2005.</a></p>" // no comma here
+        }
+]
+};

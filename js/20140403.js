568b9ca6f38a143955c5a2af04007adc7eb26501
brentajones <bjones@bjones.net>
2014-04-03 12:06:36 -0500

    update for Mar 31, 2014

---
+++ js/20140403.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for the week of Mar 31, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "When is the Normandy School District set to go out of business?",
+            "a": [
+                {"option": "April 15",      "correct": false},
+                {"option": "May 31",     "correct": false},
+                {"option": "June 30",      "correct": false},
+                {"option": "No one knows",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/normandy-not-bankrupt-now-its-future-remains-cloudy'>Education officials had said the district would go bankrupt by April 1, but as Dale Singer reported, Normandy’s future remains uncertain. It will finish out the current school year, but what happens after that is likely to be determined by the Missouri legislature, while is in session until mid-May. The state school board meets shortly after that to determine the district’s fate.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/normandy-not-bankrupt-now-its-future-remains-cloudy'>Education officials had said the district would go bankrupt by April 1, but as Dale Singer reported, Normandy’s future remains uncertain. It will finish out the current school year, but what happens after that is likely to be determined by the Missouri legislature, while is in session until mid-May. The state school board meets shortly after that to determine the district’s fate.</a></p>" // no comma here
+        },
+        { 
+            "q": "Under a bill passed by the Missouri Senate, what would be the top income tax rate for individuals?",
+            "a": [
+                {"option": "6 percent",      "correct": false},
+                {"option": "5.75 percent",     "correct": false},
+                {"option": "5.5 percent",      "correct": true},
+                {"option": "5.25 percent",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-senate-passes-620-million-tax-cut-bill'>The Senate bill cuts the top rate to 5.5 percent from the current 6 percent. Capital correspondent Marshall Griffin reported that the legislation, which also would phase in a 25 percent deduction on an individual's business income, would cost the state $620 million in revenue annually. It now goes to the House, which passed its own version of a tax cut in February.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-senate-passes-620-million-tax-cut-bill'>The Senate bill cuts the top rate to 5.5 percent from the current 6 percent. Capital correspondent Marshall Griffin reported that the legislation, which also would phase in a 25 percent deduction on an individual's business income, would cost the state $620 million in revenue annually. It now goes to the House, which passed its own version of a tax cut in February.</a></p>" // no comma here
+        },
+        { 
+            "q": "What original document will be displayed at the Missouri History Museum starting this fall?",
+            "a": [
+                {"option": "First draft of Dred Scott decision",      "correct": false},
+                {"option": "First Missouri Constitution",     "correct": false},
+                {"option": "St. Louis city charter",      "correct": false},
+                {"option": "Louisiana Purchase treaty",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-history-museum-display-original-louisiana-purchase-treaty'>The Louisiana Purchase treaty, the actual document that made what is now Missouri part of the United States in 1803, will be on loan from the National Archives to the Missouri History Museum. Nancy Fowler reported that the more 828,000 square miles of land from France immediately doubled the size of the country and eventually became part or all of 14 states.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-history-museum-display-original-louisiana-purchase-treaty'>The Louisiana Purchase treaty, the actual document that made what is now Missouri part of the United States in 1803, will be on loan from the National Archives to the Missouri History Museum. Nancy Fowler reported that the more 828,000 square miles of land from France immediately doubled the size of the country and eventually became part or all of 14 states.</a></p>" // no comma here
+        },
+        { 
+            "q": "A new study found that Missouri has given up how much in corporate tax revenue since the recession?",
+            "a": [
+                {"option": "$945 million",      "correct": false},
+                {"option": "$1.4 billion",     "correct": false},
+                {"option": "$1.7 billion",      "correct": true},
+                {"option": "$2.1 billion",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-missouri-gave-17-billion-corporate-taxes-recession'>The report co-published Monday by the Center for Effective Government, National People's Action and GrassRoots Organizing said corporations are paying Missouri about $1.7 billion, or 26 percent, less in income taxes than they did at the beginning of the recession six years ago. Joseph Leahy reported that efforts to make more attractive to businesses led to the policy changes.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-missouri-gave-17-billion-corporate-taxes-recession'>The report co-published Monday by the Center for Effective Government, National People's Action and GrassRoots Organizing said corporations are paying Missouri about $1.7 billion, or 26 percent, less in income taxes than they did at the beginning of the recession six years ago. Joseph Leahy reported that efforts to make more attractive to businesses led to the policy changes.</a></p>" // no comma here
+        },
+        { 
+            "q": "Under Missouri’s Cold Weather Rule, utilities may not shut off service when the temperature dips below what mark within a 24-hour period?",
+            "a": [
+                {"option": "34 degrees",      "correct": false},
+                {"option": "32 degrees",     "correct": true},
+                {"option": "30 degrees",      "correct": false},
+                {"option": "28 degrees",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/laclede-extends-cold-weather-rule'>Laclede Gas announced that because of the unusually cold winter, it will honor Missouri’s Cold Weather Rule for an extra month, through April. Maria Altman explained that state law prohibits utility companies from turning off customer’s service if the temperature dips below 32 degrees within a 24-hour period.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/laclede-extends-cold-weather-rule'>Laclede Gas announced that because of the unusually cold winter, it will honor Missouri’s Cold Weather Rule for an extra month, through April. Maria Altman explained that state law prohibits utility companies from turning off customer’s service if the temperature dips below 32 degrees within a 24-hour period.</a></p>" // no comma here
+        },
+        { 
+            "q": "The son of what longtime Missouri politician is running for a seat in the state Senate?",
+            "a": [
+                {"option": "Christopher Bond",      "correct": false},
+                {"option": "John Ashcroft",     "correct": true},
+                {"option": "John Danforth",      "correct": false},
+                {"option": "Jay Nixon",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/republicans-vie-keep-control-democratic-leaning-district'>Jay Ashcroft, son of former Governor, Senator and U.S. Attorney General John Ashcroft, was a last-minute entrant into the GOP primary for Missouri Senate in the 24th District in St. Louis County. Jason Rosenblum reported that the winner of the three-way Republican primary will face Democrat Rep. Jill Schupp to succeed Sen. John Lamping, R-Ladue, who is not seeking re-election.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/republicans-vie-keep-control-democratic-leaning-district'>Jay Ashcroft, son of former Governor, Senator and U.S. Attorney General John Ashcroft, was a last-minute entrant into the GOP primary for Missouri Senate in the 24th District in St. Louis County. Jason Rosenblum reported that the winner of the three-way Republican primary will face Democrat Rep. Jill Schupp to succeed Sen. John Lamping, R-Ladue, who is not seeking re-election.</a></p>" // no comma here
+        },
+        { 
+            "q": "Researchers are looking into whether medical treatment has contributed to the spread of what disease among African Americans?",
+            "a": [
+                {"option": "Sickle cell anemia",      "correct": false},
+                {"option": "AIDS",     "correct": false},
+                {"option": "Diabetes",      "correct": false},
+                {"option": "Asthma",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-hypothesis-asthma-blacks-are-medications-part-problem'>Reporter Robert Joiner wrote that a new, federal study is looking beyond environmental factors to explain uncontrollable asthma among many African Americans. The study, funded by the National Institute of Health,  is described as a definitive look at why some of the treatments are ineffective for blacks, and whether genes influence how some blacks respond to the therapies.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-hypothesis-asthma-blacks-are-medications-part-problem'>Reporter Robert Joiner wrote that a new, federal study is looking beyond environmental factors to explain uncontrollable asthma among many African Americans. The study, funded by the National Institute of Health,  is described as a definitive look at why some of the treatments are ineffective for blacks, and whether genes influence how some blacks respond to the therapies.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri General Assembly gave unanimous approval to a bill sponsored by Rep. Rory Ellinger, D-University City, giving legal protections to what group in the state?",
+            "a": [
+                {"option": "Welfare recipients",      "correct": false},
+                {"option": "Breast-feeding mothers",     "correct": true},
+                {"option": "Veterans",      "correct": false},
+                {"option": "Physicians in training",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/breast-feeding-protection-bill-sponsored-ailing-lawmaker-sent-gov-nixon'>Ellinger, who is on leave from the legislature after being diagnosed with liver cancer, sponsored the bill to allow breast-feeding moms to be excused from jury duty and would mandate that breast-feeding in public does not equal indecent exposure or conduct. The bill was put on a fast track to passage.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/breast-feeding-protection-bill-sponsored-ailing-lawmaker-sent-gov-nixon'>Ellinger, who is on leave from the legislature after being diagnosed with liver cancer, sponsored the bill to allow breast-feeding moms to be excused from jury duty and would mandate that breast-feeding in public does not equal indecent exposure or conduct. The bill was put on a fast track to passage.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which Southern Illinois landmark was NOT on the list of endangered structures released by Landmarks Illinois?",
+            "a": [
+                {"option": "Giant ketchup bottle near Collinsville",      "correct": true},
+                {"option": "Hamilton Primary School in Otterville ",     "correct": false},
+                {"option": "Hotel Belleville, last used as a retirement home",      "correct": false},
+                {"option": "Old Millstadt water tower",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/millstadt-water-tower-hotel-belleville-make-illinois-historic-endangered-structures-list'>Fans of the primary school, hotel and water tower are working to raise funds to save the structures, Donna Korando reported. The Millstadt water tower is the first such structure to be included on the list regularly issued by Landmarks Illinois.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/millstadt-water-tower-hotel-belleville-make-illinois-historic-endangered-structures-list'>Fans of the primary school, hotel and water tower are working to raise funds to save the structures, Donna Korando reported. The Millstadt water tower is the first such structure to be included on the list regularly issued by Landmarks Illinois.</a></p>" // no comma here
+        },
+        { 
+            "q": "Laurie Westfall, widow of former St. Louis County Executive Buzz Westfall, was appointed to what county board?",
+            "a": [
+                {"option": "Elections",      "correct": false},
+                {"option": "Police Commissioners",     "correct": true},
+                {"option": "Highways and Traffic",      "correct": false},
+                {"option": "World Trade Center",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/westfalls-approval-brings-end-squabbles-over-police-board-nominations'>Westfall’s appointment by County Executive Charlie Dooley ended a long-running saga to fill vacancies on the board of police commissioners. Jason Rosenbaum reported that her unanimous approval means that the five-member board is now at full strength for the first time in months.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/westfalls-approval-brings-end-squabbles-over-police-board-nominations'>Westfall’s appointment by County Executive Charlie Dooley ended a long-running saga to fill vacancies on the board of police commissioners. Jason Rosenbaum reported that her unanimous approval means that the five-member board is now at full strength for the first time in months.</a></p>" // no comma here
+        }	
+]
+};

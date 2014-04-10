cdddd275454f63a5b0515c342672197b604e32a7
brentajones <bjones@bjones.net>
2014-04-10 17:57:25 -0500

    update for week

---
+++ js/20140410.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Apr 11, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "President Barack Obama has nominated Ronnie White to the U.S. District Court of the Eastern District of Missouri. It is the second time White has been nominated to this position. What year did President Bill Clinton originally nominate White?",
+            "a": [
+                {"option": "1996",      "correct": false},
+                {"option": "1997",     "correct": true},
+                {"option": "1998",      "correct": false},
+                {"option": "1999",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ronnie-whites-nomination-recalls-past-battles-and-hard-feelings'>As Jason Rosenbaum reported, President Bill Clinton nominated White in 1997. But his nomination ran into fierce opposition from then-U.S. Sen. John Ashcroft. He convinced his Republican colleagues to vote against White as a bloc. That amounted to a death knell since the GOP controlled the Senate at the time.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ronnie-whites-nomination-recalls-past-battles-and-hard-feelings'>As Jason Rosenbaum reported, President Bill Clinton nominated White in 1997. But his nomination ran into fierce opposition from then-U.S. Sen. John Ashcroft. He convinced his Republican colleagues to vote against White as a bloc. That amounted to a death knell since the GOP controlled the Senate at the time.</a></p>" // no comma here
+        },
+        { 
+            "q": "Laclede Gas has acquired another natural gas utility, this one outside of Missouri. In what state is the new acquisition located?",
+            "a": [
+                {"option": "Alabama",      "correct": true},
+                {"option": "Arkansas",     "correct": false},
+                {"option": "Mississippi",      "correct": false},
+                {"option": "Tennessee",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/laclede-expands-alabama'>As Maria Altman reported, Laclede Gas announced Monday it will buy the Alabama Gas Corp., known as Alagasco, from Energen Corp. for $1.6 billion. Laclede Group President and CEO Suzanne Sitherwood said the effective cost would drop to $1.34 billion after taking tax benefits of about $260 million into account. The acquisition is also expected to generate net revenue in the first year.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/laclede-expands-alabama'>As Maria Altman reported, Laclede Gas announced Monday it will buy the Alabama Gas Corp., known as Alagasco, from Energen Corp. for $1.6 billion. Laclede Group President and CEO Suzanne Sitherwood said the effective cost would drop to $1.34 billion after taking tax benefits of about $260 million into account. The acquisition is also expected to generate net revenue in the first year.</a></p>" // no comma here
+        },
+        { 
+            "q": "Municipal elections were held throughout the region on Tuesday. That's when the public elects local leaders, school board members and approves tax and bond issues. What was the voter turnout in St. Louis County?",
+            "a": [
+                {"option": "2 percent",      "correct": false},
+                {"option": "12 percent",     "correct": true},
+                {"option": "22 percent",      "correct": false},
+                {"option": "32 percent",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/muny-election-results-clayton-splits-props-u-city-firefighters-campaign-controversy'>As Chris McDaniel reported, only 12 percent of registered St. Louis County voters turned out to the polls. Typically fewer than one out of five voters casts vote in Missouri's April elections. Election director Rita Days had said earlier that the election officals have the same amount of preparation to do whether 90 percent or 10 percent of voters show up.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/muny-election-results-clayton-splits-props-u-city-firefighters-campaign-controversy'>As Chris McDaniel reported, only 12 percent of registered St. Louis County voters turned out to the polls. Typically fewer than one out of five voters casts vote in Missouri's April elections. Election director Rita Days had said earlier that the election officals have the same amount of preparation to do whether 90 percent or 10 percent of voters show up.</a></p>" // no comma here
+        },
+        { 
+            "q": "Every year, the American Rivers organization lists the nation's most endangered rivers. This year, one of our state's rivers was named the third most endangered river in the country. Which river is it?",
+            "a": [
+                {"option": "Current River",      "correct": false},
+                {"option": "Meramec River",     "correct": false},
+                {"option": "Mississippi River",      "correct": true},
+                {"option": "Missouri River",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/mississippi-river-ranked-third-most-endangered-country'>Our intern Nora Ibrahim reported that Eileen Fretz, director of flood management at American Rivers, said the Mississippi River is now a concern to both public safety and the environment because of the increased use of levees and other structures that are designed to make the river usable for industry. She pointed to a particular project the U.S. Army Corps of Engineers proposed that will make a change to the New Madrid Floodway.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/mississippi-river-ranked-third-most-endangered-country'>Our intern Nora Ibrahim reported that Eileen Fretz, director of flood management at American Rivers, said the Mississippi River is now a concern to both public safety and the environment because of the increased use of levees and other structures that are designed to make the river usable for industry. She pointed to a particular project the U.S. Army Corps of Engineers proposed that will make a change to the New Madrid Floodway.</a></p>" // no comma here
+        },
+        { 
+            "q": "Fair St. Louis gave the area “something to talk about” Tuesday when it announced that Bonnie Raitt is among the top acts for this year’s Fair St. Louis. The Fray and the Band Perry round out the list of headliners for this year’s annual Fourth of July festival. This year, though, the annual fair is moving away from its traditional venue -- the Arch grounds. Where will Fair St. Louis be held?",
+            "a": [
+                {"option": "Malcolm W. Martin Memorial Park",      "correct": false},
+                {"option": "Soldiers Memorial",     "correct": false},
+                {"option": "Forest Park",      "correct": true},
+                {"option": "Queeny Park",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fray-bonnie-raitt-band-perry-headline-fair-st-louis-foray-forest-park'>Reporter Nancy Fowler reminded readers and listeners that this year's Fair St. Louis moves to Forest Park because of construction at the Arch grounds.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fray-bonnie-raitt-band-perry-headline-fair-st-louis-foray-forest-park'>Reporter Nancy Fowler reminded readers and listeners that this year's Fair St. Louis moves to Forest Park because of construction at the Arch grounds.</a></p>" // no comma here
+        },
+        { 
+            "q": "On Tuesday, the Missouri legislature approved a supplemental budget that includes money to help ensure that the Normandy School District stays afloat for the remainder of the school year. How much did the legislature approve for Normandy?",
+            "a": [
+                {"option": "$1.5 million",      "correct": false},
+                {"option": "$2 million",     "correct": true},
+                {"option": "$5 million",      "correct": false},
+                {"option": "$6.8 million",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/voters-back-incumbents-normandy-ferguson-florissant-school-board-elections'>As Tim Lloyd reported, originally the district estimated it needed $6.8 million to stay open for the school year. That number was later reduced to $5 million and approved by the House. But last month the Senate voted to send only $1.5 million to the cash-strapped district, and both chambers ironed out the final dollar amount -- $2 million -- in a conference committee.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/voters-back-incumbents-normandy-ferguson-florissant-school-board-elections'>As Tim Lloyd reported, originally the district estimated it needed $6.8 million to stay open for the school year. That number was later reduced to $5 million and approved by the House. But last month the Senate voted to send only $1.5 million to the cash-strapped district, and both chambers ironed out the final dollar amount -- $2 million -- in a conference committee.</a></p>" // no comma here
+        },
+        { 
+            "q": "Every year more than 20,000 children ages 0 to 19 are injured by guns, said Dr. David Jaffe, the medical director of emergency services at St. Louis Children's Hospital. Every day, seven of those injuries are fatal. How many kids with gunshot wounds does St. Louis Children's Hospital treat each year on average?",
+            "a": [
+                {"option": "48",      "correct": false},
+                {"option": "70",     "correct": true},
+                {"option": "95",      "correct": false},
+                {"option": "120",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/forum-umsl-looks-solutions-epidemic-gun-violence-affecting-youth'>As Rachel Lippmann reported, Children's Hospital treats an average of 70 kids a year with gunshot wounds. Solutions to what was widely called an &quot;epidemic&quot; of gun violence plaguing the country and St. Louis were the topic Wednesday of the &quot;Safe and Sound&quot; forum at the University of Missouri-St. Louis.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/forum-umsl-looks-solutions-epidemic-gun-violence-affecting-youth'>As Rachel Lippmann reported, Children's Hospital treats an average of 70 kids a year with gunshot wounds. Solutions to what was widely called an &quot;epidemic&quot; of gun violence plaguing the country and St. Louis were the topic Wednesday of the &quot;Safe and Sound&quot; forum at the University of Missouri-St. Louis.</a></p>" // no comma here
+        },
+        { 
+            "q": "State Rep. Rory Ellinger, who died this week at the age 0f 72, spent a lifetime fighting for civil rights. For a brief time in the 1960s, he served as a bodyguard for what civil rights leader?",
+            "a": [
+                {"option": "William Clay, Sr.",      "correct": false},
+                {"option": "Pearlie Evans",     "correct": false},
+                {"option": "Medger Evers",      "correct": false},
+                {"option": "Martin Luther King Jr.",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/rep-rory-ellinger-civil-rights-activist-attorney-fought-vulnerable-and-powerless'>As Gloria Ross reported that following graduation from the University of Missouri-Kansas City in 1963, Mr. Ellinger joined the Chicago staff of Young Christian Students, an activist Catholic group working with Martin Luther King. He organized peace and civil rights groups in Boston and Cleveland, working with the Student Nonviolent Coordinating Committee and the Congress for Racial Equality.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/rep-rory-ellinger-civil-rights-activist-attorney-fought-vulnerable-and-powerless'>As Gloria Ross reported that following graduation from the University of Missouri-Kansas City in 1963, Mr. Ellinger joined the Chicago staff of Young Christian Students, an activist Catholic group working with Martin Luther King. He organized peace and civil rights groups in Boston and Cleveland, working with the Student Nonviolent Coordinating Committee and the Congress for Racial Equality.</a></p>" // no comma here
+        },
+        { 
+            "q": "Although passing &quot;right-to-work&quot; legislation was a key priority of Missouri House Speaker Tim Jones, it may be dead for this session even though it received first-round approval in the House. A minimum of 82 votes were required to advance it to the Missouri Senate. By how many votes did it fall short?",
+            "a": [
+                {"option": "4",      "correct": true},
+                {"option": "5",     "correct": false},
+                {"option": "8",      "correct": false},
+                {"option": "10",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/right-work-hits-snag-missouri-house-not-enough-votes-move-senate'>As Marshall Griffin reported, the &quot;right-to-work&quot; bill fell four votes short of the minimum 82 votes required to advance it to the Missouri Senate. Nineteen Republicans joined the entire Democratic House caucus in voting &quot;no.&quot;</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/right-work-hits-snag-missouri-house-not-enough-votes-move-senate'>As Marshall Griffin reported, the &quot;right-to-work&quot; bill fell four votes short of the minimum 82 votes required to advance it to the Missouri Senate. Nineteen Republicans joined the entire Democratic House caucus in voting &quot;no.&quot;</a></p>" // no comma here
+        },
+        { 
+            "q": "Last fall, an in-school dental clinic opened in Valley Middle School in House Springs's Northwest School District. Superintendent Paul Ziegler saw the dental clinic as an opportunity to help his students. “Having a tooth ache is not something that’s going to lend itself real well to paying attention and focusing in school,” Ziegler said. In November, its first month of operation, 39 patients received dental care. How many patients were treated in February?",
+            "a": [
+                {"option": "55",      "correct": false},
+                {"option": "137",     "correct": false},
+                {"option": "202",      "correct": false},
+                {"option": "300",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/accounted-school-dental-clinic-house-springs-gets-students-smiling'>Though the clinic has only been open for short time, its popularity has rapidly grown. In November, 39 patients received dental care. In contrast, 300 patients were seen in February. It also sees parents of students during the day, and anyone in the community can come to the clinic in the evening for dental services.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/accounted-school-dental-clinic-house-springs-gets-students-smiling'>Though the clinic has only been open for short time, its popularity has rapidly grown. In November, 39 patients received dental care. In contrast, 300 patients were seen in February. It also sees parents of students during the day, and anyone in the community can come to the clinic in the evening for dental services.</a></p>" // no comma here
+        }	
+]
+};

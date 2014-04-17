81e397012d4c657c6462c3f2d9ef5030b4c8e50c
brentajones <bjones@bjones.net>
2014-04-17 12:58:15 -0500

    update for week

---
+++ js/20140417.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Apr 18, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "A tax-cut bill sent by lawmakers to Gov. Jay Nixon had the support of one Democrat in the Missouri House. Who was it?",
+            "a": [
+                {"option": "T.J. McKenna of Festus",      "correct": false},
+                {"option": "Judy Morgan of Kansas City",     "correct": false},
+                {"option": "Bill Otto of Maryland Heights",      "correct": false},
+                {"option": "Jeff Roorda of Barnhart",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-blasts-tax-cut-bill-sent-him-missouri-lawmakers'>Jefferson City correspondent Marshall Griffin reports that if Nixon vetoes the bill, as expected, at least one Democratic vote, possibly Jeff Roorda’s, would be needed to override the veto in the House. The veto of a tax-cut bill last year was sustained when a number of Republicans did not vote to override Nixon’s rejection of the legislation.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-blasts-tax-cut-bill-sent-him-missouri-lawmakers'>Jefferson City correspondent Marshall Griffin reports that if Nixon vetoes the bill, as expected, at least one Democratic vote, possibly Jeff Roorda’s, would be needed to override the veto in the House. The veto of a tax-cut bill last year was sustained when a number of Republicans did not vote to override Nixon’s rejection of the legislation.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri state Board of Education has extended the authority of a Special Administrative Board to run the St. Louis Public Schools until when?",
+            "a": [
+                {"option": "June 2015",      "correct": false},
+                {"option": "June 2016",     "correct": true},
+                {"option": "June 2017",      "correct": false},
+                {"option": "June 2018",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/state-board-education-extends-sab-control-st-louis-public-schools-two-more-years'>The board voted in Jefferson City on Tuesday to extend the SAB’s authority over the city schools for two more years, until June 30, 2016. Tim Lloyd reported that the three-member appointed board has been in charge of the system since 2007.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/state-board-education-extends-sab-control-st-louis-public-schools-two-more-years'>The board voted in Jefferson City on Tuesday to extend the SAB’s authority over the city schools for two more years, until June 30, 2016. Tim Lloyd reported that the three-member appointed board has been in charge of the system since 2007.</a></p>" // no comma here
+        },
+        { 
+            "q": "Glenn Miller, the man who is charged with killing three people outside Jewish agencies near Kansas City last weekend, ran as a write-in candidate for what office in Missouri in 2010?",
+            "a": [
+                {"option": "U.S. Senate",      "correct": true},
+                {"option": "U.S. House",     "correct": false},
+                {"option": "Missouri Senate",      "correct": false},
+                {"option": "Missouri House",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/kc-shooting-suspect-no-stranger-jewish-agencies-here'>Miller, from southwest Missouri, ran on for the U.S. Senate on a platform that blamed Jews for many of the problems in the United States. Dale Singer reported that groups like the Anti-Defamation League have long been following Miller’s activities.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/kc-shooting-suspect-no-stranger-jewish-agencies-here'>Miller, from southwest Missouri, ran on for the U.S. Senate on a platform that blamed Jews for many of the problems in the United States. Dale Singer reported that groups like the Anti-Defamation League have long been following Miller’s activities.</a></p>" // no comma here
+        },
+        { 
+            "q": "A music festival bill passed by the St. Louis Board of Aldermen sets aside which holiday weekends for shows produced by Los Angeles-based ICM Partners?",
+            "a": [
+                {"option": "Memorial Day and Labor Day",      "correct": true},
+                {"option": "Memorial Day and July 4th",     "correct": false},
+                {"option": "July 4th and Labor Day",      "correct": false},
+                {"option": "Memorial Day, July 4th and Labor Day",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/summer-rocks-bill-rolls-mayor-slay'>Rachel Lippmann reported that as soon as Mayor Francis Slay signs the bill as expected, ICM Partners may start negotiating for talent and financing for shows starting Memorial Day and Labor Day of next year. Supporters had originally hoped to pass the bill in March, which would have given ICM the chance to produce a show for this Labor Day, but objections to the original bill prompted it to be rewritten.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/summer-rocks-bill-rolls-mayor-slay'>Rachel Lippmann reported that as soon as Mayor Francis Slay signs the bill as expected, ICM Partners may start negotiating for talent and financing for shows starting Memorial Day and Labor Day of next year. Supporters had originally hoped to pass the bill in March, which would have given ICM the chance to produce a show for this Labor Day, but objections to the original bill prompted it to be rewritten.</a></p>" // no comma here
+        },
+        { 
+            "q": "What section of federal education law concerns reporting of sexual assault cases on college campuses?",
+            "a": [
+                {"option": "Title I",      "correct": false},
+                {"option": "Title IV",     "correct": false},
+                {"option": "Title VII",      "correct": false},
+                {"option": "Title IX",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/outside-consultant-will-review-um-sexual-assault-policies'>Title IX, prohibiting sexual discrimination in education, has been in the news because of the investigation into the suicide of a swimmer at Mizzou who said she was raped several months before killing herself. Dale Singer reported on steps the University of Missouri is taking to strengthening its Title IX reporting. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/outside-consultant-will-review-um-sexual-assault-policies'>Title IX, prohibiting sexual discrimination in education, has been in the news because of the investigation into the suicide of a swimmer at Mizzou who said she was raped several months before killing herself. Dale Singer reported on steps the University of Missouri is taking to strengthening its Title IX reporting. </a></p>" // no comma here
+        },
+        { 
+            "q": "Illinois’ two U.S. senators showed the secretary of the Air Force around Scott Air Force Base in an effort to shore up prospects for the base’s future. What is the secretary’s name?",
+            "a": [
+                {"option": "Michael Wynne",      "correct": false},
+                {"option": "Michael Donley",     "correct": false},
+                {"option": "Eric Fanning",      "correct": false},
+                {"option": "Deborah Lee James",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/illinois-senators-continue-push-showcase-scott-air-force-base'>The visit was the first to Scott by Deborah Lee James since she was confirmed by the Senate last year as secretary of the Air Force. Jason Rosenbaum reported that proposed defense cuts could lead to reductions at the St. Clair County installation.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/illinois-senators-continue-push-showcase-scott-air-force-base'>The visit was the first to Scott by Deborah Lee James since she was confirmed by the Senate last year as secretary of the Air Force. Jason Rosenbaum reported that proposed defense cuts could lead to reductions at the St. Clair County installation.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which local hospital is using paramedics to try to reduce the number of 911 calls it has to handle?",
+            "a": [
+                {"option": "Christian ",      "correct": true},
+                {"option": "Barnes",     "correct": false},
+                {"option": "Mercy",      "correct": false},
+                {"option": "DePaul",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-take-emergency-care-christian-hospital-using-paramedics-reduce-911-calls'>Under the Christian Hospital program, reporter Robert Joiner found, a specially trained EMS worker travels to the scene of perceived emergencies to determine whether 911 callers need ER care or they can get by with less intensive care for non-emergency problems. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-take-emergency-care-christian-hospital-using-paramedics-reduce-911-calls'>Under the Christian Hospital program, reporter Robert Joiner found, a specially trained EMS worker travels to the scene of perceived emergencies to determine whether 911 callers need ER care or they can get by with less intensive care for non-emergency problems. </a></p>" // no comma here
+        },
+        { 
+            "q": "Students at Washington University are protesting to demand the school cut its ties with what company?",
+            "a": [
+                {"option": "Dow Chemical",      "correct": false},
+                {"option": "Wal-Mart",     "correct": false},
+                {"option": "Peabody Energy",      "correct": true},
+                {"option": "Ameren",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/washington-university-student-protest-continues'>Kelly MacNeil reported that the protest targeted Peabody Energy, which the students said harms the environment and vulnerable communities. They want the company’s president and CEO, Greg Boyce, removed from the Washington U. board, but their demands have not been met.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/washington-university-student-protest-continues'>Kelly MacNeil reported that the protest targeted Peabody Energy, which the students said harms the environment and vulnerable communities. They want the company’s president and CEO, Greg Boyce, removed from the Washington U. board, but their demands have not been met.</a></p>" // no comma here
+        },
+        { 
+            "q": "More than a year and a half before the election for Missouri governor, Democrat Chris Koster has how much money in his campaign fund?",
+            "a": [
+                {"option": "$1 million",      "correct": false},
+                {"option": "$1.5 million",     "correct": false},
+                {"option": "$2 million",      "correct": true},
+                {"option": "$2.5 million",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-outraising-outspending-likely-2016-gop-rivals-governor'>The latest campaign reports show that Koster, the current attorney general, has raised $539,364 so far this calendar year and had amassed just over $2 million in the bank. Jo Mannies reported that he has more money than either of the expected GOP candidates for the job, Tom Schweich and Catherine Hanaway.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-outraising-outspending-likely-2016-gop-rivals-governor'>The latest campaign reports show that Koster, the current attorney general, has raised $539,364 so far this calendar year and had amassed just over $2 million in the bank. Jo Mannies reported that he has more money than either of the expected GOP candidates for the job, Tom Schweich and Catherine Hanaway.</a></p>" // no comma here
+        },
+        { 
+            "q": "U.S. Sen. Mark Kirk, R-Ill., called on Boeing to extend production at its north St. Louis County defense facility of which aircraft?",
+            "a": [
+                {"option": "Apache",      "correct": false},
+                {"option": "Growler",     "correct": true},
+                {"option": "Super Hornet",      "correct": false},
+                {"option": "Osprey",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sen-kirk-calls-congress-extend-fighter-jet-production-st-louis'>Kirk toured the Boeing assembly line and told reporters he will try to convince Congress to approve and appropriate funds for 22 more of the radar-jamming EA-18 G Growler aircraft. Joseph Leahy reported that Kirk flew in missions aboard the Growler’s predecessor over the former Yugoslavia.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sen-kirk-calls-congress-extend-fighter-jet-production-st-louis'>Kirk toured the Boeing assembly line and told reporters he will try to convince Congress to approve and appropriate funds for 22 more of the radar-jamming EA-18 G Growler aircraft. Joseph Leahy reported that Kirk flew in missions aboard the Growler’s predecessor over the former Yugoslavia.</a></p>" // no comma here
+        }
+]
+};

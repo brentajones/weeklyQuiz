769e18cfda53221024cea6b761d27dd906ea5a87
brentajones <bjones@bjones.net>
2014-04-24 13:51:23 -0500

    added quiz for week

---
+++ js/20140424.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Apr 25, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Pat Whitaker resigned from the Zoo-Museum District board after allegations of a conflict of interest involving which institution?",
+            "a": [
+                {"option": "History Museum",      "correct": false},
+                {"option": "Zoo",     "correct": false},
+                {"option": "Science Center",      "correct": true},
+                {"option": "Art Museum",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/conflict-interest-allegations-lead-zmd-board-resignation-and-push-stronger-regs'>Whitaker’s Arcturis design firm recently won a contract with the Science Center, which receives about $10 million each year in tax dollars from the district. Nancy Fowler reported that Whitaker, who was named to the board in January, said she had been told that her actions did not violate any ethical guidelines.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/conflict-interest-allegations-lead-zmd-board-resignation-and-push-stronger-regs'>Whitaker’s Arcturis design firm recently won a contract with the Science Center, which receives about $10 million each year in tax dollars from the district. Nancy Fowler reported that Whitaker, who was named to the board in January, said she had been told that her actions did not violate any ethical guidelines.</a></p>" // no comma here
+        },
+        { 
+            "q": "Under a tax-cut bill sent to Gov. Jay Nixon, what would be the top income tax rate for Missourians?",
+            "a": [
+                {"option": "7.5 percent",      "correct": false},
+                {"option": "6.5 percent",     "correct": false},
+                {"option": "5.5 percent",      "correct": true},
+                {"option": "4.5 percent",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixons-latest-tax-cut-battle-general-assembly-resembles-earlier-fight-1'>The bill would cut Missouri’s top income tax rate to 5.5 percent from the current 6 percent. But Jo Mannies reported that Nixon says the bill has a “fatal flaw” that would eliminate income taxes altogether for anyone making more than $9,000. The governor is expected to veto the bill before the deadline of May 1, which would give lawmakers time to attempt an override before the legislative session adjourns May 16.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixons-latest-tax-cut-battle-general-assembly-resembles-earlier-fight-1'>The bill would cut Missouri’s top income tax rate to 5.5 percent from the current 6 percent. But Jo Mannies reported that Nixon says the bill has a “fatal flaw” that would eliminate income taxes altogether for anyone making more than $9,000. The governor is expected to veto the bill before the deadline of May 1, which would give lawmakers time to attempt an override before the legislative session adjourns May 16.</a></p>" // no comma here
+        },
+        { 
+            "q": "Dwaun Warmack, chosen by Harris-Stowe State University to be its new president, currently is a vice president at what university?",
+            "a": [
+                {"option": "Fisk ",      "correct": false},
+                {"option": "Bethune-Cookman ",     "correct": true},
+                {"option": "Clark Atlanta ",      "correct": false},
+                {"option": "Morgan State ",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/harris-stowe-names-dwaun-warmack-new-president'>Warmack is currently senior vice president for administration and student services at Bethune-Cookman University in Daytona Beach, Fla. Dale Singer reported that he will become president at Harris-Stowe on July 14.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/harris-stowe-names-dwaun-warmack-new-president'>Warmack is currently senior vice president for administration and student services at Bethune-Cookman University in Daytona Beach, Fla. Dale Singer reported that he will become president at Harris-Stowe on July 14.</a></p>" // no comma here
+        },
+        { 
+            "q": "Gov. Jay Nixon visited which Rockwood high school Monday to talk about how a tax-cut bill would hurt Missouri schools?",
+            "a": [
+                {"option": "Eureka",      "correct": false},
+                {"option": "Lafayette",     "correct": false},
+                {"option": "Marquette",      "correct": false},
+                {"option": "Summit",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-says-tax-cut-would-hurt-schools'>At Rockwood Summit, Dale Singer reported, Nixon said that a $620 million tax cut bill passed by the General Assembly would hurt schools and weaken Missouri’s chances to have a strong workforce and attract companies. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-says-tax-cut-would-hurt-schools'>At Rockwood Summit, Dale Singer reported, Nixon said that a $620 million tax cut bill passed by the General Assembly would hurt schools and weaken Missouri’s chances to have a strong workforce and attract companies. </a></p>" // no comma here
+        },
+        { 
+            "q": "Boeing’s start-up arm known as Ventures is moving some employees from its Hazelwood campus to where?",
+            "a": [
+                {"option": "Laclede’s Landing",      "correct": false},
+                {"option": "Cortex",     "correct": true},
+                {"option": "Clayton",      "correct": false},
+                {"option": "Wentzville",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/boeing-s-ventures-moving-cortex'>About 20 employees in Boeing’s Ventures start-up will move into 7,500 square feet in the Cortex complex in the Central West End, Maria Altman reported. Boeing began Ventures about two years ago in an effort to grow business outside its expertise in airplane and defense production.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/boeing-s-ventures-moving-cortex'>About 20 employees in Boeing’s Ventures start-up will move into 7,500 square feet in the Cortex complex in the Central West End, Maria Altman reported. Boeing began Ventures about two years ago in an effort to grow business outside its expertise in airplane and defense production.</a></p>" // no comma here
+        },
+        { 
+            "q": "The north St. Louis grocery store that Schnucks will be closing used to be a store in what chain?",
+            "a": [
+                {"option": "Kroger",      "correct": false},
+                {"option": "A&P",     "correct": false},
+                {"option": "National",      "correct": true},
+                {"option": "None – it started as a Schnucks store",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/offset-loss-grocery-stores-city-tries-alternate-tactics'>The grocer said it would close its North Grand store on May 10, claiming it had run a deficit since it acquired the store in 1995 from National, another supermarket chain. Robert Joiner reported that city officials are trying new tactics to make sure neighborhoods have access to full-service food stores.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/offset-loss-grocery-stores-city-tries-alternate-tactics'>The grocer said it would close its North Grand store on May 10, claiming it had run a deficit since it acquired the store in 1995 from National, another supermarket chain. Robert Joiner reported that city officials are trying new tactics to make sure neighborhoods have access to full-service food stores.</a></p>" // no comma here
+        },
+        { 
+            "q": "Disposable diapers for one baby cost how much every month?",
+            "a": [
+                {"option": "$25",      "correct": false},
+                {"option": "$50",     "correct": false},
+                {"option": "$75",      "correct": false},
+                {"option": "$100",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/diaper-drive-donates-25000-diapers-families-need'>Because of the $100 cost per month for disposable diapers for a baby, Nicole Hudson and Amanda Doyle began a drive on Facebook to secure diapers for families that can’t afford them. Kelly MacNeil reported that the effort brought in 25,000 diapers for the group Nurses for Newborns, with more expected when the drive was extended.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/diaper-drive-donates-25000-diapers-families-need'>Because of the $100 cost per month for disposable diapers for a baby, Nicole Hudson and Amanda Doyle began a drive on Facebook to secure diapers for families that can’t afford them. Kelly MacNeil reported that the effort brought in 25,000 diapers for the group Nurses for Newborns, with more expected when the drive was extended.</a></p>" // no comma here
+        },
+        { 
+            "q": "What does the ride service Lyft use to help cultivate a friendly image for its passengers?",
+            "a": [
+                {"option": "Chocolate chip cookies",      "correct": false},
+                {"option": "Complimentary wi-fi",     "correct": false},
+                {"option": "A pink mustache on car grills",      "correct": true},
+                {"option": "Drivers who also sing opera",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/judge-orders-ride-share-lyft-stop-service'>Lyft wanted to begin service in the St. Louis area, but as Jason Rosenbaum and Chris McDaniel reported, a judge issued an order to halt the rides after the St. Louis Metropolitan Taxi Cab Commission filed suit. So for now, the pink mustache cars aren't supposed to be on the streets. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/judge-orders-ride-share-lyft-stop-service'>Lyft wanted to begin service in the St. Louis area, but as Jason Rosenbaum and Chris McDaniel reported, a judge issued an order to halt the rides after the St. Louis Metropolitan Taxi Cab Commission filed suit. So for now, the pink mustache cars aren't supposed to be on the streets. </a></p>" // no comma here
+        },
+        { 
+            "q": "Besides Louis IX, the namesake of the city of St. Louis, how many other French kings became saints?",
+            "a": [
+                {"option": "None",      "correct": true},
+                {"option": "One",     "correct": false},
+                {"option": "Two",      "correct": false},
+                {"option": "Three",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/why-st-louis-named-after-french-king-who-was-born-800-years-ago'>Louis IX was the only French king to become a saint, Camille Phillips reported. His life and death were bookended by the Crusades – he was crowned in 1226 at the age of 12 after his father died in the wars, and in 1270 he himself died while on his second crusade to Jerusalem.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/why-st-louis-named-after-french-king-who-was-born-800-years-ago'>Louis IX was the only French king to become a saint, Camille Phillips reported. His life and death were bookended by the Crusades – he was crowned in 1226 at the age of 12 after his father died in the wars, and in 1270 he himself died while on his second crusade to Jerusalem.</a></p>" // no comma here
+        },
+        { 
+            "q": "Employees at what hospital got training in how to react when an armed intruder enters the workplace?",
+            "a": [
+                {"option": "DePaul",      "correct": false},
+                {"option": "St. Anthony’s",     "correct": true},
+                {"option": "Mercy",      "correct": false},
+                {"option": "Barnes",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/police-employees-learn-what-do-when-faced-shooter'>Reporter Rachel Lippmann observed the training of employees at St. Anthony’s as part of a program called “Run, Hide, Fight.” Participants are trained to know when is the best time to do each of those things. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/police-employees-learn-what-do-when-faced-shooter'>Reporter Rachel Lippmann observed the training of employees at St. Anthony’s as part of a program called “Run, Hide, Fight.” Participants are trained to know when is the best time to do each of those things. </a></p>" // no comma here
+        }	
+]
+};

1a600f6d53b12d48894b54a951dcfef28f7d3530
brentajones <bjones@bjones.net>
2014-05-01 16:47:30 -0500

    update for week

---
+++ js/20140501.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for May 2, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "The Missouri Senate approved a measure to let voters decide whether to impose what kind of tax for the state’s transportation needs?",
+            "a": [
+                {"option": "Sales tax",      "correct": true},
+                {"option": "Fuel tax",     "correct": false},
+                {"option": "Income tax",      "correct": false},
+                {"option": "Personal property tax",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-senate-passes-scaled-back-transportation-sales-tax-proposal'>The Senate measure would raise the sales tax by three-quarters of a cent if voters approve; a House-passed measure would institute a 1-cent sales tax. Marshall Griffin reported that the state needs $485 million each year just to maintain its current transportation system.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-senate-passes-scaled-back-transportation-sales-tax-proposal'>The Senate measure would raise the sales tax by three-quarters of a cent if voters approve; a House-passed measure would institute a 1-cent sales tax. Marshall Griffin reported that the state needs $485 million each year just to maintain its current transportation system.</a></p>" // no comma here
+        },
+        { 
+            "q": "St. Louis Mayor Francis Slay went to Jefferson City Monday to denounce legislation  moving through the Missouri General Assembly as “absurd, embarrassing and reckless.” What is the bill’s topic?",
+            "a": [
+                {"option": "School choice",      "correct": false},
+                {"option": "Tax cuts",     "correct": false},
+                {"option": "Gun control",      "correct": true},
+                {"option": "Right to work",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/big-city-mayors-speak-out-against-gun-nullification-bills-missouri-capitol'>Along with the mayor of Kansas City, Sly James, Slay criticized a bill that would nullify federal gun laws within Missouri. Marshall Griffin said two versions of the bill, one in the House and one in the Senate, are being debated; Gov. Jay Nixon vetoed a bill on the same topic last year.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/big-city-mayors-speak-out-against-gun-nullification-bills-missouri-capitol'>Along with the mayor of Kansas City, Sly James, Slay criticized a bill that would nullify federal gun laws within Missouri. Marshall Griffin said two versions of the bill, one in the House and one in the Senate, are being debated; Gov. Jay Nixon vetoed a bill on the same topic last year.</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the name of the new Medicaid expansion plan introduced by Gov. Jay Nixon in Cape Girardeau on Monday?",
+            "a": [
+                {"option": "A Healthy Missouri",      "correct": false},
+                {"option": "Sound Bodies, Sound Missouri",     "correct": false},
+                {"option": "Missouri Checkup",      "correct": false},
+                {"option": "Missouri Health Works",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-proposes-new-medicaid-expansion-program-catching-legislators-guard'>Under Nixon’s “Missouri Health Works” plan, according to reporter Jo Mannies, the state would direct extra federal Medicaid dollars to small businesses to subsidize a portion of the health insurance for employees who are paid less than 138 percent of the poverty level – just under $16,000 a year for an individual adult. The governor said versions of the idea are already in place in nine other states.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-proposes-new-medicaid-expansion-program-catching-legislators-guard'>Under Nixon’s “Missouri Health Works” plan, according to reporter Jo Mannies, the state would direct extra federal Medicaid dollars to small businesses to subsidize a portion of the health insurance for employees who are paid less than 138 percent of the poverty level – just under $16,000 a year for an individual adult. The governor said versions of the idea are already in place in nine other states.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these is NOT an issue included in the impeachment charges against Gov. Jay Nixon heard by a Missouri House committee?",
+            "a": [
+                {"option": "Special elections",      "correct": false},
+                {"option": "Fee offices",     "correct": true},
+                {"option": "Same-sex marriage",      "correct": false},
+                {"option": "Concealed weapons",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/impeachment-hearings-against-nixon-wrap-no-vote-yet'>The three articles of impeachment against Nixon involved his executive order on tax returns for same-sex couples, the timing of his calling of special elections to fill legislative vacancies and his failure to discipline officials in a controversy over Missouri’s list of conceal-carry permit holders. Marshall Griffin reported that hearings over the allegations ended without a vote by a House committee.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/impeachment-hearings-against-nixon-wrap-no-vote-yet'>The three articles of impeachment against Nixon involved his executive order on tax returns for same-sex couples, the timing of his calling of special elections to fill legislative vacancies and his failure to discipline officials in a controversy over Missouri’s list of conceal-carry permit holders. Marshall Griffin reported that hearings over the allegations ended without a vote by a House committee.</a></p>" // no comma here
+        },
+        { 
+            "q": "Where in Forest Park is the Loop Trolley going to stop?",
+            "a": [
+                {"option": "Muny Opera",      "correct": false},
+                {"option": "Zoo",     "correct": false},
+                {"option": "History Museum",      "correct": true},
+                {"option": "Boathouse",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/loop-trolley-lawsuit-dismissed'>The trolley line would go from the Delmar Loop to the History Museum along a fixed track. Maria Altman reported that a lawsuit that could have blocked the project was dismissed by a federal judge.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/loop-trolley-lawsuit-dismissed'>The trolley line would go from the Delmar Loop to the History Museum along a fixed track. Maria Altman reported that a lawsuit that could have blocked the project was dismissed by a federal judge.</a></p>" // no comma here
+        },
+        { 
+            "q": "Voters in the 13th Ward in St. Louis chose a new alderman Tuesday to replace veteran Fred Wessels, who was named to head what city agency?",
+            "a": [
+                {"option": "Board of Public Service",      "correct": false},
+                {"option": "Community Development Agency",     "correct": true},
+                {"option": "Emergency Management Agency",      "correct": false},
+                {"option": "Planning and Urban Design Agency",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/murphy-wins-13th-ward-race-replace-wessels'>Wessels was named to head the Community Development Agency after serving 25 years on the Board of Aldermen, Chris McDaniel reported. Democrat Beth Murphy won the special election for his seat on Tuesday, defeating Republican Conan Prendergast. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/murphy-wins-13th-ward-race-replace-wessels'>Wessels was named to head the Community Development Agency after serving 25 years on the Board of Aldermen, Chris McDaniel reported. Democrat Beth Murphy won the special election for his seat on Tuesday, defeating Republican Conan Prendergast. </a></p>" // no comma here
+        },
+        { 
+            "q": "In the latest clash between St. Louis County Executive Charlie Dooley and his Democratic challenger, Steve Stenger, what was the topic?",
+            "a": [
+                {"option": "Property assessments",      "correct": false},
+                {"option": "Cronyism",     "correct": false},
+                {"option": "Animal euthanasia",      "correct": true},
+                {"option": "Zoo-Museum District",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stenger-dooley-spar-over-county-animal-shelters-euthanization-rate-0'>Stenger claimed the county’s health department, which runs the animal shelter, is responsible for the deaths of thousands of dogs and cats each year. Jo Mannies reported this reaction from Dooley’s campaign: “It looks like silly season has come early.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stenger-dooley-spar-over-county-animal-shelters-euthanization-rate-0'>Stenger claimed the county’s health department, which runs the animal shelter, is responsible for the deaths of thousands of dogs and cats each year. Jo Mannies reported this reaction from Dooley’s campaign: “It looks like silly season has come early.”</a></p>" // no comma here
+        },
+        { 
+            "q": "What Republican politician did U.S. Rep. William Lacy Clay, D-St. Louis, invite to Missouri to see the effects of proposed cuts in federal spending?",
+            "a": [
+                {"option": "Ron Paul",      "correct": false},
+                {"option": "Rand Paul",     "correct": false},
+                {"option": "Paul Ryan",      "correct": true},
+                {"option": "John Boehner",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/clay-invites-paul-ryan-visit-st-louis-witness-effects-proposed-budget-cuts'>In a letter to Paul Ryan, as reported by Jo Mannies, Clay said the proposed GOP budget would devastate programs for the elderly, handicapped and the poor. Ryan met with the Congressional Black Caucus for what he called “an engaging and productive discussion.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/clay-invites-paul-ryan-visit-st-louis-witness-effects-proposed-budget-cuts'>In a letter to Paul Ryan, as reported by Jo Mannies, Clay said the proposed GOP budget would devastate programs for the elderly, handicapped and the poor. Ryan met with the Congressional Black Caucus for what he called “an engaging and productive discussion.”</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the only St. Charles County municipality that has red light cameras?",
+            "a": [
+                {"option": "Wentzville",      "correct": false},
+                {"option": "St. Charles",     "correct": false},
+                {"option": "O’Fallon",      "correct": false},
+                {"option": "St. Peters",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-charles-county-council-consider-red-light-camera-ban'>St. Peters is currently the only municipality in the county that employs the controversial cameras, which are set up at seven intersections, Joseph Leahy reported. A charter amendment to ban the cameras countywide could be on the ballot this summer.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-charles-county-council-consider-red-light-camera-ban'>St. Peters is currently the only municipality in the county that employs the controversial cameras, which are set up at seven intersections, Joseph Leahy reported. A charter amendment to ban the cameras countywide could be on the ballot this summer.</a></p>" // no comma here
+        },
+        { 
+            "q": "According to Washington University history professor Peter Kastor, what is most likely the proper way to pronounce the name Sacagawea?",
+            "a": [
+                {"option": "Sah-cog-ah-WAY-ah",      "correct": true},
+                {"option": "Sack-ah-ja-WEE-ah",     "correct": false},
+                {"option": "Sack-AH-ja-way-ah",      "correct": false},
+                {"option": "Sock-ah-JA-way-ah",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/listen-why-youre-probably-pronouncing-sacagawea-wrong'>On “St. Louis on the Air,” Kastor said that during her lifetime, the name of Lewis and Clark’s Native American guide most likely was pronounced with the hard “g” sound. He explained how the more common pronunciation, with the “j” sound, came to be popular. Blame it on an editor; that’s what we always do.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/listen-why-youre-probably-pronouncing-sacagawea-wrong'>On “St. Louis on the Air,” Kastor said that during her lifetime, the name of Lewis and Clark’s Native American guide most likely was pronounced with the hard “g” sound. He explained how the more common pronunciation, with the “j” sound, came to be popular. Blame it on an editor; that’s what we always do.</a></p>" // no comma here
+        }
+]
+};

a3ec6ba856d28e13f204bd06e5c93bed72664718
brentajones <bjones@bjones.net>
2014-05-08 17:12:58 -0500

    update for week

---
+++ js/20140508.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for May 9, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        {
+            "q": "Keith English, the lone Democrat who voted to override Gov. Jay Nixon’s veto of a $620 million tax-cut bill, is from what north St Louis County municipality?",
+            "a": [
+                {"option": "Ferguson",      "correct": false},
+                {"option": "Jennings",     "correct": false},
+                {"option": "Florissant",      "correct": true},
+                {"option": "Spanish Lake",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-house-joins-senate-override-veto-tax-cut-bill'>With the vote from English, a Florissant Democrat, Republicans were able to override Nixon’s veto. The bill will gradually lower Missouri’s top tax rate on income to 5.5 percent, Marshall Griffin and Jo Mannies reported.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-house-joins-senate-override-veto-tax-cut-bill'>With the vote from English, a Florissant Democrat, Republicans were able to override Nixon’s veto. The bill will gradually lower Missouri’s top tax rate on income to 5.5 percent, Marshall Griffin and Jo Mannies reported.</a></p>" // no comma here
+        },
+        {
+            "q": "A group calling itself “Teach Great” turned in petition signatures to get on the Missouri ballot a measure affecting what?",
+            "a": [
+                {"option": "Charter schools",      "correct": false},
+                {"option": "Common Core standards",     "correct": false},
+                {"option": "Teacher preparation",      "correct": false},
+                {"option": "Teacher tenure",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/teachers-unions-gear-oppose-proposals-hiring-firing'>If it gets on the ballot and is approved by voters, the Teach Great proposal would eliminate teacher tenure and base teacher retention on performance-based evaluations and \"quantifiable student performance data.\" Dale Singer reported that teachers unions are gearing up to oppose the issue if it gets a spot on the November statewide ballot.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/teachers-unions-gear-oppose-proposals-hiring-firing'>If it gets on the ballot and is approved by voters, the Teach Great proposal would eliminate teacher tenure and base teacher retention on performance-based evaluations and \"quantifiable student performance data.\" Dale Singer reported that teachers unions are gearing up to oppose the issue if it gets a spot on the November statewide ballot.</a></p>" // no comma here
+        },
+        {
+            "q": "The mayor of Chesterfield said his city could leave St. Louis County and join St. Charles County over what issue?",
+            "a": [
+                {"option": "MetroLink expansion",      "correct": false},
+                {"option": "Possible merger with St. Louis",     "correct": false},
+                {"option": "Sales tax distribution",      "correct": true},
+                {"option": "Flood plain protection",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chesterfield-mayors-secession-talk-raises-eyebrows-and-faces-long-long-odds'>Chesterfield Mayor Bob Nation said his city should be able to keep more of the sales tax that stores there collect. Jason Rosenbaum reported that Nation’s comment about possible secession was met with skepticism.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chesterfield-mayors-secession-talk-raises-eyebrows-and-faces-long-long-odds'>Chesterfield Mayor Bob Nation said his city should be able to keep more of the sales tax that stores there collect. Jason Rosenbaum reported that Nation’s comment about possible secession was met with skepticism.</a></p>" // no comma here
+        },
+        {
+            "q": "More than 20 protesters unhappy over what issue were arrested in the Missouri Capitol after they disrupted Senate debate?",
+            "a": [
+                {"option": "School transfers",      "correct": false},
+                {"option": "Gun control",     "correct": false},
+                {"option": "Income tax",      "correct": false},
+                {"option": "Medicaid expansion",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/medicaid-expansion-supporters-disrupt-missouri-senate'>The protesters began shouting slogans about Medicaid expansion and singing songs from the public gallery above the Senate floor during debate on an unrelated bill, Marshall Griffin reported. Missouri lawmakers have so far rebuffed efforts to expand Medicaid in next year's state budget.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/medicaid-expansion-supporters-disrupt-missouri-senate'>The protesters began shouting slogans about Medicaid expansion and singing songs from the public gallery above the Senate floor during debate on an unrelated bill, Marshall Griffin reported. Missouri lawmakers have so far rebuffed efforts to expand Medicaid in next year's state budget.</a></p>" // no comma here
+        },
+        {
+            "q": "Which St. Louis neighborhood will be the next focus of “hot spot” policing?",
+            "a": [
+                {"option": "Wells-Goodfellow",      "correct": true},
+                {"option": "College Hill",     "correct": false},
+                {"option": "Tower Grove South",      "correct": false},
+                {"option": "The Ville",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wells-goodfellow-next-site-st-louis-police-hot-spot-policing'>Wells-Goodfellow, on the city’s northwest side, follows College Hill and Tower Grove South as neighborhoods getting extra attention from the St. Louis Police Department. Rachel Lippmann reported that overall crime in the neighborhood is down, but it's been the site of eight of the city's 44 homicides this year.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wells-goodfellow-next-site-st-louis-police-hot-spot-policing'>Wells-Goodfellow, on the city’s northwest side, follows College Hill and Tower Grove South as neighborhoods getting extra attention from the St. Louis Police Department. Rachel Lippmann reported that overall crime in the neighborhood is down, but it's been the site of eight of the city's 44 homicides this year.</a></p>" // no comma here
+        },
+        {
+            "q": "What was Missouri’s revenue situation in April?",
+            "a": [
+                {"option": "Individual income tax up, sales tax up",      "correct": false},
+                {"option": "Individual income tax down, sales tax up",     "correct": true},
+                {"option": "Individual income tax up, sales tax down",      "correct": false},
+                {"option": "Individual income tax down, sales tax down",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-revenue-drops-april-raising-questions-and-concerns'>Compared with April 2013, individual income tax collections were down 13.5 percent, while sales tax collections were up 7.7 percent. Jo Mannies reported that the state had a drop in net general-revenue collections of 5 percent in April, or  $50 million.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-revenue-drops-april-raising-questions-and-concerns'>Compared with April 2013, individual income tax collections were down 13.5 percent, while sales tax collections were up 7.7 percent. Jo Mannies reported that the state had a drop in net general-revenue collections of 5 percent in April, or  $50 million.</a></p>" // no comma here
+        },
+        {
+            "q": "Students from Normandy played host to their counterparts from which Parkway high school on Tuesday?",
+            "a": [
+                {"option": "Parkway Central",      "correct": false},
+                {"option": "Parkway North",     "correct": true},
+                {"option": "Parkway South",      "correct": false},
+                {"option": "Parkway West",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/after-semester-talking-online-normandy-and-parkway-students-finally-meet'>Students from Normandy and Parkway North had been meeting by video conference during the school year, but finally got together in person. Tim Lloyd reported that the meeting helped both sides break through stereotypes and see other students as real people.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/after-semester-talking-online-normandy-and-parkway-students-finally-meet'>Students from Normandy and Parkway North had been meeting by video conference during the school year, but finally got together in person. Tim Lloyd reported that the meeting helped both sides break through stereotypes and see other students as real people.</a></p>" // no comma here
+        },
+        {
+            "q": "What former politician wrote a book titled “Success Can Kill You”?",
+            "a": [
+                {"option": "Jeff Smith",      "correct": false},
+                {"option": "Rod Jetton",     "correct": true},
+                {"option": "William Webster",      "correct": false},
+                {"option": "Kacie Starr Triplett",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-power-brokers-fall-rod-jettons-new-book-raises-questions-about-politics'>Rod Jetton was once speaker of the Missouri House, but by the time he left office, the FBI was investigating him for bribery and he was facing serious jail time after being accused of felony assault. Jason Rosenbaum reported that Jetton’s book offers new insight into how misplaced priorities, flattery and bitterness can seriously backfire.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-power-brokers-fall-rod-jettons-new-book-raises-questions-about-politics'>Rod Jetton was once speaker of the Missouri House, but by the time he left office, the FBI was investigating him for bribery and he was facing serious jail time after being accused of felony assault. Jason Rosenbaum reported that Jetton’s book offers new insight into how misplaced priorities, flattery and bitterness can seriously backfire.</a></p>" // no comma here
+        },
+        {
+            "q": "A new program at the St. Louis County Library called “hoopla” involves what?",
+            "a": [
+                {"option": "Music concerts",      "correct": false},
+                {"option": "Streaming video",     "correct": true},
+                {"option": "Teen-oriented programming",      "correct": false},
+                {"option": "Services for older adults",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-library-patrons-can-stream-movies-music-and-more'>Donna Korando reported that “hoopla” will allow county library card holders to download and stream movies, TV shows, music and audio books.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-library-patrons-can-stream-movies-music-and-more'>Donna Korando reported that “hoopla” will allow county library card holders to download and stream movies, TV shows, music and audio books.</a></p>" // no comma here
+        },
+        {
+            "q": "Sam Page, who will be the Democratic candidate for the St. Louis County Council in a special election in August, ran unsuccessfully for what statewide office in 2008?",
+            "a": [
+                {"option": "Lieutenant governor",      "correct": true},
+                {"option": "Secretary of state",     "correct": false},
+                {"option": "Treasurer",      "correct": false},
+                {"option": "Attorney general",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/parties-choose-nominees-fill-vacant-seat-st-louis-county-council'>Page lost the 2008 race for lieutenant governor to Peter Kinder. A physician and former state representative, Page will face Robert Saettele, a city councilman in Bridgeton who also is a businessman, on Aug. 2, to succeed the late Kathleen Burkett on the council, Jo Mannies reported. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/parties-choose-nominees-fill-vacant-seat-st-louis-county-council'>Page lost the 2008 race for lieutenant governor to Peter Kinder. A physician and former state representative, Page will face Robert Saettele, a city councilman in Bridgeton who also is a businessman, on Aug. 2, to succeed the late Kathleen Burkett on the council, Jo Mannies reported. </a></p>" // no comma here
+        }
+]
+};

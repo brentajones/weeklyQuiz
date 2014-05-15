a91745ffa103cf19d07087539ee84665c0123c3f
brentajones <bjones@bjones.net>
2014-05-15 17:30:23 -0500

    update for week

---
+++ js/20140515.js
@@ -0,0 +1,123 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for May 16, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Vice President Joe Biden came to St. Louis this week to visit what attraction?",
+            "a": [
+                {"option": "Ballpark Village",      "correct": false},
+                {"option": "Forest Park",     "correct": false},
+                {"option": "Botanical Garden",      "correct": false},
+                {"option": "Gateway Arch",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/visit-arch-biden-praises-investments-infrastructure'>Biden came to visit the Arch and praise improvements to the grounds, part of the CityArchRiver project, as a prime example of successful partnerships. Chris McDaniel and Nora Ibrahim reported that the vice president said the federal government needed to invest in jobs, but added that the federal government couldn’t do it alone.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/visit-arch-biden-praises-investments-infrastructure'>Biden came to visit the Arch and praise improvements to the grounds, part of the CityArchRiver project, as a prime example of successful partnerships. Chris McDaniel and Nora Ibrahim reported that the vice president said the federal government needed to invest in jobs, but added that the federal government couldn’t do it alone.</a></p>" // no comma here
+        },
+        { 
+            "q": "The debate over Tesla automobiles in Missouri has centered on what issue?",
+            "a": [
+                {"option": "Union membership by workers",      "correct": false},
+                {"option": "Direct sales to consumers",     "correct": true},
+                {"option": "Expensive maintenance contracts",      "correct": false},
+                {"option": "Parts from overseas",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fight-over-electric-car-provides-jolt-final-week-legislative-session'>At issue was a provision that would prevent automakers from selling directly to consumers instead of through dealerships. Jo Mannies and Jason Rosenbaum reported that the prohibition could have effectively killed Missouri operations for the maker of the electric car.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fight-over-electric-car-provides-jolt-final-week-legislative-session'>At issue was a provision that would prevent automakers from selling directly to consumers instead of through dealerships. Jo Mannies and Jason Rosenbaum reported that the prohibition could have effectively killed Missouri operations for the maker of the electric car.</a></p>" // no comma here
+        },
+        { 
+            "q": "What kind of insect is the Emerald Ash Borer, an invasive species that threatens ash trees in the St. Louis area?",
+            "a": [
+                {"option": "Moth",      "correct": false},
+                {"option": "Beetle",     "correct": true},
+                {"option": "Aphid",      "correct": false},
+                {"option": "Weevil",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-ash-trees-be-warned-emerald-ash-borer-loose'>The green beetle, with a penchant for any kind of ash tree, has infested and killed millions of trees nationwide. Shula Neuman reported that the Emerald Ash Borer was first spotted in Missouri in Wayne County in 2008. Since then, it’s spread around the state and has been identified in Perry County, which is only about 50 miles south of St. Louis.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-ash-trees-be-warned-emerald-ash-borer-loose'>The green beetle, with a penchant for any kind of ash tree, has infested and killed millions of trees nationwide. Shula Neuman reported that the Emerald Ash Borer was first spotted in Missouri in Wayne County in 2008. Since then, it’s spread around the state and has been identified in Perry County, which is only about 50 miles south of St. Louis.</a></p>" // no comma here
+        },
+        { 
+            "q": "Missouri Gov. Jay Nixon was criticized for paying dues to the National Governors Association from the budget of what department?",
+            "a": [
+                {"option": "Revenue",      "correct": false},
+                {"option": "Transportation",     "correct": false},
+                {"option": "Education",      "correct": false},
+                {"option": "Social Services",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/legislators-governor-spar-over-dues-governors-group'>The chairs of legislative budget committees said the governor misused the money that came from the Department of Social Services to pay the dues. Jo Mannies reported that Nixon’s office said the spending had never been secret and accused critics of using a “diversionary stunt’’ to distract the public from missteps during the legislative session.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/legislators-governor-spar-over-dues-governors-group'>The chairs of legislative budget committees said the governor misused the money that came from the Department of Social Services to pay the dues. Jo Mannies reported that Nixon’s office said the spending had never been secret and accused critics of using a “diversionary stunt’’ to distract the public from missteps during the legislative session.</a></p>" // no comma here
+        },
+        { 
+            "q": "What uniform number have the Rams assigned to high-profile draft pick Michael Sam?",
+            "a": [
+                {"option": "94",      "correct": false},
+                {"option": "95",     "correct": false},
+                {"option": "96",      "correct": true},
+                {"option": "97",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/michael-sam-just-wants-play-football'>Sam, who would become the NFL’s first openly gay player if he makes the Rams roster, was assigned uniform number 96. Jess Jiang reported that at a news conference introducing the team’s draft picks, Sam said he’s ready to stop talking about his sexual orientation and let his work on the field speak for itself.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/michael-sam-just-wants-play-football'>Sam, who would become the NFL’s first openly gay player if he makes the Rams roster, was assigned uniform number 96. Jess Jiang reported that at a news conference introducing the team’s draft picks, Sam said he’s ready to stop talking about his sexual orientation and let his work on the field speak for itself.</a></p>" // no comma here
+        },
+        { 
+            "q": "Who is expected to perform at the opening of the new Jazz Center in St. Louis this fall?",
+            "a": [
+                {"option": "Ellis Marsalis",      "correct": false},
+                {"option": "Branford Marsalis",     "correct": false},
+                {"option": "Wynton Marsalis",      "correct": true},
+                {"option": "All three",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/jazz-st-louis-get-10-million-makeover'>After receiving a $10 million makeover, the center is expected to open on the site of the former Jazz at the Bistro with a performance by Wynton Marsalis in October. Nancy Fowler reported that new location will include a renovated performance space, an education center and a jazz lounge.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/jazz-st-louis-get-10-million-makeover'>After receiving a $10 million makeover, the center is expected to open on the site of the former Jazz at the Bistro with a performance by Wynton Marsalis in October. Nancy Fowler reported that new location will include a renovated performance space, an education center and a jazz lounge.</a></p>" // no comma here
+        },
+        { 
+            "q": "A new report from the group Better Together focuses on what issue involving regional economic development?",
+            "a": [
+                {"option": "Sales tax distribution",      "correct": false},
+                {"option": "Tax-increment financing",     "correct": true},
+                {"option": "Rehabilitation of old buildings",      "correct": false},
+                {"option": "Public transportation",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/better-togethers-economic-development-report-examines-tif-use'>The report emphasizes that some the region's tax-increment financing plans have been beneficial to municipalities, but it says the widespread use of TIFs is pitting municipalities against each other. Jason Rosenbaum reported it pointed to a 2010 situation where Bridgeton approved a TIF to entice a Wal-Mart to move two miles away from its original location.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/better-togethers-economic-development-report-examines-tif-use'>The report emphasizes that some the region's tax-increment financing plans have been beneficial to municipalities, but it says the widespread use of TIFs is pitting municipalities against each other. Jason Rosenbaum reported it pointed to a 2010 situation where Bridgeton approved a TIF to entice a Wal-Mart to move two miles away from its original location.</a></p>" // no comma here
+        },
+        { 
+            "q": "Where has Circus Flora played besides St. Louis?",
+            "a": [
+                {"option": "Kennedy Center",      "correct": true},
+                {"option": "Chicago’s Millennium Park",     "correct": false},
+                {"option": "Branson",      "correct": false},
+                {"option": "Madison Square Garden",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/david-balding-founder-circus-flora-lived-life-under-big-top'>Circus Flora, which was founded by the late David Balding, has played not only in St. Louis but at the Kennedy Center and other spots. Robert Duffy reported that Balding, who died last week of injuries suffered in a fall, named the circus after an elephant he rescued.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/david-balding-founder-circus-flora-lived-life-under-big-top'>Circus Flora, which was founded by the late David Balding, has played not only in St. Louis but at the Kennedy Center and other spots. Robert Duffy reported that Balding, who died last week of injuries suffered in a fall, named the circus after an elephant he rescued.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which artist is NOT on the list of those who will play LouFest this year?",
+            "a": [
+                {"option": "OutKast",      "correct": false},
+                {"option": "Magnetic Zeros",     "correct": true},
+                {"option": "Cake",      "correct": false},
+                {"option": "Arctic Monkeys",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-loufest-lineup-features-outkast-cake-matt-kim'>The Magnetic Zeros played the Forest Park festival last year but aren’t on this year’s bill. Nancy Fowler reported that that this year's 40-band Loufest lineup offers a diversity of musical acts for the Sept. 6-7 event.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-loufest-lineup-features-outkast-cake-matt-kim'>The Magnetic Zeros played the Forest Park festival last year but aren’t on this year’s bill. Nancy Fowler reported that that this year's 40-band Loufest lineup offers a diversity of musical acts for the Sept. 6-7 event.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these St. Louis entertainment venues is the oldest?",
+            "a": [
+                {"option": "Tivoli",      "correct": false},
+                {"option": "Hi-Pointe",     "correct": true},
+                {"option": "Fox",      "correct": false},
+                {"option": "Esquire",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-film-history-actors-moguls-movies-and-theaters-ties-region'>The Hi-Pointe opened in 1922, giving it the distinction of being the area’s oldest movie theater still in existence, Amanda Honigfort reported. The Tivoli opened two years later, featuring both silent movies and vaudeville.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-film-history-actors-moguls-movies-and-theaters-ties-region'>The Hi-Pointe opened in 1922, giving it the distinction of being the area’s oldest movie theater still in existence, Amanda Honigfort reported. The Tivoli opened two years later, featuring both silent movies and vaudeville.</a></p>" // no comma here
+        },
+	
+]
+};

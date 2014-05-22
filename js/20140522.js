c2e48e583e5dd24ca39863a8b179417d4d44aa48
brentajones <bjones@bjones.net>
2014-05-22 15:43:27 -0500

    update for week

---
+++ js/20140522.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for May 23, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "What will the newly created Normandy school entity be called?",
+            "a": [
+                {"option": "Normandy Student Initiative",      "correct": false},
+                {"option": "Normandy Schools Collaborative",     "correct": true},
+                {"option": "Normandy Education Consortium",      "correct": false},
+                {"option": "Normandy Learning Experiment",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-board-education-approves-replacing-normandy-school-district-new-organization'>The Missouri state board of education voted to replace the existing Normandy school district with the Normandy Schools Collaborative, effective July 1. Dale Singer reported that the new entity’s borders will be the same, but all contracts will be void and Normandy’s accreditation status is yet to be determined.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-board-education-approves-replacing-normandy-school-district-new-organization'>The Missouri state board of education voted to replace the existing Normandy school district with the Normandy Schools Collaborative, effective July 1. Dale Singer reported that the new entity’s borders will be the same, but all contracts will be void and Normandy’s accreditation status is yet to be determined.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which U.S. Supreme Court justice issued a stay in Missouri’s planned execution of Russell Bucklew?",
+            "a": [
+                {"option": "Ruth Bader Ginsburg",      "correct": false},
+                {"option": "Elena Kagan",     "correct": false},
+                {"option": "Samuel Alito",      "correct": true},
+                {"option": "Anthony Kennedy",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-prepares-first-execution-oklahoma-botched-one'>Justice Samuel Alito issued the stay in the case of Bucklew, who had been sentenced to die for a murder and rape in 1996. Chris McDaniel reported that Bucklew claimed a rare birth defect would mean the execution protocol would result in extreme pain.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-prepares-first-execution-oklahoma-botched-one'>Justice Samuel Alito issued the stay in the case of Bucklew, who had been sentenced to die for a murder and rape in 1996. Chris McDaniel reported that Bucklew claimed a rare birth defect would mean the execution protocol would result in extreme pain.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these is NOT a focus of a company that won one of 20 $50,000 Arch Grants for start-up firms?",
+            "a": [
+                {"option": "Preventing head injuries in student athletes",      "correct": false},
+                {"option": "Helping homeowners save money on electricity",     "correct": false},
+                {"option": "Determining the best time to buy a cheap airline ticket",      "correct": true},
+                {"option": "Making a life vest that lets swimmers rescue themselves",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/arch-grants-board-announces-2014-class'>All of the ideas except the one about cheap airline tickets were part of the list of 20 winning proposals in the latest round of Arch Grants. Maria Altman reported that six of the winning startups are already located in St. Louis, and the rest will be coming, some from as far away as England and Colombia.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/arch-grants-board-announces-2014-class'>All of the ideas except the one about cheap airline tickets were part of the list of 20 winning proposals in the latest round of Arch Grants. Maria Altman reported that six of the winning startups are already located in St. Louis, and the rest will be coming, some from as far away as England and Colombia.</a></p>" // no comma here
+        },
+        { 
+            "q": "Missouri Sens. Claire McCaskill and Roy Blunt recently jointly signed a letter on what issue?",
+            "a": [
+                {"option": "Sexual assault on campus",      "correct": false},
+                {"option": "Defense contracting",     "correct": false},
+                {"option": "Flood plain management",      "correct": false},
+                {"option": "Health care for veterans",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/mccaskill-blunt-renew-calls-improving-care-military-veterans'>The bi-partisan letter asked Department of Veterans Affairs Secretary Eric Shinseki to provide answers about allegations regarding delays in mental-health care at Cochran Veterans Administration hospital. Jo Mannies reported that some VA facilities elsewhere have been under attack over accusations of inappropriately long wait times before veterans could obtain appointments for medical care.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/mccaskill-blunt-renew-calls-improving-care-military-veterans'>The bi-partisan letter asked Department of Veterans Affairs Secretary Eric Shinseki to provide answers about allegations regarding delays in mental-health care at Cochran Veterans Administration hospital. Jo Mannies reported that some VA facilities elsewhere have been under attack over accusations of inappropriately long wait times before veterans could obtain appointments for medical care.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which Missouri Republican is being urged to enter the 2016 race for governor?",
+            "a": [
+                {"option": "Peter Kinder",      "correct": false},
+                {"option": "Tim Jones",     "correct": false},
+                {"option": "Blaine Luetkemeyer",      "correct": true},
+                {"option": "Jane Cunningham",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/luetkemeyer-getting-strong-encouragement-jump-gop-2016-field-governor'>Blaine Luetkemeyer, who represents Missouri’s sprawling 3rd Congressional District, has been urged to enter the Republican primary, joining Tom Schweich and Catherine Hanaway. Jo Mannies reported that Luetkemeyer has not rejected the idea and says he will study it after his re-election campaign this fall.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/luetkemeyer-getting-strong-encouragement-jump-gop-2016-field-governor'>Blaine Luetkemeyer, who represents Missouri’s sprawling 3rd Congressional District, has been urged to enter the Republican primary, joining Tom Schweich and Catherine Hanaway. Jo Mannies reported that Luetkemeyer has not rejected the idea and says he will study it after his re-election campaign this fall.</a></p>" // no comma here
+        },
+        { 
+            "q": "Officials in what St. Charles County school district announced plans for a new truancy court to help solve chronic student absenteeism?",
+            "a": [
+                {"option": "Francis Howell	",      "correct": false},
+                {"option": "Fort Zumwalt",     "correct": false},
+                {"option": "Orchard Farm",      "correct": false},
+                {"option": "St. Charles",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-charles-launches-push-get-students-class'>The truancy court will try to stem absenteeism in schools in the St. Charles district. Rachel Lippmann reported that depending on what authorities find, parents of chronically truant students could be charged with a misdemeanor.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-charles-launches-push-get-students-class'>The truancy court will try to stem absenteeism in schools in the St. Charles district. Rachel Lippmann reported that depending on what authorities find, parents of chronically truant students could be charged with a misdemeanor.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which plays are being presented this year by Shakespeare Festival St. Louis in Forest Park?",
+            "a": [
+                {"option": "Henry IV and Henry V",      "correct": true},
+                {"option": "Henry V and Henry VI",     "correct": false},
+                {"option": "Henry VI and Henry VII",      "correct": false},
+                {"option": "Henry VII and Henry VIII",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/shakespeare-park-takes-two-plays-0'>The company is presenting Henry IV and Henry V on alternating nights, allowing for continuity of the storyline. Camille Phillips reported that audiences will also get the chance to see the two shows back to back on Saturday, June 7 and Saturday, June 14.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/shakespeare-park-takes-two-plays-0'>The company is presenting Henry IV and Henry V on alternating nights, allowing for continuity of the storyline. Camille Phillips reported that audiences will also get the chance to see the two shows back to back on Saturday, June 7 and Saturday, June 14.</a></p>" // no comma here
+        },
+        { 
+            "q": "The U.S. chess championships were played this week at the Chess Club and Scholastic Center of St. Louis. Where is the center located?",
+            "a": [
+                {"option": "Webster University",      "correct": false},
+                {"option": "Downtown",     "correct": false},
+                {"option": "Central West End",      "correct": true},
+                {"option": "Chesterfield",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chess-armageddon-looms-today-us-championships-finale'>Gata Kamsky and Irina Krush won the men’s and women’s championships, respectively, at the matches played at the Chess Club and Scholastic Center of St. Louis in the Central West End. In both cases, Brian Jerauld reported, regulation play ended in a three-way tie for first place, triggering playoffs.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chess-armageddon-looms-today-us-championships-finale'>Gata Kamsky and Irina Krush won the men’s and women’s championships, respectively, at the matches played at the Chess Club and Scholastic Center of St. Louis in the Central West End. In both cases, Brian Jerauld reported, regulation play ended in a three-way tie for first place, triggering playoffs.</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the setting for this year’s production of “The Magic Flute” by Opera Theatre of St. Louis?",
+            "a": [
+                {"option": "Europe in the 17th century",      "correct": false},
+                {"option": "Japan in the 19th century",     "correct": false},
+                {"option": "A 1930s Hollywood musical sound stage",      "correct": true},
+                {"option": "Modern day Italy",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/magic-flute-brings-fun-and-sparkles-opera-theatre-st-louis'>Isaac Mizrahi is staging the “Magic Flute” production on a 1930s Hollywood musical sound stage. Patricia Rice reported that Mizrahi is adding dancers who will shadow the major characters expressing added emotion, a la choreographer Agnes de Mille’s  layered dance into “Oklahoma.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/magic-flute-brings-fun-and-sparkles-opera-theatre-st-louis'>Isaac Mizrahi is staging the “Magic Flute” production on a 1930s Hollywood musical sound stage. Patricia Rice reported that Mizrahi is adding dancers who will shadow the major characters expressing added emotion, a la choreographer Agnes de Mille’s  layered dance into “Oklahoma.”</a></p>" // no comma here
+        },
+        { 
+            "q": "What was the first production in the newly renovated Sun Theater when it opened in 1913?",
+            "a": [
+                {"option": "Hamlet",      "correct": false},
+                {"option": "Faust",     "correct": true},
+                {"option": "The Misanthrope",      "correct": false},
+                {"option": "Oedipus Rex",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/100-year-old-sun-theater-grand-center-reopens-after-major-renovation'>The first production at the theater on March 1, 1913, was Johann Wolfgang von Goethe’s “Faust,” performed entirely in German. Alex Heuer reported that the $11.5 million renovation of the Sun will provide performing arts space for the Grand Center Arts Academy.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/100-year-old-sun-theater-grand-center-reopens-after-major-renovation'>The first production at the theater on March 1, 1913, was Johann Wolfgang von Goethe’s “Faust,” performed entirely in German. Alex Heuer reported that the $11.5 million renovation of the Sun will provide performing arts space for the Grand Center Arts Academy.</a></p>" // no comma here
+        }
+]
+};

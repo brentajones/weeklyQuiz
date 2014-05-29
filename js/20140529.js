6451b3e47535e1c786d3fa6f934df6d405f059cd
brentajones <bjones@bjones.net>
2014-05-29 12:23:36 -0500

    update for week

---
+++ js/20140529.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for May 30, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Poet Maya Angelou, who died this week at age 86, was born in St. Louis with what name?",
+            "a": [
+                {"option": "Martha Williamson",      "correct": false},
+                {"option": "Mary Washington",     "correct": false},
+                {"option": "Miriam Jefferson",      "correct": false},
+                {"option": "Marguerite Johnson",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/maya-angelou-poet-activist-and-singular-storyteller-dies-86'>Angelou was born as Marguerite Johnson. It was her brother who first called her Maya, and the name stuck, NPR’s Lynn Neary reported. Later she added the Angelou, a version of her first husband's name.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/maya-angelou-poet-activist-and-singular-storyteller-dies-86'>Angelou was born as Marguerite Johnson. It was her brother who first called her Maya, and the name stuck, NPR’s Lynn Neary reported. Later she added the Angelou, a version of her first husband's name.</a></p>" // no comma here
+        },
+        { 
+            "q": "How much does Gov. Jay Nixon say that last-minute tax cuts approved by the Missouri legislature would cost local jurisdictions?",
+            "a": [
+                {"option": "$251 million",      "correct": false},
+                {"option": "$351 million",     "correct": true},
+                {"option": "$451 million",      "correct": false},
+                {"option": "$561 million",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-warns-huge-cuts-local-services-because-legislatures-last-minute-tax-cuts'>Nixon says the lawmakers’ “grab bag of giveaways” will cost local jurisdictions in Missouri $351 million and he plans to use his veto power to undo some of the tax cuts. Jo Mannies reported that House Speaker Tim Jones said the governor misrepresented legislators’ goals in making the cuts.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-warns-huge-cuts-local-services-because-legislatures-last-minute-tax-cuts'>Nixon says the lawmakers’ “grab bag of giveaways” will cost local jurisdictions in Missouri $351 million and he plans to use his veto power to undo some of the tax cuts. Jo Mannies reported that House Speaker Tim Jones said the governor misrepresented legislators’ goals in making the cuts.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these is NOT one of the three key areas of focus of the new strategic plan from the St. Louis Economic Development Partnership?",
+            "a": [
+                {"option": "Tax policy",      "correct": true},
+                {"option": "Jobs",     "correct": false},
+                {"option": "People",      "correct": false},
+                {"option": "Place",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/economic-development-partnership-charts-out-priorities'>The report released Tuesday said the group will focus on jobs, people and place. Jason Rosenbaum reported that goals for the effort include supporting entrepreneurs, increasing foreign trade and investment and attracting talented immigrants to the region.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/economic-development-partnership-charts-out-priorities'>The report released Tuesday said the group will focus on jobs, people and place. Jason Rosenbaum reported that goals for the effort include supporting entrepreneurs, increasing foreign trade and investment and attracting talented immigrants to the region.</a></p>" // no comma here
+        },
+        { 
+            "q": "Plans to build a coal ash landfill near what Franklin County town have drawn protests?",
+            "a": [
+                {"option": "Washington",      "correct": false},
+                {"option": "Union",     "correct": false},
+                {"option": "Labadie",      "correct": true},
+                {"option": "St. Clair",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/franklin-county-board-hear-appeal-against-amerens-coal-ash-landfill-plans'>Some Franklin County residents, along with the Labadie Environmental Organization, are voicing disapproval of the county's decision to allow Ameren to build a coal ash landfill next to its coal-fired power plant in Labadie. Veronique LaCapra reported that the protests center around a claim that the county violated its own regulations in issuing a permit for the landfill.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/franklin-county-board-hear-appeal-against-amerens-coal-ash-landfill-plans'>Some Franklin County residents, along with the Labadie Environmental Organization, are voicing disapproval of the county's decision to allow Ameren to build a coal ash landfill next to its coal-fired power plant in Labadie. Veronique LaCapra reported that the protests center around a claim that the county violated its own regulations in issuing a permit for the landfill.</a></p>" // no comma here
+        },
+        { 
+            "q": "St. Louis County announced a new agreement with what entity designed to speed construction projects?",
+            "a": [
+                {"option": "Ameren Missouri",      "correct": false},
+                {"option": "Metropolitan Sewer District",     "correct": true},
+                {"option": "St. Louis Labor Council",      "correct": false},
+                {"option": "Associated General Contractors",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-agreement-between-st-louis-county-and-sewer-distict-promises-more-business'>The agreement between the county and the Metropolitan Sewer District allows them to share software that will track and manage construction permits. Maria Altman reported that officials from the two entities said having one system for permitting will be more cost-effective for both governmental agencies. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-agreement-between-st-louis-county-and-sewer-distict-promises-more-business'>The agreement between the county and the Metropolitan Sewer District allows them to share software that will track and manage construction permits. Maria Altman reported that officials from the two entities said having one system for permitting will be more cost-effective for both governmental agencies. </a></p>" // no comma here
+        },
+        { 
+            "q": "What group put up the big bird cage at the Zoo in Forest Park as part of the 1904 World’s Fair?",
+            "a": [
+                {"option": "Audubon Society",      "correct": false},
+                {"option": "Humane Society",     "correct": false},
+                {"option": "Smithsonian Institution",      "correct": true},
+                {"option": "National Geographic Society",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/big-bird-cage-where-st-louis-zoo-took-flight-after-1904-worlds-fair'>The big cage, whose dome reaches 50 feet into the sky, was constructed by the Smithsonian Institution to house its bird exhibit at the fair. Mary Delach Leonard reported that it was believed to be the world's largest aviary at the time -- more than twice the length of a basketball court.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/big-bird-cage-where-st-louis-zoo-took-flight-after-1904-worlds-fair'>The big cage, whose dome reaches 50 feet into the sky, was constructed by the Smithsonian Institution to house its bird exhibit at the fair. Mary Delach Leonard reported that it was believed to be the world's largest aviary at the time -- more than twice the length of a basketball court.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which St. Louis circuit judge blocked a ballot measure that would have allowed voters to put restrictions on which companies could receive tax incentives?",
+            "a": [
+                {"option": "Timothy Wilson",      "correct": false},
+                {"option": "Margaret Neill",     "correct": false},
+                {"option": "Robert Dierker",      "correct": true},
+                {"option": "Thomas Frawley",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/judge-permanently-blocks-ballot-vote-anti-peabody-initiative'>Judge Robert Dierker ruled that the Take Back St. Louis initiative was &quot;illegal and void on its face&quot; because it conflicts with Missouri laws governing tax increment financing and special business districts. Rachel Lippmann reported that supporters of the drive said that the ruling shut down a conversation about how development dollars are used in St. Louis.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/judge-permanently-blocks-ballot-vote-anti-peabody-initiative'>Judge Robert Dierker ruled that the Take Back St. Louis initiative was &quot;illegal and void on its face&quot; because it conflicts with Missouri laws governing tax increment financing and special business districts. Rachel Lippmann reported that supporters of the drive said that the ruling shut down a conversation about how development dollars are used in St. Louis.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri Military Memorial Foundation is working to raise money for a memorial wall to be located where?",
+            "a": [
+                {"option": "Jefferson Barracks Park",      "correct": true},
+                {"option": "Forest Park",     "correct": false},
+                {"option": "Gateway Arch grounds",      "correct": false},
+                {"option": "Soldiers Memorial downtown",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-group-plans-memory-wall-honor-missourians-killed-iraq-and-afghanistan'>The group is working to raise $1 million for the memorial wall in Jefferson Barracks Park to honor those who died in Iraq, Afghanistan and Desert Storm. Mary Delach Leonard reported that this summer, it plans to put up a park pavilion that will eventually connect to the monument with a pathway of memorial pavers.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-group-plans-memory-wall-honor-missourians-killed-iraq-and-afghanistan'>The group is working to raise $1 million for the memorial wall in Jefferson Barracks Park to honor those who died in Iraq, Afghanistan and Desert Storm. Mary Delach Leonard reported that this summer, it plans to put up a park pavilion that will eventually connect to the monument with a pathway of memorial pavers.</a></p>" // no comma here
+        },
+        { 
+            "q": "Students from what St. Louis County school district worked with professional artists for a show at the Clayton Fine Art Gallery?",
+            "a": [
+                {"option": "Mehlville",      "correct": false},
+                {"option": "Affton",     "correct": false},
+                {"option": "Bayless",      "correct": false},
+                {"option": "Lindbergh",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/art-show-will-feature-work-lindbergh-students-and-their-mentors'>Students from the Lindbergh schools worked with professional artists for “The Blooming Artists Project.” Mary Delach Leonard reported that the show will feature student artwork side by side with the work of their mentors -- local jewelers, sculptors, painters, fiber artists and photographers.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/art-show-will-feature-work-lindbergh-students-and-their-mentors'>Students from the Lindbergh schools worked with professional artists for “The Blooming Artists Project.” Mary Delach Leonard reported that the show will feature student artwork side by side with the work of their mentors -- local jewelers, sculptors, painters, fiber artists and photographers.</a></p>" // no comma here
+        },
+        { 
+            "q": "The St. Louis Media History Foundation has acquired copies of an early bi-lingual newspaper published here. It was published in English and what other language?",
+            "a": [
+                {"option": "German",      "correct": false},
+                {"option": "French",     "correct": false},
+                {"option": "Spanish",      "correct": true},
+                {"option": "Hebrew",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/local-media-history-group-acquires-copies-bi-lingual-business-paper'>The foundation bought nine copies of the Spanish-English newspaper El Comercio del Valle from a private collector. Rachel Lippmann reported that it began publishing in 1876 and everything but the front page was in Spanish.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/local-media-history-group-acquires-copies-bi-lingual-business-paper'>The foundation bought nine copies of the Spanish-English newspaper El Comercio del Valle from a private collector. Rachel Lippmann reported that it began publishing in 1876 and everything but the front page was in Spanish.</a></p>" // no comma here
+        }
+]
+};

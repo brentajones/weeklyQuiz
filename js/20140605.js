83f0c2b9e82f6fb2c278a594523101ce8239e4d2
brentajones <bjones@bjones.net>
2014-06-05 17:09:31 -0500

    update for week

---
+++ js/20140605.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jun 6, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+	{ 
+    "q": "Which of these Missouri school districts has not been run by an appointed board?",
+    "a": [
+        {"option": "Normandy",      "correct": true},
+        {"option": "Riverview Gardens",     "correct": false},
+        {"option": "St. Louis",      "correct": false},
+        {"option": "Wellston",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/appointed-boards-hold-lessons-normandy-keep-sense-urgency-even-when-change-slow'>The other districts have had state-appointed boards, but Normandy has been run so far by an elected board. Dale Singer reported that a plan approved last month would change that, with the newly configured Normandy Schools Collaborative run by a state-appointed board as of July 1; the move is being challenged in court.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/appointed-boards-hold-lessons-normandy-keep-sense-urgency-even-when-change-slow'>The other districts have had state-appointed boards, but Normandy has been run so far by an elected board. Dale Singer reported that a plan approved last month would change that, with the newly configured Normandy Schools Collaborative run by a state-appointed board as of July 1; the move is being challenged in court.</a></p>" // no comma here
+},
+{ 
+    "q": "Which statewide official proposed that Missouri set up its own state-run laboratory to create the drugs needed for lethal injections?",
+    "a": [
+        {"option": "Jay Nixon",      "correct": false},
+        {"option": "Peter Kinder",     "correct": false},
+        {"option": "Chris Koster",      "correct": true},
+        {"option": "Tom Schweich",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/kosters-stance-death-penalty-may-boost-democrats-election-chances'>Attorney General Chris Koster made the proposal about drugs for executions following growing concerns about the secrecy surrounding Missouri’s lethal injection methods. Jo Mannies reported that the suggestion has touched off political debate about the idea.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/kosters-stance-death-penalty-may-boost-democrats-election-chances'>Attorney General Chris Koster made the proposal about drugs for executions following growing concerns about the secrecy surrounding Missouri’s lethal injection methods. Jo Mannies reported that the suggestion has touched off political debate about the idea.</a></p>" // no comma here
+},
+{ 
+    "q": "A bill signed into law this week sets Missouri’s presidential primary in what month?",
+    "a": [
+        {"option": "March",      "correct": true},
+        {"option": "April",     "correct": false},
+        {"option": "May",      "correct": false},
+        {"option": "June",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouris-longstanding-dispute-over-presidential-primaries-may-be-resolved'>The newly signed law sets the presidential primary on the second Tuesday after the first Monday in March. Jo Mannies reported that the 2016 primary date set under the law would be March 15, the first day allowed by the two national political parties without incurring penalties.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouris-longstanding-dispute-over-presidential-primaries-may-be-resolved'>The newly signed law sets the presidential primary on the second Tuesday after the first Monday in March. Jo Mannies reported that the 2016 primary date set under the law would be March 15, the first day allowed by the two national political parties without incurring penalties.</a></p>" // no comma here
+},
+{ 
+    "q": "What reason did Gov. Jay Nixon give for opposing a sales tax hike for transportation on the August ballot?",
+    "a": [
+        {"option": "Sales taxes fall too heavily on lower-income families",      "correct": false},
+        {"option": "Lawmakers have passed too many tax breaks",     "correct": false},
+        {"option": "Transportation improvements should be funded more by users of services",      "correct": false},
+        {"option": "All of the above",     "correct": true} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-counties-ready-project-wish-lists-nixon-opposed'>Coming out against the sales-tax proposal, the governor said it would be irresponsible to approve it when lawmakers have passed so many tax breaks, the increase would fall too heavily on working class Missouri families and the heaviest users of the state’s roads would get a free pass. Jason Rosenbaum reported on the projects sought by St. Louis and St. Louis County if the 0.75 percent increase wins approval in August.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-counties-ready-project-wish-lists-nixon-opposed'>Coming out against the sales-tax proposal, the governor said it would be irresponsible to approve it when lawmakers have passed so many tax breaks, the increase would fall too heavily on working class Missouri families and the heaviest users of the state’s roads would get a free pass. Jason Rosenbaum reported on the projects sought by St. Louis and St. Louis County if the 0.75 percent increase wins approval in August.</a></p>" // no comma here
+},
+{ 
+    "q": "Which Republican member of the U.S. House from Missouri did NOT take part in appearances in favor of the “Right to Farm and Ranch” amendment on the Aug. 5 ballot?",
+    "a": [
+        {"option": "Vicky Hartzler",      "correct": false},
+        {"option": "Jason Smith",     "correct": false},
+        {"option": "Billy Long",      "correct": false},
+        {"option": "Ann Wagner",     "correct": true} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/right-farm-tour-hits-jefferson-city-former-lt-gov-criticizes-proposal'>Ann Wagner did not join fellow Republicans Hartzler, Smith, Long and Blaine Luetkemeyer as they fanned out through Missouri to push for passage of the amendment. Marshall Griffin reported that former Lt. Gov. Joe Maxwell, a Democrat, opposes the amendment.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/right-farm-tour-hits-jefferson-city-former-lt-gov-criticizes-proposal'>Ann Wagner did not join fellow Republicans Hartzler, Smith, Long and Blaine Luetkemeyer as they fanned out through Missouri to push for passage of the amendment. Marshall Griffin reported that former Lt. Gov. Joe Maxwell, a Democrat, opposes the amendment.</a></p>" // no comma here
+},
+{ 
+    "q": "What is the target date set by the Environmental Protection Agency for reduction in carbon dioxide emissions from existing power plants?",
+    "a": [
+        {"option": "2020",      "correct": false},
+        {"option": "2025",     "correct": false},
+        {"option": "2030",      "correct": true},
+        {"option": "2035",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-carbon-dioxide-limits-could-mean-big-changes-coal-powered-states-missouri-illinois'>The state-by-state proposal to cut carbon dioxide emissions by the year 2030 sparked immediate debate over the impact, especially in states such as Missouri that depend heavily on coal. Veronique LaCapra reported that Missouri’s target rate for 2030 is higher than what many states are emitting now.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-carbon-dioxide-limits-could-mean-big-changes-coal-powered-states-missouri-illinois'>The state-by-state proposal to cut carbon dioxide emissions by the year 2030 sparked immediate debate over the impact, especially in states such as Missouri that depend heavily on coal. Veronique LaCapra reported that Missouri’s target rate for 2030 is higher than what many states are emitting now.</a></p>" // no comma here
+},
+{ 
+    "q": "Which member of the U.S. Senate blasted the proposed EPA regulations as an “unprecedented power grab”?",
+    "a": [
+        {"option": "Dick Durbin",      "correct": false},
+        {"option": "Mark Kirk",     "correct": false},
+        {"option": "Claire McCaskill",      "correct": false},
+        {"option": "Roy Blunt",     "correct": true} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/political-rancor-fear-display-debate-over-epas-coal-proposals'>Sen. Roy Blunt, R-Mo., co-sponsored a bill titled the “Coal Country Protection Act” after criticizing the EPA proposal.” Jo Mannies reported that his allies include every other Republican member of Congress from Missouri and Illinois.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/political-rancor-fear-display-debate-over-epas-coal-proposals'>Sen. Roy Blunt, R-Mo., co-sponsored a bill titled the “Coal Country Protection Act” after criticizing the EPA proposal.” Jo Mannies reported that his allies include every other Republican member of Congress from Missouri and Illinois.</a></p>" // no comma here
+},
+{ 
+    "q": "John R. Ashcroft, son of former Missouri Gov. and U.S. Sen. John Ashcroft, is running for what office?",
+    "a": [
+        {"option": "Missouri House",      "correct": false},
+        {"option": "Missouri Senate",     "correct": true},
+        {"option": "Attorney General",      "correct": false},
+        {"option": "St. Louis County Council",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/major-parties-bringing-top-guns-24th-district-senate-fight'>The younger Ashcroft has filed seeking the Republican nomination for the Missouri Senate in the 24th District. Jo Mannies reported that two other Republicans have filed to oppose Missouri Rep. Jill Schupp of Creve Coeur, who is unopposed for the Democratic nomination.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/major-parties-bringing-top-guns-24th-district-senate-fight'>The younger Ashcroft has filed seeking the Republican nomination for the Missouri Senate in the 24th District. Jo Mannies reported that two other Republicans have filed to oppose Missouri Rep. Jill Schupp of Creve Coeur, who is unopposed for the Democratic nomination.</a></p>" // no comma here
+},
+{ 
+    "q": "What was the latest issue to spark rancor at a meeting of the St. Louis County Council?",
+    "a": [
+        {"option": "The proposed transportation sales tax",      "correct": false},
+        {"option": "Minority and women participation in projects",     "correct": true},
+        {"option": "Money for county parks",      "correct": false},
+        {"option": "Emergency preparedness",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/election-year-tensions-boil-over-during-st-louis-county-council-meeting'>Council members engaged in sharp debate over a bill to require participation by minorities and women in county projects. Jason Rosenbaum reported that the friction was the latest sign of boiling election year tensions between County Executive Charlie Dooley and members of the council aligned with Councilman Steve Stenger, a fellow Democrat vying for county executive.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/election-year-tensions-boil-over-during-st-louis-county-council-meeting'>Council members engaged in sharp debate over a bill to require participation by minorities and women in county projects. Jason Rosenbaum reported that the friction was the latest sign of boiling election year tensions between County Executive Charlie Dooley and members of the council aligned with Councilman Steve Stenger, a fellow Democrat vying for county executive.</a></p>" // no comma here
+},
+{ 
+    "q": "Where was a recent match between the Bosnian National Soccer Team and a team from Ivory Coast played?",
+    "a": [
+        {"option": "Busch Stadium",      "correct": false},
+        {"option": "Edward Jones Dome",     "correct": true},
+        {"option": "Saint Louis University",      "correct": false},
+        {"option": "Soccer Park in Fenton",     "correct": false} // no comma here
+    ],
+    "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wake-flooding-bosnians-have-another-chance-cheer-national-team'>Bosnia defeated Ivory Coast 2-1 in the game at the Edward Jones Dome. Tim Lloyd reported that widespread destruction after recent flooding in Bosnia has rekindled memories of the war two decades ago.</a></p>",
+    "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wake-flooding-bosnians-have-another-chance-cheer-national-team'>Bosnia defeated Ivory Coast 2-1 in the game at the Edward Jones Dome. Tim Lloyd reported that widespread destruction after recent flooding in Bosnia has rekindled memories of the war two decades ago.</a></p>" // no comma here
+}
+]
+};

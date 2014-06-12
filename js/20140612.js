bf8036bf19c9f7e022f2bac28c605d54373fa79d
brentajones <bjones@bjones.net>
2014-06-12 17:25:40 -0500

    update for week

---
+++ js/20140612.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jun 13, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Archbishop Robert Carlson testified at a deposition involving alleged child sexual abuse because of his lengthy service as a priest in what state?",
+            "a": [
+                {"option": "Minnesota",      "correct": true},
+                {"option": "Wisconsin",     "correct": false},
+                {"option": "Michigan",      "correct": false},
+                {"option": "Iowa",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-archbishop-carlson-remembers-little-minnesota-abuse-scandal-deposition'>Carlson’s deposition from the lawsuit in Minnesota was released this week, with statements that that he wasn't sure whether he knew it was illegal for priests to have sex with children when he served as chancellor of the Twin Cities archdiocese in the 1980s. Bill Raack reported that a statement from the St. Louis Archdiocese clarified that “while not being able to recall his knowledge of the law exactly as it was many decades ago, the Archbishop did make clear that he knows child sex abuse is a crime today.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-archbishop-carlson-remembers-little-minnesota-abuse-scandal-deposition'>Carlson’s deposition from the lawsuit in Minnesota was released this week, with statements that that he wasn't sure whether he knew it was illegal for priests to have sex with children when he served as chancellor of the Twin Cities archdiocese in the 1980s. Bill Raack reported that a statement from the St. Louis Archdiocese clarified that “while not being able to recall his knowledge of the law exactly as it was many decades ago, the Archbishop did make clear that he knows child sex abuse is a crime today.”</a></p>" // no comma here
+        },
+        { 
+            "q": "What was the vote when the Missouri Supreme Court last June upheld the law allowing students to transfer from unaccredited schools?",
+            "a": [
+                {"option": "Unanimous",      "correct": true},
+                {"option": "6-1",     "correct": false},
+                {"option": "5-2",      "correct": false},
+                {"option": "4-3",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/one-year-later-no-end-sight-school-transfer-turmoil'>The judges unanimously upheld the law, setting in motion a scramble for students who wanted to transfer from unaccredited Normandy and Riverview Gardens. Dale Singer reported that a year later, the transfer program is still filled with uncertainty and unanswered questions.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/one-year-later-no-end-sight-school-transfer-turmoil'>The judges unanimously upheld the law, setting in motion a scramble for students who wanted to transfer from unaccredited Normandy and Riverview Gardens. Dale Singer reported that a year later, the transfer program is still filled with uncertainty and unanswered questions.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which member of the St. Louis County Council sponsored bills on minority participation that were shelved this week?",
+            "a": [
+                {"option": "Mike O’Mara",      "correct": false},
+                {"option": "Steve Stenger",     "correct": false},
+                {"option": "Pat Dolan",      "correct": false},
+                {"option": "Hazel Erby",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-council-shelves-minority-participation-bill'>Hazel Erby, a Democrat from University City, sponsors the bills that would set up hiring guidelines for minority and female workers on construction projects of $1 million or more. Jason Rosenbaum reported that she said she would delay considering them until she talked to other members of the council.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-council-shelves-minority-participation-bill'>Hazel Erby, a Democrat from University City, sponsors the bills that would set up hiring guidelines for minority and female workers on construction projects of $1 million or more. Jason Rosenbaum reported that she said she would delay considering them until she talked to other members of the council.</a></p>" // no comma here
+        },
+        { 
+            "q": "What kinds of projects make up the bulk of the proposed list of ways to spend increased transportation tax funds in the St. Louis area?",
+            "a": [
+                {"option": "Mass transit",      "correct": false},
+                {"option": "Bike trails",     "correct": false},
+                {"option": "Roads and bridges",      "correct": true},
+                {"option": "They all get about the same amount",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/analysis-roads-far-outweigh-bikes-transportation-tax-list'>Of the $1.5 billion in projects on the wish lists from local officials, roads and bridges take up the largest share by far. Jason Rosenbaum reported that the list for upgrades that could be funded if Missourians pass a .75 percent sales tax increase in August is likely to change before it is finalized by East-West Gateway later this month.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/analysis-roads-far-outweigh-bikes-transportation-tax-list'>Of the $1.5 billion in projects on the wish lists from local officials, roads and bridges take up the largest share by far. Jason Rosenbaum reported that the list for upgrades that could be funded if Missourians pass a .75 percent sales tax increase in August is likely to change before it is finalized by East-West Gateway later this month.</a></p>" // no comma here
+        },
+        { 
+            "q": "Sen. Claire McCaskill, D-Mo., said Dr. Mehmet Oz of TV fame would be part of a hearing next week on what topic?",
+            "a": [
+                {"option": "Problems with medical care for veterans",      "correct": false},
+                {"option": "Ineffective weight-loss products",     "correct": true},
+                {"option": "Health care on college campuses",      "correct": false},
+                {"option": "Delays in approval for new drugs",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dr-oz-join-mccaskill-hearing-weight-loss-scams'>McCaskill said Dr. Oz would be part of a hearing on ineffective weight-loss products. Jo Mannies reported that as chairman of the Senate Subcommittee on Consumer Protection, McCaskill says she has become increasing aware that millions of Americans annually fall victim to “weight-loss diet scams.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dr-oz-join-mccaskill-hearing-weight-loss-scams'>McCaskill said Dr. Oz would be part of a hearing on ineffective weight-loss products. Jo Mannies reported that as chairman of the Senate Subcommittee on Consumer Protection, McCaskill says she has become increasing aware that millions of Americans annually fall victim to “weight-loss diet scams.”</a></p>" // no comma here
+        },
+        { 
+            "q": "Who gave $100,000 to the Missouri Democratic Party last weekend?",
+            "a": [
+                {"option": "The Democratic National Party",      "correct": false},
+                {"option": "Claire McCaskill",     "correct": false},
+                {"option": "Bill Gates",      "correct": false},
+                {"option": "Chris Koster",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-takes-charge-he-gives-100000-missouri-democratic-party'>Attorney General Chris Koster announced the gift – his second $100,000 donation to the state Democratic party – shortly before the annual Jefferson-Jackson dinner in St. Louis. Jo Mannies reported that the donation made Koster, who is running for governor in 2016, the largest single donor to the state party.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-takes-charge-he-gives-100000-missouri-democratic-party'>Attorney General Chris Koster announced the gift – his second $100,000 donation to the state Democratic party – shortly before the annual Jefferson-Jackson dinner in St. Louis. Jo Mannies reported that the donation made Koster, who is running for governor in 2016, the largest single donor to the state party.</a></p>" // no comma here
+        },
+        { 
+            "q": "Articulated buses were introduced to St. Louis on what route on Monday?",
+            "a": [
+                {"option": "Grand",      "correct": true},
+                {"option": "Natural Bridge",     "correct": false},
+                {"option": "Washington",      "correct": false},
+                {"option": "Jefferson",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/metros-big-buses-roll-busy-grand-route-monday'>Metro introduced the longer, bendable buses that will carry larger passenger loads on the Grand line, which often is overcrowded. Jason Rosenbaum reported that the 60-foot buses should increase capacity by around 25 percent.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/metros-big-buses-roll-busy-grand-route-monday'>Metro introduced the longer, bendable buses that will carry larger passenger loads on the Grand line, which often is overcrowded. Jason Rosenbaum reported that the 60-foot buses should increase capacity by around 25 percent.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which member of the Zoo-Museum District board proposed adding African-American institutions to the district?",
+            "a": [
+                {"option": "Thelma Cook",      "correct": false},
+                {"option": "Robert Powell",     "correct": true},
+                {"option": "Ben Uchitelle",      "correct": false},
+                {"option": "Tom Campbell",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/zoo-museum-district-board-member-exploring-tax-dollars-african-american-institutions'>Robert Powell, who owns the Portfolio Gallery & Art Center in Grand Center, says the district should consider expanding to include other institutions such as black arts organizations as well as the St. Louis Symphony and the Transportation Museum. Nancy Fowler reported that a 2005 law would allow the expansion, subject to a popular vote.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/zoo-museum-district-board-member-exploring-tax-dollars-african-american-institutions'>Robert Powell, who owns the Portfolio Gallery & Art Center in Grand Center, says the district should consider expanding to include other institutions such as black arts organizations as well as the St. Louis Symphony and the Transportation Museum. Nancy Fowler reported that a 2005 law would allow the expansion, subject to a popular vote.</a></p>" // no comma here
+        },
+        { 
+            "q": "State Rep. Sue Allen, R-Town and Country, is urging Gov. Jay Nixon to preserve what kind of benefits for Medicaid recipients in next year's state budget?",
+            "a": [
+                {"option": "Neonatal",      "correct": false},
+                {"option": "Dental",     "correct": true},
+                {"option": "Physical therapy",      "correct": false},
+                {"option": "Speech therapy",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/lawmaker-calls-gov-nixon-preserve-dental-benefits-state-budget'>Allen is asking Nixon to keep dental benefits for Medicaid recipients in next year's state budget when he signs the $26.4 billion spending plan into law later this month. Marshall Griffin reported that Allen, chairs the committee that oversees budget writing for the three state agencies that handle Medicaid spending, urged Missourians to contact Nixon to ask that the dental benefits be retained.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/lawmaker-calls-gov-nixon-preserve-dental-benefits-state-budget'>Allen is asking Nixon to keep dental benefits for Medicaid recipients in next year's state budget when he signs the $26.4 billion spending plan into law later this month. Marshall Griffin reported that Allen, chairs the committee that oversees budget writing for the three state agencies that handle Medicaid spending, urged Missourians to contact Nixon to ask that the dental benefits be retained.</a></p>" // no comma here
+        },
+        { 
+            "q": "Rick Dildine is leaving his position as head of Shakespeare Festival St. Louis to take a similar job in what state?",
+            "a": [
+                {"option": "Maine",      "correct": false},
+                {"option": "Massachusetts",     "correct": true},
+                {"option": "New Hampshire",      "correct": false},
+                {"option": "Connecticut",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dildine-leaving-shakespeare-festival-st-louis'>Dildine, who has expanded the scope of Shakespeare Festival St. Louis in the five years that he has been in charge of it, will become executive director of Shakespeare & Company in Lenox, Mass. Donna Korando reported that during his tenure here, audiences have increased for the spring performances in Forest Park, but he also took the festival out of its wooded glen.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dildine-leaving-shakespeare-festival-st-louis'>Dildine, who has expanded the scope of Shakespeare Festival St. Louis in the five years that he has been in charge of it, will become executive director of Shakespeare & Company in Lenox, Mass. Donna Korando reported that during his tenure here, audiences have increased for the spring performances in Forest Park, but he also took the festival out of its wooded glen.</a></p>" // no comma here
+        }
+]
+};

c4d3f1283260c154d8a37707eb1b71a6b9a6f420
brentajones <bjones@bjones.net>
2014-06-19 17:37:38 -0500

    added quiz for week

---
+++ js/20140619.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jun 20, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Who will be in charge of operations at the new Normandy school district?",
+            "a": [
+                {"option": "A committee of three educators",      "correct": false},
+                {"option": "The current superintendent ",     "correct": true},
+                {"option": "A member of the state board of education",      "correct": false},
+                {"option": "It hasn’t been determined yet",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-school-board-limits-normandy-transfers-appoints-some-members-new-governing-panel'>Among the decisions made this past week by the state board of education about the new Normandy was the retention of Ty McNichols as superintendent, but he will be an at-will employee with no contract. Dale Singer reported that with all contracts lapsing at the end of June, hiring of teachers and others for the upcoming school year is continuing.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-school-board-limits-normandy-transfers-appoints-some-members-new-governing-panel'>Among the decisions made this past week by the state board of education about the new Normandy was the retention of Ty McNichols as superintendent, but he will be an at-will employee with no contract. Dale Singer reported that with all contracts lapsing at the end of June, hiring of teachers and others for the upcoming school year is continuing.</a></p>" // no comma here
+        },
+        { 
+            "q": "Wednesday’s death of John Winfield means Missouri has executed how many inmates in 2014?",
+            "a": [
+                {"option": "Four",      "correct": false},
+                {"option": "Five",     "correct": true},
+                {"option": "Six",      "correct": false},
+                {"option": "Seven",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-becomes-second-state-carry-out-execution-after-oklahomas-botched-affair'>Winfield was the fifth person put to death by lethal injection in Missouri this year. Chris McDaniel reported that the execution took place in spite of concerns over the state's secret drug supplier, as well as the state's likely intimidation of a correctional officer who supported clemency for the inmate.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-becomes-second-state-carry-out-execution-after-oklahomas-botched-affair'>Winfield was the fifth person put to death by lethal injection in Missouri this year. Chris McDaniel reported that the execution took place in spite of concerns over the state's secret drug supplier, as well as the state's likely intimidation of a correctional officer who supported clemency for the inmate.</a></p>" // no comma here
+        },
+        { 
+            "q": "How will a long-dormant piece of property on Delmar owned by the Missouri History Museum be used?",
+            "a": [
+                {"option": "Community center",      "correct": false},
+                {"option": "Parking lot",     "correct": false},
+                {"option": "Storage for Loop Trolley",      "correct": true},
+                {"option": "Green space",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/history-museum-s-delmar-property-support-loop-trolley'>The museum’s board voted to lease the land at 5863 Delmar to St. Louis’ Loop Trolley Development District. Nancy Fowler reported that the site, whose purchase contributed to the departure of former museum president Robert Archibald, will be used to store materials and equipment for the Trolley project, at a cost of $10 a month for two years.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/history-museum-s-delmar-property-support-loop-trolley'>The museum’s board voted to lease the land at 5863 Delmar to St. Louis’ Loop Trolley Development District. Nancy Fowler reported that the site, whose purchase contributed to the departure of former museum president Robert Archibald, will be used to store materials and equipment for the Trolley project, at a cost of $10 a month for two years.</a></p>" // no comma here
+        },
+        { 
+            "q": "Who spent the most on lobbying Missouri public officials during the most recent 24 months on record?",
+            "a": [
+                {"option": "Ameren",      "correct": true},
+                {"option": "Missouri Hospital Association",     "correct": false},
+                {"option": "Missouri Beverage Association",      "correct": false},
+                {"option": "Missouri Cable Telecommunications Association",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://tinyurl.com/nettv4g'>Records show that Ameren spent $91,649 on lobbying in the 24-month period, according to the Lobbying Missouri website run jointly by NPR and St. Louis Public Radio. Chris McDaniel reported that in the first four months of this year, lawmakers accepted $558,813.68 worth of gifts, about on par with past years.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://tinyurl.com/nettv4g'>Records show that Ameren spent $91,649 on lobbying in the 24-month period, according to the Lobbying Missouri website run jointly by NPR and St. Louis Public Radio. Chris McDaniel reported that in the first four months of this year, lawmakers accepted $558,813.68 worth of gifts, about on par with past years.</a></p>" // no comma here
+        },
+        { 
+            "q": "By what date do Missouri officials expect to pay the rest of this year’s income tax refunds?",
+            "a": [
+                {"option": "End of June",      "correct": true},
+                {"option": "End of July",     "correct": false},
+                {"option": "End of August",      "correct": false},
+                {"option": "Labor Day",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-has-yet-pay-out-113-million-state-tax-refunds'>Thousands of Missourians are still waiting for state income tax refunds, nearly two months after the April 15 filing deadline, but state officials say they should have all of the checks out by the end of June. Marshall Griffin reported that the delay can be partially blamed on Missouri's revenue collections in May, which were 3 percent, or nearly $20 million, below what they were a year ago at this time.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-has-yet-pay-out-113-million-state-tax-refunds'>Thousands of Missourians are still waiting for state income tax refunds, nearly two months after the April 15 filing deadline, but state officials say they should have all of the checks out by the end of June. Marshall Griffin reported that the delay can be partially blamed on Missouri's revenue collections in May, which were 3 percent, or nearly $20 million, below what they were a year ago at this time.</a></p>" // no comma here
+        },
+        { 
+            "q": "The state of Missouri faces a federal lawsuit over licensing for what treatment?",
+            "a": [
+                {"option": "Hair coloring",      "correct": false},
+                {"option": "Hair braiding",     "correct": true},
+                {"option": "Hair cutting",      "correct": false},
+                {"option": "Hair waxing",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-faces-lawsuit-over-its-licensing-requirement-hair-braiding'>The Virginia-based Institute for Justice, a libertarian advocacy group, brought the suit challenging the constitutionality of Missouri’s requirement that African-style hair braiders to get cosmetology licenses. Rachel Lippmann reported that the action was filed on behalf of the owners of two braiding shops in St. Louis — neither of whom has a cosmetology license. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-faces-lawsuit-over-its-licensing-requirement-hair-braiding'>The Virginia-based Institute for Justice, a libertarian advocacy group, brought the suit challenging the constitutionality of Missouri’s requirement that African-style hair braiders to get cosmetology licenses. Rachel Lippmann reported that the action was filed on behalf of the owners of two braiding shops in St. Louis — neither of whom has a cosmetology license. </a></p>" // no comma here
+        },
+        { 
+            "q": "Approval for tax credits to help cover the cost of renovating a museum on the grounds of the Gateway Arch came this week from which state agency?",
+            "a": [
+                {"option": "Office of Administration",      "correct": false},
+                {"option": "Missouri Development Finance Board",     "correct": true},
+                {"option": "Department of Revenue",      "correct": false},
+                {"option": "All three have approved the deal",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-development-finance-board-approves-15-million-tax-credits-museum-under-gateway-arch'>The Missouri Development Finance Board voted for up to $15 million in incentives, which would be used to cover half the cost of private donations that total around $30 million. Marshall Griffin reported that the deal also needs approval from the Office of Administration and the Department of Revenue, which have not acted yet.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-development-finance-board-approves-15-million-tax-credits-museum-under-gateway-arch'>The Missouri Development Finance Board voted for up to $15 million in incentives, which would be used to cover half the cost of private donations that total around $30 million. Marshall Griffin reported that the deal also needs approval from the Office of Administration and the Department of Revenue, which have not acted yet.</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the name of the firm that will locate at the Danforth Plant Science Center in Creve Coeur?",
+            "a": [
+                {"option": "KLM",      "correct": false},
+                {"option": "KWS",     "correct": true},
+                {"option": "KFC",      "correct": false},
+                {"option": "KWK",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/european-big-ag-company-chooses-st-louis'>KWS, a German agricultural company, is opening a research center at BRDG Park in the Donald Danforth Plant Science Center’s campus. Maria Altman reported that KWS chose St. Louis over many other cities including North Carolina’s Research Triangle Park, where several major seed companies have a presence.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/european-big-ag-company-chooses-st-louis'>KWS, a German agricultural company, is opening a research center at BRDG Park in the Donald Danforth Plant Science Center’s campus. Maria Altman reported that KWS chose St. Louis over many other cities including North Carolina’s Research Triangle Park, where several major seed companies have a presence.</a></p>" // no comma here
+        },
+        { 
+            "q": "The latest book by St. Louis author Ridley Pearson has what title?",
+            "a": [
+                {"option": "The Blue Room",      "correct": false},
+                {"option": "The Green Room",     "correct": false},
+                {"option": "The Red Room",      "correct": true},
+                {"option": "The Rainbow Room",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-author-ridley-pearson-poaching-publishing-and-his-new-book-red-room'>Pearson’s novel “The Red Room” is the third book in his Risk Agent Series. Camille Phillips reported that his protagonists enter the world of the illegal art trade and in the process encounter an Iranian nuclear physicist who has become a target of the Mossad, Israel’s intelligence agency.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-author-ridley-pearson-poaching-publishing-and-his-new-book-red-room'>Pearson’s novel “The Red Room” is the third book in his Risk Agent Series. Camille Phillips reported that his protagonists enter the world of the illegal art trade and in the process encounter an Iranian nuclear physicist who has become a target of the Mossad, Israel’s intelligence agency.</a></p>" // no comma here
+        },
+        { 
+            "q": "In the opera “27,” being presented by Opera Theatre of St. Louis, what does the number in the title stand for?",
+            "a": [
+                {"option": "The year – 1927 – when the action takes place",      "correct": false},
+                {"option": "The Paris address of the principal characters",     "correct": true},
+                {"option": "The number of characters in the production",      "correct": false},
+                {"option": "It doesn’t stand for anything",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/reflection-27-opera-theatre-again-uses-powerful-art-challenge'>The title of the work by composer Ricky Ian Gordon and librettist Royce Vavrek  refers to the address of the Paris apartment of Gertrude Stein and Alice B. Toklas, at 27, rue de Fleurus. Robert W. Duffy says the production focuses the spotlight of truth on a situation of extraordinary moral complexity.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/reflection-27-opera-theatre-again-uses-powerful-art-challenge'>The title of the work by composer Ricky Ian Gordon and librettist Royce Vavrek  refers to the address of the Paris apartment of Gertrude Stein and Alice B. Toklas, at 27, rue de Fleurus. Robert W. Duffy says the production focuses the spotlight of truth on a situation of extraordinary moral complexity.</a></p>" // no comma here
+        }	
+]
+};

aee55ad59b874bb96314e8e4ff4a3a715944406d
brentajones <bjones@bjones.net>
2014-06-26 17:53:56 -0500

    update for week

---
+++ js/20140626.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jun 27, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "How much money did Gov. Jay Nixon temporarily withhold from the Missouri state budget?",
+            "a": [
+                {"option": "$1.1 billion",      "correct": false},
+                {"option": "$846 million",     "correct": true},
+                {"option": "$276 million",      "correct": false},
+                {"option": "None – all of his cuts were in line-item vetoes, not temporary withholding",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-cuts-more-1-billion-fy2015-state-budget'>Nixon announced Tuesday that he had cut $1.1 billion from the budget, including nearly $276 million in line-item vetoes and $846 million in temporary withholds. Marshall Griffin reported that the withholdings could be released by the governor at a later date depending on what happens to his vetoes of tax breaks and other factors.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-cuts-more-1-billion-fy2015-state-budget'>Nixon announced Tuesday that he had cut $1.1 billion from the budget, including nearly $276 million in line-item vetoes and $846 million in temporary withholds. Marshall Griffin reported that the withholdings could be released by the governor at a later date depending on what happens to his vetoes of tax breaks and other factors.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which was NOT listed by Gov. Jay Nixon as a reason he vetoed a wide-ranging school transfer bill?",
+            "a": [
+                {"option": "It cost too much money",      "correct": true},
+                {"option": "It allowed use of public funds for private schools",     "correct": false},
+                {"option": "It cut off transportation for transfer students",      "correct": false},
+                {"option": "It let receiving districts not count test scores for transfer students",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-vetoes-school-transfer-bill-citing-public-money-private-schools-and-other-concerns'>In detailing his reasons for the veto, Nixon cited the private school option, the lack of transportation for transfer students and the tradeoff that would let receiving districts that cut their tuition to not count the test scores for transfer students in their own state evaluation. Dale Singer reported that Nixon also criticized Francis Howell for deciding not to take any transfer students in the coming school year.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-vetoes-school-transfer-bill-citing-public-money-private-schools-and-other-concerns'>In detailing his reasons for the veto, Nixon cited the private school option, the lack of transportation for transfer students and the tradeoff that would let receiving districts that cut their tuition to not count the test scores for transfer students in their own state evaluation. Dale Singer reported that Nixon also criticized Francis Howell for deciding not to take any transfer students in the coming school year.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri Supreme Court agreed to hear a case involving red light cameras in what city?",
+            "a": [
+                {"option": "Kansas City",      "correct": false},
+                {"option": "St. Louis",     "correct": true},
+                {"option": "Branson",      "correct": false},
+                {"option": "Columbia",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-supreme-court-hear-challenge-st-louis-red-light-cameras'>The state Supreme Court agreed to hear the case brought by two women from St. Louis. Rachel Lippmann reported that the court previously has declined to hear seven other challenges to red light camera laws, all of which raised questions that were similar to those in the St. Louis case. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-supreme-court-hear-challenge-st-louis-red-light-cameras'>The state Supreme Court agreed to hear the case brought by two women from St. Louis. Rachel Lippmann reported that the court previously has declined to hear seven other challenges to red light camera laws, all of which raised questions that were similar to those in the St. Louis case. </a></p>" // no comma here
+        },
+        { 
+            "q": "What is one of the major goals of the St. Louis Mosaic Project?",
+            "a": [
+                {"option": "Expanding the Grand Center arts district",      "correct": false},
+                {"option": "Providing grants for local visual artists",     "correct": false},
+                {"option": "Retaining international students from local universities",      "correct": true},
+                {"option": "Fostering better relationships among area immigrants",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-universities-collaborate-keep-international-students-region'>The St. Louis Mosaic Project includes five universities that are trying to help foreign students find work at local employers, which could ultimately help sponsor them through the immigration process. Tim Lloyd reported that it was launched the same day a comprehensive immigration reform package cleared the U.S. Senate; it has now stalled in Congress.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-universities-collaborate-keep-international-students-region'>The St. Louis Mosaic Project includes five universities that are trying to help foreign students find work at local employers, which could ultimately help sponsor them through the immigration process. Tim Lloyd reported that it was launched the same day a comprehensive immigration reform package cleared the U.S. Senate; it has now stalled in Congress.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these metropolitan areas had stronger job growth than St. Louis in the years 2010-13?",
+            "a": [
+                {"option": "Kansas City",      "correct": false},
+                {"option": "Detroit",     "correct": false},
+                {"option": "Cleveland",      "correct": false},
+                {"option": "All of the above",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-job-growth-slow-compared-other-cities'>Figures from the Bureau of Labor Statistics showed that the St. Louis area’s job growth in the period was just 1.8 percent. Maria Altman reported that Kansas City had nearly double the growth with 3.5 percent, Detroit and Cleveland also did better than St. Louis, and the national average was 4.7 percent.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-job-growth-slow-compared-other-cities'>Figures from the Bureau of Labor Statistics showed that the St. Louis area’s job growth in the period was just 1.8 percent. Maria Altman reported that Kansas City had nearly double the growth with 3.5 percent, Detroit and Cleveland also did better than St. Louis, and the national average was 4.7 percent.</a></p>" // no comma here
+        },
+        { 
+            "q": "What was the vote in the U.S. Supreme Court’s ruling that authorities need warrants to conduct cell phone searches.",
+            "a": [
+                {"option": "Unanimous",      "correct": true},
+                {"option": "8-1",     "correct": false},
+                {"option": "6-3",      "correct": false},
+                {"option": "5-4",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/us-supreme-court-rules-authorities-need-warrants-cell-phone-searches'>In a ruling written by Chief Justice John Roberts, the court ruled unanimously that police almost always need to get warrants to search the cell phones of people they arrest. William Freivogel reported that the cell phone decision, coupled with one two years ago limiting the use of GPS data to track criminal suspects, demonstrates that the court is responding to threats that modern communications technology pose to privacy.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/us-supreme-court-rules-authorities-need-warrants-cell-phone-searches'>In a ruling written by Chief Justice John Roberts, the court ruled unanimously that police almost always need to get warrants to search the cell phones of people they arrest. William Freivogel reported that the cell phone decision, coupled with one two years ago limiting the use of GPS data to track criminal suspects, demonstrates that the court is responding to threats that modern communications technology pose to privacy.</a></p>" // no comma here
+        },
+        { 
+            "q": "How many square feet will the Ikea store in the Cortex district have when it opens next year?",
+            "a": [
+                {"option": "260,000",      "correct": false},
+                {"option": "380,000",     "correct": true},
+                {"option": "420,000",      "correct": false},
+                {"option": "510,000",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ikea-groundbreaking-regional-leaders-hail-swedish-retailers-economic-promise'>After this week’s official groundbreaking, the 380,000-square-foot store is scheduled to open in the fall of 2015 in the Cortex Innovation District in St. Louis’ central corridor. Jason Rosenbaum reported that it's the Swedish retailer’s first store in the state of Missouri. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ikea-groundbreaking-regional-leaders-hail-swedish-retailers-economic-promise'>After this week’s official groundbreaking, the 380,000-square-foot store is scheduled to open in the fall of 2015 in the Cortex Innovation District in St. Louis’ central corridor. Jason Rosenbaum reported that it's the Swedish retailer’s first store in the state of Missouri. </a></p>" // no comma here
+        },
+        { 
+            "q": "A former launch area for a Nike missile base that is now up for sale is located in what Southern Illinois County?",
+            "a": [
+                {"option": "Macoupin",      "correct": false},
+                {"option": "Madison",     "correct": false},
+                {"option": "Monroe",      "correct": true},
+                {"option": "St. Clair",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nike-missile-launch-site-southern-illinois-can-be-yours-highest-bid'>The 14-acre site, which will be auctioned on July 12, is located on a hill in Monroe County near Hecker, though it has a Red Bud address. Mary Delach Leonard reported that the relic of the Cold War hasn’t seen a missile since it was deactivated in January 1969.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nike-missile-launch-site-southern-illinois-can-be-yours-highest-bid'>The 14-acre site, which will be auctioned on July 12, is located on a hill in Monroe County near Hecker, though it has a Red Bud address. Mary Delach Leonard reported that the relic of the Cold War hasn’t seen a missile since it was deactivated in January 1969.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Missouri Coalition for the Environment filed suit to block the Public Service Commission from ending what program?",
+            "a": [
+                {"option": "Tax credits for wind energy",      "correct": false},
+                {"option": "Keeping power on for low-income families",     "correct": false},
+                {"option": "Rebate for installation of solar panels",      "correct": true},
+                {"option": "Lower rates for high-energy users",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/lawsuit-seeks-save-solar-rebates'>The coalition wants to block a request by Ameren and Kansas City Power & Light to end Missouri’s solar rebate program. Maria Altman reported that even if the program remains on the books, both utility companies say they could no longer pay rebates because they had reached a 1 percent cap laid out in the rules for the voter-approved program.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/lawsuit-seeks-save-solar-rebates'>The coalition wants to block a request by Ameren and Kansas City Power & Light to end Missouri’s solar rebate program. Maria Altman reported that even if the program remains on the books, both utility companies say they could no longer pay rebates because they had reached a 1 percent cap laid out in the rules for the voter-approved program.</a></p>" // no comma here
+        },
+        { 
+            "q": "The former site of a north St. Louis grocery store, known as Tillie’s Corner, could be designated a landmark. What neighborhood is it in?",
+            "a": [
+                {"option": "St. Louis Place",      "correct": false},
+                {"option": "Jeff-Vander-Lou",     "correct": true},
+                {"option": "The Ville",      "correct": false},
+                {"option": "Carr Square",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/city-landmark-status-sought-tillies-corner'>In 2012, a windstorm blew down buildings at Tillie’s Corner at Garrison and Sheridan in the Jeff-Vander-Lou neighborhood, where Lillie V. \"Granny\" Pearson had operated a grocery store called Tillie's Food Stop from 1948 to 1988. Rachel Lippmann reported that Pearson’s granddaughter is working to have the corner site designated as a city landmark.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/city-landmark-status-sought-tillies-corner'>In 2012, a windstorm blew down buildings at Tillie’s Corner at Garrison and Sheridan in the Jeff-Vander-Lou neighborhood, where Lillie V. \"Granny\" Pearson had operated a grocery store called Tillie's Food Stop from 1948 to 1988. Rachel Lippmann reported that Pearson’s granddaughter is working to have the corner site designated as a city landmark.</a></p>" // no comma here
+        }
+]
+};

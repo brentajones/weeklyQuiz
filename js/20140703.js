6844bf9c985df9c524b41cc427426f25bc6b65cb
brentajones <bjones@bjones.net>
2014-07-03 17:21:19 -0500

    update for week

---
+++ js/20140703.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jul 4, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Which member of the elected Normandy school board was named to the board of the new Normandy Schools Collaborative?",
+            "a": [
+                {"option": "William Humphrey",      "correct": false},
+                {"option": "Sheila Williams",     "correct": true},
+                {"option": "Nancy Hartman",      "correct": false},
+                {"option": "Terry Artis",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-normandy-school-board-completed'>Sheila Williams, who was vice president of the elected Normandy board, is one of five people named to the appointed board of the Normandy Schools Collaborative. Dale Singer reported that Williams retired from the Normandy schools in 2008 after serving as its executive director of school programs. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-normandy-school-board-completed'>Sheila Williams, who was vice president of the elected Normandy board, is one of five people named to the appointed board of the Normandy Schools Collaborative. Dale Singer reported that Williams retired from the Normandy schools in 2008 after serving as its executive director of school programs. </a></p>" // no comma here
+        },
+        { 
+            "q": "Which member of the U.S. Supreme Court wrote the Hobby Lobby opinion?",
+            "a": [
+                {"option": "John Roberts",      "correct": false},
+                {"option": "Anthony Kennedy",     "correct": false},
+                {"option": "Antonin Scalia",      "correct": false},
+                {"option": "Samuel Alito",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/hobby-lobby-decision-says-corporations-have-religious-freedom-local-cases-seek-more'>Justice Samuel Alito wrote the opinion, which won a 5-4 majority. William Freivogel reported the decision extends the protections of the Religious Freedom Restoration Act to closely held corporations that object on religious grounds to certain contraceptives. It affects about a dozen lawsuits in Missouri and Illinois and about 70 nationally.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/hobby-lobby-decision-says-corporations-have-religious-freedom-local-cases-seek-more'>Justice Samuel Alito wrote the opinion, which won a 5-4 majority. William Freivogel reported the decision extends the protections of the Religious Freedom Restoration Act to closely held corporations that object on religious grounds to certain contraceptives. It affects about a dozen lawsuits in Missouri and Illinois and about 70 nationally.</a></p>" // no comma here
+        },
+        { 
+            "q": "An Illinois case decided by the U.S. Supreme Court involved what kind of worker who did not want to have to pay union dues?",
+            "a": [
+                {"option": "Teacher",      "correct": false},
+                {"option": "Firefighter",     "correct": false},
+                {"option": "Home health care",      "correct": true},
+                {"option": "Trash hauler",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/case-striking-illinois-requirement-home-health-workers-pay-dues-may-only-apply-state'>The court ruled in favor of Pamela Harris, whose son’s disabilities required that he have around-the-clock care. William Freivogel reported that Harris became the child’s home health worker and she objected to having to pay union dues she thought reduced the amount of money she had to care for her child.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/case-striking-illinois-requirement-home-health-workers-pay-dues-may-only-apply-state'>The court ruled in favor of Pamela Harris, whose son’s disabilities required that he have around-the-clock care. William Freivogel reported that Harris became the child’s home health worker and she objected to having to pay union dues she thought reduced the amount of money she had to care for her child.</a></p>" // no comma here
+        },
+        { 
+            "q": "If it passes, how much is the 0.75 percent transportation sales tax, on Missouri’s ballot in August, expected to bring in each year?",
+            "a": [
+                {"option": "$335 million",      "correct": false},
+                {"option": "$438 million",     "correct": false},
+                {"option": "$539 million",      "correct": true},
+                {"option": "$637 million",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/five-things-you-need-know-about-transportation-tax'>The sales tax increase is expected to generate $539 million annually for 10 years. Jason Rosenbaum reported that about 10 percent of the funds would go directly to cities and counties, which have the power to decide on their own transportation priorities.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/five-things-you-need-know-about-transportation-tax'>The sales tax increase is expected to generate $539 million annually for 10 years. Jason Rosenbaum reported that about 10 percent of the funds would go directly to cities and counties, which have the power to decide on their own transportation priorities.</a></p>" // no comma here
+        },
+        { 
+            "q": "State Sen. Eric Schmitt, R-Glendale, announced he would seek what office in 2016?",
+            "a": [
+                {"option": "Lieutenant governor",      "correct": false},
+                {"option": "Treasurer",     "correct": true},
+                {"option": "Attorney general",      "correct": false},
+                {"option": "No office – he’s dropping out of politics.",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-state-senator-announces-gop-bid-missouri-treasurer-2016'>Schmitt changed his expected political plans by announcing Wednesday that he’s running for Missouri state treasurer in 2016. Jo Mannies reported that he had long been considered a likely candidate for attorney general and also had been openly courted to run for lieutenant governor.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/area-state-senator-announces-gop-bid-missouri-treasurer-2016'>Schmitt changed his expected political plans by announcing Wednesday that he’s running for Missouri state treasurer in 2016. Jo Mannies reported that he had long been considered a likely candidate for attorney general and also had been openly courted to run for lieutenant governor.</a></p>" // no comma here
+        },
+        { 
+            "q": "The new president of the Missouri AFL-CIO, Mike Louis, is a member of what union?",
+            "a": [
+                {"option": "Autoworkers",      "correct": false},
+                {"option": "Machinists",     "correct": true},
+                {"option": "Teamsters",      "correct": false},
+                {"option": "Postal workers",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-afl-cio-chooses-new-leaders-both-st-louis-area'>Mike Louis has been a member of the Machinists Union since 1973. Jo Mannies reported that since 2004 he has been president of the Machinists’ state council, which is the union’s political arm.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-afl-cio-chooses-new-leaders-both-st-louis-area'>Mike Louis has been a member of the Machinists Union since 1973. Jo Mannies reported that since 2004 he has been president of the Machinists’ state council, which is the union’s political arm.</a></p>" // no comma here
+        },
+        { 
+            "q": "According to a new study, Missouri spends a greater share of its academic research money than any other state in the nation on what discipline?",
+            "a": [
+                {"option": "Physics",      "correct": false},
+                {"option": "Chemistry",     "correct": false},
+                {"option": "Bioscience",      "correct": true},
+                {"option": "Political science",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-missouri-spends-lions-share-research-funds-bioscience'>A report from Battelle and the Biotechnology Industry Organization said Missouri spends a greater share of its academic research money on biosciences than any other state in the nation. Joseph Leahy reported that in 2012, the state’s research universities devoted 85 percent, or about $895 million dollars, to academic bioscience research, compared with 61 percent for the national average.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-missouri-spends-lions-share-research-funds-bioscience'>A report from Battelle and the Biotechnology Industry Organization said Missouri spends a greater share of its academic research money on biosciences than any other state in the nation. Joseph Leahy reported that in 2012, the state’s research universities devoted 85 percent, or about $895 million dollars, to academic bioscience research, compared with 61 percent for the national average.</a></p>" // no comma here
+        },
+        { 
+            "q": "Horitculturists Cindy Gilberg, who died this week, was the longtime owner of a nursery in which community?",
+            "a": [
+                {"option": "Augusta",      "correct": false},
+                {"option": "Wildwood",     "correct": true},
+                {"option": "Pacific",      "correct": false},
+                {"option": "Eureka",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/cindy-gilberg-horticulturalist-helped-change-way-st-louisans-plant-gardens'>Gilberg and her husband opened Gilberg Perennial Farms in Wildwood in 1979. Gloria Ross reported that as the area become more suburban and developed, they closed the business in 2006 and she turned her full attention to horticultural consulting, teaching and writing.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/cindy-gilberg-horticulturalist-helped-change-way-st-louisans-plant-gardens'>Gilberg and her husband opened Gilberg Perennial Farms in Wildwood in 1979. Gloria Ross reported that as the area become more suburban and developed, they closed the business in 2006 and she turned her full attention to horticultural consulting, teaching and writing.</a></p>" // no comma here
+        },
+        { 
+            "q": "The NPR show Wait Wait , Don’t Tell Me said this about a famous local delicacy: “If they'd had these in the 1810s, Lewis and Clark never would have left St. Louis.” What were they talking about?",
+            "a": [
+                {"option": "Brain sandwiches",      "correct": false},
+                {"option": "Pappy’s rib tips",     "correct": false},
+                {"option": "Ted Drewes concretes",      "correct": true},
+                {"option": "Toasted ravioli",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sandwich-monday-concrete'>The Sandwich Monday blog post featured Ted Drewes concretes, where the discoverers of the St. Louis treat marveled that you could turn them upside down and the custard wouldn’t fall out of the cup. Another wag put it this way:  I'd like to be a victim of the mob that makes its cement shoes out of Ted Drewes' Concrete.”</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sandwich-monday-concrete'>The Sandwich Monday blog post featured Ted Drewes concretes, where the discoverers of the St. Louis treat marveled that you could turn them upside down and the custard wouldn’t fall out of the cup. Another wag put it this way:  I'd like to be a victim of the mob that makes its cement shoes out of Ted Drewes' Concrete.”</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the formal name of the foul-smelling corpse flower, which bloomed this week at the Missouri Botanical Garden?",
+            "a": [
+                {"option": "Rafflesia arnoldii",      "correct": false},
+                {"option": "Hydnora africana",     "correct": false},
+                {"option": "Bulbophyllum phalaenopsis",      "correct": false},
+                {"option": "Titan arum",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stopping-smell-corpse-flower-botanical-garden'>Though all four flowers have been ranked among the rankest-smelling blooms around, the corpse flower that bloomed at MoBot this week is the Titan arum, known as Izzy. Alan Greenblatt reported that the plant expends a great deal of energy when in bloom, reaching a temperature of 99 degrees -- hotter than healthy humans.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stopping-smell-corpse-flower-botanical-garden'>Though all four flowers have been ranked among the rankest-smelling blooms around, the corpse flower that bloomed at MoBot this week is the Titan arum, known as Izzy. Alan Greenblatt reported that the plant expends a great deal of energy when in bloom, reaching a temperature of 99 degrees -- hotter than healthy humans.</a></p>" // no comma here
+        }
+]
+};

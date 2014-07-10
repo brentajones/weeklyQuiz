bc144a68f6a220118348942bdc79c8f46756619b
brentajones <bjones@bjones.net>
2014-07-10 17:19:34 -0500

    update for week

---
+++ js/20140710.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jul 11, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "In the abuse case that was settled this week by the Archdiocese of St. Louis, in what parish did the alleged assault take place?",
+            "a": [
+                {"option": "St. Ambrose",      "correct": false},
+                {"option": "St. Cecilia",     "correct": false},
+                {"option": "St. Cronan",      "correct": true},
+                {"option": "St. John the Baptist",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> ",
+            "incorrect": "<p><span>Incorrect.</span></p> " // no comma here
+        },
+        { 
+            "q": "Which of these is NOT a potential site for the relocation of the National Geospatial-Intelligence Agency?",
+            "a": [
+                {"option": "The old Pruitt-Igoe site",      "correct": false},
+                {"option": "Near Scott Air Force Base",     "correct": false},
+                {"option": "Weldon Spring",      "correct": false},
+                {"option": "Downtown",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/department-defense-mapping-agency-announces-six-possible-sites-new-facility'>The agency, which employs about 3,000 people at its current location at 3200 South 2nd St. and Arsenal Street in the city, released a list of six potential sites for its move, but downtown was not among them. Maria Altman reported once a site is chosen, the agency expects to move into the new facility in 2021.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/department-defense-mapping-agency-announces-six-possible-sites-new-facility'>The agency, which employs about 3,000 people at its current location at 3200 South 2nd St. and Arsenal Street in the city, released a list of six potential sites for its move, but downtown was not among them. Maria Altman reported once a site is chosen, the agency expects to move into the new facility in 2021.</a></p>" // no comma here
+        },
+        { 
+            "q": "What is the name of the new Missouri law designed to protect religious expression in public schools?",
+            "a": [
+                {"option": "Free Expression For Religious Liberty Act",      "correct": false},
+                {"option": "Missouri Student Religious Liberties Act",     "correct": true},
+                {"option": "Religious Freedom In Schools Act",      "correct": false},
+                {"option": "Student/Parent Religious Expression Act",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-law-protects-religious-expression-missouris-public-schools'>The Missouri Student Religious Liberties Act, signed into law by Gov. Jay Nixon, must treat voluntary expression of religious views the same way views on other subjects are treated. Dale Singer reported that some observers think the law could actually limit student expression because school districts would have less control to monitor what students say and might provide fewer opportunities for such expression.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/new-law-protects-religious-expression-missouris-public-schools'>The Missouri Student Religious Liberties Act, signed into law by Gov. Jay Nixon, must treat voluntary expression of religious views the same way views on other subjects are treated. Dale Singer reported that some observers think the law could actually limit student expression because school districts would have less control to monitor what students say and might provide fewer opportunities for such expression.</a></p>" // no comma here
+        },
+        { 
+            "q": "For every can of food donated to the St. Louis Public Library, how much in library fines will be forgiven?",
+            "a": [
+                {"option": "25 cents",      "correct": false},
+                {"option": "50 cents",     "correct": false},
+                {"option": "75 cents",      "correct": false},
+                {"option": "1 dollar",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/overdue-books-pay-your-fine-food'>The Food for Fines program, which began in 2008, lets borrowers save $1 in fines for each can of food donated, up to a maximum of $25. Library scofflaw Maria Altman reported that last year, library users and staff donated enough food to provide 11,000 meals, and the library hopes to top that this year.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/overdue-books-pay-your-fine-food'>The Food for Fines program, which began in 2008, lets borrowers save $1 in fines for each can of food donated, up to a maximum of $25. Library scofflaw Maria Altman reported that last year, library users and staff donated enough food to provide 11,000 meals, and the library hopes to top that this year.</a></p>" // no comma here
+        },
+        { 
+            "q": "Everest College in St. Louis County will be closing its doors soon. Where is its campus located?",
+            "a": [
+                {"option": "Westport",      "correct": false},
+                {"option": "South County",     "correct": false},
+                {"option": "Earth City",      "correct": true},
+                {"option": "Wildwood",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/profit-everest-college-closing-earth-city'>Everest College’s campus in Earth City is one of a dozen nationwide that will be closing as part of a federal settlement with its parent company, Corinthian Colleges. Dale Singer reported that Everest said students currently enrolled at the school will be allowed to complete their studies.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/profit-everest-college-closing-earth-city'>Everest College’s campus in Earth City is one of a dozen nationwide that will be closing as part of a federal settlement with its parent company, Corinthian Colleges. Dale Singer reported that Everest said students currently enrolled at the school will be allowed to complete their studies.</a></p>" // no comma here
+        },
+        { 
+            "q": "The University of Missouri is expanding an early alert system that tracks students’ academic performance after a successful pilot program at which of its campuses?",
+            "a": [
+                {"option": "Columbia",      "correct": true},
+                {"option": "Kansas City",     "correct": false},
+                {"option": "Rolla",      "correct": false},
+                {"option": "St. Louis",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/um-system-expands-academic-tracking-platform-boost-retention-and-graduation-rates'>The system, which is designed to improve retention and graduation rates by better connecting students, faculty and staff, had its pilot program at the Columbia campus. Joseph Leahy reported that it gives advisers real-time grading information on students and tracks performance trends among classes and subjects. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/um-system-expands-academic-tracking-platform-boost-retention-and-graduation-rates'>The system, which is designed to improve retention and graduation rates by better connecting students, faculty and staff, had its pilot program at the Columbia campus. Joseph Leahy reported that it gives advisers real-time grading information on students and tracks performance trends among classes and subjects. </a></p>" // no comma here
+        },
+        { 
+            "q": "What positions on a city-county merger have been taken by St. Louis County Executive Charlie Dooley and his Democratic challenger, Steve Stenger?",
+            "a": [
+                {"option": "Dooley for, Stenger against",      "correct": false},
+                {"option": "Stenger for, Dooley against",     "correct": false},
+                {"option": "Both against",      "correct": false},
+                {"option": "Both have been non-committal so far",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-executive-candidates-shy-away-backing-city-county-union'>Neither Dooley nor Stenger has taken a firm position on a city-county merger so far; instead, they say they will wait and see what kind of formal proposal is presented. Jason Rosenbaum reported that the two GOP candidates for county executive, Rick Stream and  Tony Pousosa, are taking a much dimmer view of a possible merger. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-county-executive-candidates-shy-away-backing-city-county-union'>Neither Dooley nor Stenger has taken a firm position on a city-county merger so far; instead, they say they will wait and see what kind of formal proposal is presented. Jason Rosenbaum reported that the two GOP candidates for county executive, Rick Stream and  Tony Pousosa, are taking a much dimmer view of a possible merger. </a></p>" // no comma here
+        },
+        { 
+            "q": "Former Illinois Sen. Alan Dixon, who died Sunday at age 86, was from what Metro East city?",
+            "a": [
+                {"option": "Edwardsville",      "correct": false},
+                {"option": "Belleville",     "correct": true},
+                {"option": "Mascoutah",      "correct": false},
+                {"option": "Alton",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/alan-j-dixon-former-us-senator-illinois-spent-lifetime-public-service'>Dixon began his political career as a police magistrate in Belleville and rose through the Illinois legislature to become state treasurer, then secretary of state. Donna Korando reported that he won the first of his two Senate elections in 1980 but lost a Democratic primary in 1992 to Carol Moseley Braun.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/alan-j-dixon-former-us-senator-illinois-spent-lifetime-public-service'>Dixon began his political career as a police magistrate in Belleville and rose through the Illinois legislature to become state treasurer, then secretary of state. Donna Korando reported that he won the first of his two Senate elections in 1980 but lost a Democratic primary in 1992 to Carol Moseley Braun.</a></p>" // no comma here
+        },
+        { 
+            "q": "Besides Missouri, how many other states give residents who are living overseas – including those in the military – the ability to register to vote and get their absentee ballots online?",
+            "a": [
+                {"option": "Ten",      "correct": false},
+                {"option": "Eight",     "correct": false},
+                {"option": "Five",      "correct": false},
+                {"option": "None – Missouri is the first state to do this",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missourians-residing-overseas-or-miltary-can-now-register-and-vote-online'>Missouri is the first state to have such a comprehensive online registration and absentee voting setup, according to Secretary of State Jason Kander. Jo Mannies reported that the site is now operational for the Aug. 5 primary election. Plans call for it to be in place for all subsequent elections held in Missouri.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missourians-residing-overseas-or-miltary-can-now-register-and-vote-online'>Missouri is the first state to have such a comprehensive online registration and absentee voting setup, according to Secretary of State Jason Kander. Jo Mannies reported that the site is now operational for the Aug. 5 primary election. Plans call for it to be in place for all subsequent elections held in Missouri.</a></p>" // no comma here
+        },
+        { 
+            "q": "Kirkwood native Sarah Paulsen used the 2008 fatal shootings in her hometown as the basis for what kind of artistic expression?",
+            "a": [
+                {"option": "Mural",      "correct": false},
+                {"option": "Opera",     "correct": false},
+                {"option": "Animated movie",      "correct": true},
+                {"option": "Book-length poem",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stl-art-game-changers-sarah-paulsen-animates-response-kirkwood-tragedy'>Paulsen created the feature-length animated movie titled “Elegy to Connie” as a memorial to Connie Karr, one of the six victims killed at Kirkwood City Hall in 2008. Nancy Fowler reported that Paulsen used collages, drawings, Claymation, 3-D landscapes, laborious stop-motion and other techniques in the film, which is scored with original music written by her cousin.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/stl-art-game-changers-sarah-paulsen-animates-response-kirkwood-tragedy'>Paulsen created the feature-length animated movie titled “Elegy to Connie” as a memorial to Connie Karr, one of the six victims killed at Kirkwood City Hall in 2008. Nancy Fowler reported that Paulsen used collages, drawings, Claymation, 3-D landscapes, laborious stop-motion and other techniques in the film, which is scored with original music written by her cousin.</a></p>" // no comma here
+        }
+]
+};

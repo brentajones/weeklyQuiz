417e6857589f4fca2eefab82a1e59f8d6a09b6ac
brentajones <bjones@bjones.net>
2014-07-17 13:21:26 -0500

    update for week

---
+++ js/20140717.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jul 18, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Former Missouri Supreme Court Judge Ronnie White finally won confirmation from the U.S. Senate for a federal judgeship. When was he first nominated to such a position?",
+            "a": [
+                {"option": "1995",      "correct": false},
+                {"option": "1997",     "correct": true},
+                {"option": "2001",      "correct": false},
+                {"option": "2005",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ronnie-white-confirmed-federal-bench-17-years-later'>White, who was the first African-American judge to sit on Missouri’s high court, was first nominated to a seat on the federal bench in 1997 by Bill Clinton. Camille Phillips reported that White’s nomination was blocked by then-Sen. John Ashcroft. White has been working in private practice in St. Louis since he retired from the Missouri Supreme Court in 2007.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ronnie-white-confirmed-federal-bench-17-years-later'>White, who was the first African-American judge to sit on Missouri’s high court, was first nominated to a seat on the federal bench in 1997 by Bill Clinton. Camille Phillips reported that White’s nomination was blocked by then-Sen. John Ashcroft. White has been working in private practice in St. Louis since he retired from the Missouri Supreme Court in 2007.</a></p>" // no comma here
+        },
+        { 
+            "q": "St. Louis Alderman Jennifer Florida was named recorder of deeds in the city, replacing Sharon Carpenter, who resigned last week. Before Monday’s announcement, Florida had been in the early stages of seeking what office?",
+            "a": [
+                {"option": "Aldermanic president",      "correct": true},
+                {"option": "State representative",     "correct": false},
+                {"option": "State senator",      "correct": false},
+                {"option": "Re-election to her aldermanic seat",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slay-names-alderman-florida-new-recorder-deeds'>Florida, a Democrat who represented the 15th Ward, had been in the early stages of an expected 2015 bid for president of the St. Louis Board of Aldermen. Jo Mannies reported that her new status raised the possibility that Florida may mount an independent bid for recorder of deeds in November, when Carpenter says she will seek the office again.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slay-names-alderman-florida-new-recorder-deeds'>Florida, a Democrat who represented the 15th Ward, had been in the early stages of an expected 2015 bid for president of the St. Louis Board of Aldermen. Jo Mannies reported that her new status raised the possibility that Florida may mount an independent bid for recorder of deeds in November, when Carpenter says she will seek the office again.</a></p>" // no comma here
+        },
+        { 
+            "q": "What issue was the topic of the latest controversy over ads in the race for St. Louis County executive?",
+            "a": [
+                {"option": "Nepotism",      "correct": false},
+                {"option": "Spending on parks",     "correct": false},
+                {"option": "Embezzlement by a county official",      "correct": false},
+                {"option": "Sex trafficking",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dooley-and-stenger-battle-over-tv-ads-womens-vote-0'>County Executive Charlie Dooley released an ad that claimed his challenger for the Democratic nomination, Steve Stenger, “represented a prostitution kingpin who sold women into slavery” 14 years ago. Jo Mannies reported that Stenger replied he took the case as required when he was a public defender and comments he made about the case in court were taken out of context.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/dooley-and-stenger-battle-over-tv-ads-womens-vote-0'>County Executive Charlie Dooley released an ad that claimed his challenger for the Democratic nomination, Steve Stenger, “represented a prostitution kingpin who sold women into slavery” 14 years ago. Jo Mannies reported that Stenger replied he took the case as required when he was a public defender and comments he made about the case in court were taken out of context.</a></p>" // no comma here
+        },
+        { 
+            "q": "New campaign finance reports show what candidate in Missouri is the leading fund raiser?",
+            "a": [
+                {"option": "Jay Nixon",      "correct": false},
+                {"option": "Chris Koster",     "correct": true},
+                {"option": "Tom Schweich",      "correct": false},
+                {"option": "Catherine Hanaway",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-transportation-tax-backers-lead-campaign-money-raising'>The campaign finance reports show that Koster, a Democrat running for governor, outpaced all other candidates in raising money, with a total of $3.11 million as of June 30, including at least $526,896 since April 1.  Jo Mannies reported that the $2.25 million Koster has in the bank is far more than the combined total of two of his possible Republican rivals, Schweich and Hanaway.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/koster-transportation-tax-backers-lead-campaign-money-raising'>The campaign finance reports show that Koster, a Democrat running for governor, outpaced all other candidates in raising money, with a total of $3.11 million as of June 30, including at least $526,896 since April 1.  Jo Mannies reported that the $2.25 million Koster has in the bank is far more than the combined total of two of his possible Republican rivals, Schweich and Hanaway.</a></p>" // no comma here
+        },
+        { 
+            "q": "According to a new law, when are new learning standards for Missouri schools supposed to be ready?",
+            "a": [
+                {"option": "2015",      "correct": false},
+                {"option": "2016",     "correct": true},
+                {"option": "2017",      "correct": false},
+                {"option": "No specific date was set",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-vetoes-armed-teacher-bill-signs-common-core-changes'>The bill signed by Gov. Jay Nixon says new home-grown standards for Missouri schools should be ready for the 2016-17 school year. Until then, Dale Singer reported, the state may proceed with tests based on the nationwide Common Core standards.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nixon-vetoes-armed-teacher-bill-signs-common-core-changes'>The bill signed by Gov. Jay Nixon says new home-grown standards for Missouri schools should be ready for the 2016-17 school year. Until then, Dale Singer reported, the state may proceed with tests based on the nationwide Common Core standards.</a></p>" // no comma here
+        },
+        { 
+            "q": "Gene Kranz, the man in charge at NASA when astronauts first walked on the Moon 45 years ago this month, graduated from what area school?",
+            "a": [
+                {"option": "Washington University",      "correct": false},
+                {"option": "University of Missouri-Rolla",     "correct": false},
+                {"option": "Saint Louis University",      "correct": true},
+                {"option": "Southern Illinois University-Edwardsville",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/forty-five-years-ago-we-first-landed-moon-meet-man-charge-mission-control'>Kranz is a graduate of Parks College at Saint Louis University. The former NASA flight director told Veronique LaCapra that he got a $500 scholarship in his native Ohio that covered his expenses while he got his degree.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/forty-five-years-ago-we-first-landed-moon-meet-man-charge-mission-control'>Kranz is a graduate of Parks College at Saint Louis University. The former NASA flight director told Veronique LaCapra that he got a $500 scholarship in his native Ohio that covered his expenses while he got his degree.</a></p>" // no comma here
+        },
+        { 
+            "q": "What group came to St. Louis to hear veterans’ stories about their problems receiving medical treatment?",
+            "a": [
+                {"option": "Disabled American Veterans",      "correct": false},
+                {"option": "Veterans of Foreign Wars",     "correct": false},
+                {"option": "Vietnam Veterans of America",      "correct": false},
+                {"option": "American Legion",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/american-legion-targets-gap-between-veterans-and-va-st-louis-health-care'>The American Legions  sent members from its “System Worth Saving Task Force” in Washington, D.C., to relay concerns directly from St. Louis-area veterans. Joseph Leahy reported that the group  has been operating week-long crisis centers for veterans and their families since early June.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/american-legion-targets-gap-between-veterans-and-va-st-louis-health-care'>The American Legions  sent members from its “System Worth Saving Task Force” in Washington, D.C., to relay concerns directly from St. Louis-area veterans. Joseph Leahy reported that the group  has been operating week-long crisis centers for veterans and their families since early June.</a></p>" // no comma here
+        },
+        { 
+            "q": "Under new regulations, horses giving carriage rides in St. Louis cannot work when the heat index reaches what point?",
+            "a": [
+                {"option": "80 degrees",      "correct": false},
+                {"option": "90 degrees",     "correct": false},
+                {"option": "100 degrees",      "correct": true},
+                {"option": "110 degrees",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-health-department-tightens-reins-carriage-horse-companies'>The new guidelines issued by Pamela Walker, head of the city’s health department, forbid horses from working when the heat index reaches 100 degrees. Joseph Leahy reported that they also limit horses from working more than eight hours a day and five days per week. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-health-department-tightens-reins-carriage-horse-companies'>The new guidelines issued by Pamela Walker, head of the city’s health department, forbid horses from working when the heat index reaches 100 degrees. Joseph Leahy reported that they also limit horses from working more than eight hours a day and five days per week. </a></p>" // no comma here
+        },
+        { 
+            "q": "What is the name of the new book from former U.S. Rep. Todd Akin, R-Mo.?",
+            "a": [
+                {"option": "Firing Straight",      "correct": false},
+                {"option": "Firing Blanks",     "correct": false},
+                {"option": "Firing Back",      "correct": true},
+                {"option": "Firing Blind",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/republicans-silence-golden-when-it-comes-akins-book'>In his book, titled “Firing Back,” Akin generally defends his controversial 2012 contention that in cases of “legitimate rape,’’ women rarely get pregnant because “their bodies have ways of shutting the whole thing down.” Jo Mannies reported that fellow Missouri Republicans appear to be ignoring the book, but Sen. Claire McCaskill, who defeated Akin in the 2012 election, says it shows what he really believes. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/republicans-silence-golden-when-it-comes-akins-book'>In his book, titled “Firing Back,” Akin generally defends his controversial 2012 contention that in cases of “legitimate rape,’’ women rarely get pregnant because “their bodies have ways of shutting the whole thing down.” Jo Mannies reported that fellow Missouri Republicans appear to be ignoring the book, but Sen. Claire McCaskill, who defeated Akin in the 2012 election, says it shows what he really believes. </a></p>" // no comma here
+        },
+        { 
+            "q": "A property described as a “Stonehenge” of southeast Missouri has been listed for $149,000. What town is it near?",
+            "a": [
+                {"option": "Poplar Bluff",      "correct": false},
+                {"option": "Perryville",     "correct": false},
+                {"option": "Patton",      "correct": true},
+                {"option": "Puxico",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/you-missed-nike-missile-base-perhaps-we-could-interest-you-missouri-stonehenge'>Real estate broker Wayne Keller listed the 54-acre property near Patton, Mo., at $149,000, complete with a mobile home to go along with a downsized replica of the famous prehistoric English monument. Mary Delach Leonard reported that Keller’s sale of an old Nike missile site near Hecker, Ill., fetched a price of $227,000.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/you-missed-nike-missile-base-perhaps-we-could-interest-you-missouri-stonehenge'>Real estate broker Wayne Keller listed the 54-acre property near Patton, Mo., at $149,000, complete with a mobile home to go along with a downsized replica of the famous prehistoric English monument. Mary Delach Leonard reported that Keller’s sale of an old Nike missile site near Hecker, Ill., fetched a price of $227,000.</a></p>" // no comma here
+        }
+]
+};

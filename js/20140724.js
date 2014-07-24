301c2809c41ca2e05eed88d7dc066b4d9e5cbcb0
brentajones <bjones@bjones.net>
2014-07-24 17:11:07 -0500

    update for week

---
+++ js/20140724.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Jul 25, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Conflicting federal court rulings involved what aspect of the Affordable Care Act?",
+            "a": [
+                {"option": "Subsidies on premiums",      "correct": true},
+                {"option": "Coverage for contraception",     "correct": false},
+                {"option": "Payments by small business",      "correct": false},
+                {"option": "Being able to choose your own doctor",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/differing-court-rulings-obamacare-mean-one-thing-nothing-changes-now'>The two rulings, out of federal appeals courts in the District of Columbia and Virginia, came to different conclusions about whether people in the states that opted not to set up their own health insurance exchanges can qualify for subsidies on their health insurance premiums. Shula Neuman and Veronique LaCapra reported that Missouri and Illinois are among those 36 states that don't have state-run exchanges.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/differing-court-rulings-obamacare-mean-one-thing-nothing-changes-now'>The two rulings, out of federal appeals courts in the District of Columbia and Virginia, came to different conclusions about whether people in the states that opted not to set up their own health insurance exchanges can qualify for subsidies on their health insurance premiums. Shula Neuman and Veronique LaCapra reported that Missouri and Illinois are among those 36 states that don't have state-run exchanges.</a></p>" // no comma here
+        },
+        { 
+            "q": "At the Aug. 5 election, Mavis Thompson and Jeffrey Boyd will face each other in a race for the Democratic nomination for what St. Louis city office?",
+            "a": [
+                {"option": "Public administrator",      "correct": false},
+                {"option": "Recorder of deeds",     "correct": false},
+                {"option": "Collector of revenue",      "correct": false},
+                {"option": "License collector",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-license-collector-race-flies-under-radar'>License collector Mavis Thompson, who was appointed to the job last year, will face Alderman Jeffrey Boyd in the Democratic primary race. Chris McDaniel reported that the winner will run in November against Green Party candidate Donald Devivo.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-license-collector-race-flies-under-radar'>License collector Mavis Thompson, who was appointed to the job last year, will face Alderman Jeffrey Boyd in the Democratic primary race. Chris McDaniel reported that the winner will run in November against Green Party candidate Donald Devivo.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which of these regulations was part of the changes made by the Metropolitan Taxicab Commission in an effort to lure Uber Black to the St. Louis market?",
+            "a": [
+                {"option": "Allowing older sedans on the road, subject to additional inspections.",      "correct": false},
+                {"option": "No longer requiring a premium sedan be booked at least an hour in advance.",     "correct": false},
+                {"option": "Eliminating the rule that a sedan company have three cars ready before gaining a license to operate",      "correct": false},
+                {"option": "All of the above",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/taxi-commission-clears-path-uber-black-st-louis'>All three of the changes were among those made by the commission to convince Uber to enter the market. Rachel Lippmann reported that Uber Black dispatches already-licensed premium sedans from a smart phone.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/taxi-commission-clears-path-uber-black-st-louis'>All three of the changes were among those made by the commission to convince Uber to enter the market. Rachel Lippmann reported that Uber Black dispatches already-licensed premium sedans from a smart phone.</a></p>" // no comma here
+        },
+        { 
+            "q": "Vice President Joe Biden spoke in St. Louis to a convention of the Veterans of Foreign Wars. Where was the meeting held?",
+            "a": [
+                {"option": "America’s Center",      "correct": true},
+                {"option": "Peabody Opera House",     "correct": false},
+                {"option": "Scottrade Center",      "correct": false},
+                {"option": "Touhill Center ",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/biden-defends-withdrawal-troops-iraq-afghanistan'>The vice president addressed the convention at America’s Center. Jo Mannies reported that he lauded the nation’s military veterans who have fought battles overseas but made clear that there’s a limit to what the United States’ military can do.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/biden-defends-withdrawal-troops-iraq-afghanistan'>The vice president addressed the convention at America’s Center. Jo Mannies reported that he lauded the nation’s military veterans who have fought battles overseas but made clear that there’s a limit to what the United States’ military can do.</a></p>" // no comma here
+        },
+        { 
+            "q": "The acting secretary for veterans affairs also addressed the VFW convention in St. Louis. What is the secretary’s name?",
+            "a": [
+                {"option": "Sloan Wilson",      "correct": false},
+                {"option": "William Sloan",     "correct": false},
+                {"option": "Sloan Gibson",      "correct": true},
+                {"option": "Stephen Williams",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/health-care-st-louis-veterans-expanding-says-acting-head-va'>Sloan Gibson, the acting secretary for veterans affairs, addressed the convention Tuesday morning, then met with employees at the VA St. Louis Health Center in the afternoon. Parth Shah reported that Gibson discussed efforts being made locally to accelerate care for veterans who have been waiting the longest.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/health-care-st-louis-veterans-expanding-says-acting-head-va'>Sloan Gibson, the acting secretary for veterans affairs, addressed the convention Tuesday morning, then met with employees at the VA St. Louis Health Center in the afternoon. Parth Shah reported that Gibson discussed efforts being made locally to accelerate care for veterans who have been waiting the longest.</a></p>" // no comma here
+        },
+        { 
+            "q": "Missourians will vote next month on creating a new lottery ticket that would benefit what?",
+            "a": [
+                {"option": "Higher education",      "correct": false},
+                {"option": "State parks",     "correct": false},
+                {"option": "Veterans",      "correct": true},
+                {"option": "Transportation",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-voters-decide-privacy-protection-veterans-lottery-ticket'>If Constitutional Amendment 8 passes, the Missouri Lottery would begin selling the special ticket by July 1, 2015, to help fund the state's veterans' homes, cemeteries and outreach programs. Marshall Griffin reported that all net proceeds, around 25 cents of every dollar, would be deposited in the state's veterans' commission capital improvement trust fund.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-voters-decide-privacy-protection-veterans-lottery-ticket'>If Constitutional Amendment 8 passes, the Missouri Lottery would begin selling the special ticket by July 1, 2015, to help fund the state's veterans' homes, cemeteries and outreach programs. Marshall Griffin reported that all net proceeds, around 25 cents of every dollar, would be deposited in the state's veterans' commission capital improvement trust fund.</a></p>" // no comma here
+        },
+        { 
+            "q": "Metro broke ground Tuesday on a new transit center in what north St. Louis County community?",
+            "a": [
+                {"option": "Ferguson",      "correct": true},
+                {"option": "Florissant",     "correct": false},
+                {"option": "Hazelwood",      "correct": false},
+                {"option": "Bridgeton",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/metro-hopes-transit-center-will-improve-north-county-residents-riding-experience'>Metro is transforming a former Mazda dealership in Ferguson into a centralized hub for its bus service. Jason Rosenbaum reported that the center is slated to have 10 bus bays, concession stands and restrooms, along with a free parking lot and an indoor waiting area to protect passengers from bad weather.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/metro-hopes-transit-center-will-improve-north-county-residents-riding-experience'>Metro is transforming a former Mazda dealership in Ferguson into a centralized hub for its bus service. Jason Rosenbaum reported that the center is slated to have 10 bus bays, concession stands and restrooms, along with a free parking lot and an indoor waiting area to protect passengers from bad weather.</a></p>" // no comma here
+        },
+        { 
+            "q": "The new St. Louis police headquarters once was a building housing offices of what company?",
+            "a": [
+                {"option": "Union Electric",      "correct": false},
+                {"option": "General American Life",     "correct": false},
+                {"option": "Ralston Purina",      "correct": false},
+                {"option": "A.G. Edwards",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-police-march-new-headquarters'>The new headquarters at 1915 Olive once was used by A.G. Edwards. Camille Phillips reported that the building was purchased in 2011 using $2.7 million in asset forfeiture funds, and the St. Louis Police Foundation contributed $3 million for renovations.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-police-march-new-headquarters'>The new headquarters at 1915 Olive once was used by A.G. Edwards. Camille Phillips reported that the building was purchased in 2011 using $2.7 million in asset forfeiture funds, and the St. Louis Police Foundation contributed $3 million for renovations.</a></p>" // no comma here
+        },
+        { 
+            "q": "An arts organization in what suburb said it had to close down because of a lack of funds?",
+            "a": [
+                {"option": "Clayton",      "correct": false},
+                {"option": "Collinsville",     "correct": false},
+                {"option": "Chesterfield",      "correct": true},
+                {"option": "Crestwood",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chesterfield-arts-cease-operations-august'>Chesterfield Arts said it faced declining revenue and was being forced to relocate. Emanuele Berry reported that the group promoted public art thought exhibitions and galleries and also offered numerous classes for youth, adults and disabled individuals.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/chesterfield-arts-cease-operations-august'>Chesterfield Arts said it faced declining revenue and was being forced to relocate. Emanuele Berry reported that the group promoted public art thought exhibitions and galleries and also offered numerous classes for youth, adults and disabled individuals.</a></p>" // no comma here
+        },
+        { 
+            "q": "After 27 years, John Karel is stepping down as head of what local park?",
+            "a": [
+                {"option": "Forest Park",      "correct": false},
+                {"option": "Creve Coeur Park",     "correct": false},
+                {"option": "Tower Grove Park",      "correct": true},
+                {"option": "Faust Park",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tower-grove-park-director-step-down-says-building-parks-legacy-wont-end'>Karel has been director of Tower Grove Park since 1987. Emanuele Berry reported that during his tenure, visitation to the park has grown dramatically and was five times higher in 2013, than it was in 1987.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tower-grove-park-director-step-down-says-building-parks-legacy-wont-end'>Karel has been director of Tower Grove Park since 1987. Emanuele Berry reported that during his tenure, visitation to the park has grown dramatically and was five times higher in 2013, than it was in 1987.</a></p>" // no comma here
+        }
+]
+};

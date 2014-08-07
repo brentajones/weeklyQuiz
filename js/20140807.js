2cbb0372edb5f2d5512294568a1035a60e0e88a2
brentajones <bjones@bjones.net>
2014-08-07 15:36:46 -0500

    update quiz for week

---
+++ js/20140807.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Aug 8, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "Now that a sales tax increase for road and bridge improvements has been rejected by Missouri voters, what alternative has been proposed by transportation officials?",
+            "a": [
+                {"option": "Toll roads",      "correct": false},
+                {"option": "Gas tax",     "correct": false},
+                {"option": "Income tax on trucking firms",      "correct": false},
+                {"option": "They haven’t proposed have any alternatives yet",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/back-drawing-board-missouris-transportation-leaders'>Missouri transportation leaders are looking to regroup after rejection of the sales tax, but they haven’t proposed any alternatives. Marshall Griffin reported that in St. Louis and St. Louis County, the measure went down by a 2-to-1 margin.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/back-drawing-board-missouris-transportation-leaders'>Missouri transportation leaders are looking to regroup after rejection of the sales tax, but they haven’t proposed any alternatives. Marshall Griffin reported that in St. Louis and St. Louis County, the measure went down by a 2-to-1 margin.</a></p>" // no comma here
+        },
+        { 
+            "q": "In what Missouri county did the proposed constitutional amendment to strengthen electronic privacy protections fail to win a majority?",
+            "a": [
+                {"option": "St. Louis County",      "correct": false},
+                {"option": "Jackson County",     "correct": false},
+                {"option": "Boone County",      "correct": false},
+                {"option": "It won in all of Missouri’s counties",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/winners-and-losers-glance-votes-county-county'>The privacy proposal, known as Amendment 9, won a majority in every county in Missouri. A map put together by Brent Jones shows the margin for that amendment and every other amendment on Tuesday’s ballot for each county in the state.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/winners-and-losers-glance-votes-county-county'>The privacy proposal, known as Amendment 9, won a majority in every county in Missouri. A map put together by Brent Jones shows the margin for that amendment and every other amendment on Tuesday’s ballot for each county in the state.</a></p>" // no comma here
+        },
+        { 
+            "q": "The Jennings school district celebrated preliminary figures that showed it to be in the full accreditation range. What percentage on the annual report does a district need for full accreditation?",
+            "a": [
+                {"option": "60 percent",      "correct": false},
+                {"option": "65 percent",     "correct": false},
+                {"option": "70 percent",      "correct": true},
+                {"option": "75 percent",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/jennings-schools-celebrate-leap-toward-full-accreditation'>Districts in Missouri are fully accredited when they earn 70 percent of the 140 points possible on their annual report. Dale Singer reported that preliminary figures from Jennings showed them to have 76.1 percent for 2014.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/jennings-schools-celebrate-leap-toward-full-accreditation'>Districts in Missouri are fully accredited when they earn 70 percent of the 140 points possible on their annual report. Dale Singer reported that preliminary figures from Jennings showed them to have 76.1 percent for 2014.</a></p>" // no comma here
+        },
+        { 
+            "q": "For two years, Missouri ranked tops in the nation for growth of tech jobs. Where did it rank in the latest survey?",
+            "a": [
+                {"option": "1st",      "correct": false},
+                {"option": "2nd",     "correct": false},
+                {"option": "10th",      "correct": true},
+                {"option": "20th",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-falls-10th-tech-job-growth'>Missouri was the fastest-growing state for tech jobs the last two years but this year, it slipped from first to 10th. Maria Altman reported that growth in Missouri was 2.1 percent, but the fastest pace so far this year was in Texas with a nearly 6 percent growth rate.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-falls-10th-tech-job-growth'>Missouri was the fastest-growing state for tech jobs the last two years but this year, it slipped from first to 10th. Maria Altman reported that growth in Missouri was 2.1 percent, but the fastest pace so far this year was in Texas with a nearly 6 percent growth rate.</a></p>" // no comma here
+        },
+        { 
+            "q": "The director of the FBI came to St. Louis this week and pledged to help local police chiefs. What is the director’s name?",
+            "a": [
+                {"option": "Louis Freeh",      "correct": false},
+                {"option": "Robert Mueller",     "correct": false},
+                {"option": "John Brennan",      "correct": false},
+                {"option": "James Comey",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-visit-fbi-chief-pledges-help-local-chiefs'>Director James Comey was in St. Louis Wednesday as part of his planned tour of all 56 of the agency's field offices. Rachel Lippmann reported that Comey said both he and the new special agent in charge of the St. Louis office, Bill Wood, are eager to help combat violent crimes in the region. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-visit-fbi-chief-pledges-help-local-chiefs'>Director James Comey was in St. Louis Wednesday as part of his planned tour of all 56 of the agency's field offices. Rachel Lippmann reported that Comey said both he and the new special agent in charge of the St. Louis office, Bill Wood, are eager to help combat violent crimes in the region. </a></p>" // no comma here
+        },
+        { 
+            "q": "Fund raising continues for a statue of jazz pioneer Miles Davis. Where will the statue be located?",
+            "a": [
+                {"option": "University City",      "correct": false},
+                {"option": "Alton",     "correct": true},
+                {"option": "East St. Louis",      "correct": false},
+                {"option": "Downtown St. Louis",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fundraising-efforts-continue-alton-statue-miles-davis-statue-design-underway'>The statue is set to be in front of 117 W. Third St. in Alton. Willis Ryder Arnold reported that the Miles Davis Memorial Project began two years ago and has raised more than $50,000 to date, with hopes to complete the project with another $27,000.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/fundraising-efforts-continue-alton-statue-miles-davis-statue-design-underway'>The statue is set to be in front of 117 W. Third St. in Alton. Willis Ryder Arnold reported that the Miles Davis Memorial Project began two years ago and has raised more than $50,000 to date, with hopes to complete the project with another $27,000.</a></p>" // no comma here
+        },
+        { 
+            "q": "John Britton, who was hailed for his career in Jefferson City, died this week. What position did he hold that earned him praise from Missouri politicians?",
+            "a": [
+                {"option": "State senator",      "correct": false},
+                {"option": "State representative",     "correct": false},
+                {"option": "Lobbyist",      "correct": true},
+                {"option": "Head of Capitol security",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/john-britton-most-powerful-lobbyist-jefferson-city-has-died'>Britton spent decades as a top lobbyist in Jefferson City. Jo Mannies reported that he was so powerful that legislators made an exception for him when a no-smoking ban went into effect years ago in the halls of the Capitol and he was granted his own floating smoking zone.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/john-britton-most-powerful-lobbyist-jefferson-city-has-died'>Britton spent decades as a top lobbyist in Jefferson City. Jo Mannies reported that he was so powerful that legislators made an exception for him when a no-smoking ban went into effect years ago in the halls of the Capitol and he was granted his own floating smoking zone.</a></p>" // no comma here
+        },
+        { 
+            "q": "A reward of up to $20,000 was announced this week in the case of Amanda Jones, who has been missing since 2005. Where was she from?",
+            "a": [
+                {"option": "St. Charles County",      "correct": false},
+                {"option": "St. Louis County",     "correct": false},
+                {"option": "St. Clair County",      "correct": false},
+                {"option": "Jefferson County",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/investigators-offer-reward-missing-mom-case'>Jones, from Jefferson County, was last seen August 14, 2005, at the Hillsboro Civic Center. Wayne Pratt reported that several tips in the case have come in over the past nine years, but nothing has panned out. </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/investigators-offer-reward-missing-mom-case'>Jones, from Jefferson County, was last seen August 14, 2005, at the Hillsboro Civic Center. Wayne Pratt reported that several tips in the case have come in over the past nine years, but nothing has panned out. </a></p>" // no comma here
+        },
+        { 
+            "q": "Where was the back to school fair for St. Louis Public Schools held?",
+            "a": [
+                {"option": "Chaifetz Arena",      "correct": true},
+                {"option": "Scotttrade Center",     "correct": false},
+                {"option": "Peabody Opera House",      "correct": false},
+                {"option": "America’s Center",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slps-gears-new-school-year'>An estimated 10,000 people attended the Back to School and Community Empowerment Festival at St. Louis University's Chaifetz Arena. Camille Phillips reported that families were able to get free school supplies and find out about area resources.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slps-gears-new-school-year'>An estimated 10,000 people attended the Back to School and Community Empowerment Festival at St. Louis University's Chaifetz Arena. Camille Phillips reported that families were able to get free school supplies and find out about area resources.</a></p>" // no comma here
+        },
+        { 
+            "q": "A delegation of religious and education officials from what Central American country came for meetings with faculty members at Saint Louis University?",
+            "a": [
+                {"option": "Belize",      "correct": true},
+                {"option": "Costa Rica",     "correct": false},
+                {"option": "El Salvador",      "correct": false},
+                {"option": "Guatemala",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slu-hosts-summit-discuss-education-belize'>Faculty members at SLU met with officials from Belize. Camille Phillips reported that the conference was designed to determine ways that SLU students and faculty can provide training and support for St. Martin’s de Porres, a Jesuit parish and grade school in Belize City.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/slu-hosts-summit-discuss-education-belize'>Faculty members at SLU met with officials from Belize. Camille Phillips reported that the conference was designed to determine ways that SLU students and faculty can provide training and support for St. Martin’s de Porres, a Jesuit parish and grade school in Belize City.</a></p>" // no comma here
+        }
+]
+};

f9c68968c9dfc126cda95f1f48e98d863b484110
brentajones <bjones@bjones.net>
2014-08-14 16:48:42 -0500

    update for week

---
+++ js/20140814.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+    "info": {
+        "name":    "St. Louis Public Radio News Quiz for Aug. 15, 2014",
+        "main":    "<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+        "results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+        "level1":  "Nice work.",
+        "level2":  "Almost there.",
+        "level3":  "Try again next week." // no comma here
+    },
+    "questions": [
+        { 
+            "q": "African-American residents make up what percentage of the population of Ferguson? ",
+            "a": [
+                {"option": "50",      "correct": false},
+                {"option": "67",     "correct": true},
+                {"option": "75",      "correct": false},
+                {"option": "85",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ferguson-numbers'>Ferguson’s population is 67 percent black and nearly 33 percent white, with Asians making up about 1 percent of the north county community. Graphics by Brent Jones compare the Ferguson breakdown with that in St. Louis County as a whole, which is 25 percent African-American and nearly 75 percent white.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/ferguson-numbers'>Ferguson’s population is 67 percent black and nearly 33 percent white, with Asians making up about 1 percent of the north county community. Graphics by Brent Jones compare the Ferguson breakdown with that in St. Louis County as a whole, which is 25 percent African-American and nearly 75 percent white.</a></p>" // no comma here
+        },
+        { 
+            "q": "What was the name of the north St. Louis County apartment complex that was a focal point in the protests surrounding the death of Michael Brown?",
+            "a": [
+                {"option": "Canfield Gardens",      "correct": true},
+                {"option": "Campbell Acres",     "correct": false},
+                {"option": "Catherine Vista",      "correct": false},
+                {"option": "Carter Square",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/peaceful-protest-against-police-brutality-turns-night-violence'>The protests took place near the Canfield Gardens apartment complex. Rachel Lippmann reported that the crowd that gathered near the site of the shooting at first was large but peaceful; later, a small group began throwing rocks and bottles at a line of police officers, and at least one person launched a flare. Unrest continued through the week.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/peaceful-protest-against-police-brutality-turns-night-violence'>The protests took place near the Canfield Gardens apartment complex. Rachel Lippmann reported that the crowd that gathered near the site of the shooting at first was large but peaceful; later, a small group began throwing rocks and bottles at a line of police officers, and at least one person launched a flare. Unrest continued through the week.</a></p>" // no comma here
+        },
+        { 
+            "q": "A U.S. Supreme Court case that limits the authority of police to shoot at unarmed fleeing suspects arose from what state?",
+            "a": [
+                {"option": "Missouri",      "correct": false},
+                {"option": "Tennessee",     "correct": true},
+                {"option": "Arkansas",      "correct": false},
+                {"option": "Illinois",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/deadly-force-what-does-law-say-about-when-police-are-allowed-use-it'>In the 1985 case Tennessee v. Garner, the high court ruled that the Constitution does not permit police to fire at unarmed, nonviolent, fleeing suspects unless there is a significant threat of death or serious physical injury to the officer or the public. William Freivogel reported that the ruling rested upon an earlier decision in a 1972 police shooting in Olivette.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/deadly-force-what-does-law-say-about-when-police-are-allowed-use-it'>In the 1985 case Tennessee v. Garner, the high court ruled that the Constitution does not permit police to fire at unarmed, nonviolent, fleeing suspects unless there is a significant threat of death or serious physical injury to the officer or the public. William Freivogel reported that the ruling rested upon an earlier decision in a 1972 police shooting in Olivette.</a></p>" // no comma here
+        },
+        { 
+            "q": "What bank is handling a fund established for the family of Michael Brown?",
+            "a": [
+                {"option": "U.S. Bank",      "correct": false},
+                {"option": "PNC Bank",     "correct": false},
+                {"option": "Reliance Bank",      "correct": false},
+                {"option": "Fifth Third Bank",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sharpton-says-brown-investigation-needs-more-transparency'>The Ferguson branch of Fifth Third Bank is handling the Michael Brown Jr. Fund. Dale Singer reported that the existence of the fund was announced during a news conference held on the Old Courthouse steps by the Rev. Al Sharpton and others.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/sharpton-says-brown-investigation-needs-more-transparency'>The Ferguson branch of Fifth Third Bank is handling the Michael Brown Jr. Fund. Dale Singer reported that the existence of the fund was announced during a news conference held on the Old Courthouse steps by the Rev. Al Sharpton and others.</a></p>" // no comma here
+        },
+        { 
+            "q": "Which north St. Louis County school districts postponed their first day of class this week because of concern over unrest in Ferguson?",
+            "a": [
+                {"option": "Ferguson-Florissant and Jennings",      "correct": true},
+                {"option": "Normandy and Ferguson-Florissant",     "correct": false},
+                {"option": "Hazelwood and Normandy",      "correct": false},
+                {"option": "Jennings and Normandy",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schools-cope-ferguson-unrest'>The Jennings school district, where most students walk to school, postponed its opening day of class from Monday to Tuesday after unrest in Ferguson Sunday night, as Dale Singer and Tim Lloyd reported. Ferguson-Florissant announced Wednesday that its schools, scheduled to open Thursday, would not open until Monday.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schools-cope-ferguson-unrest'>The Jennings school district, where most students walk to school, postponed its opening day of class from Monday to Tuesday after unrest in Ferguson Sunday night, as Dale Singer and Tim Lloyd reported. Ferguson-Florissant announced Wednesday that its schools, scheduled to open Thursday, would not open until Monday.</a></p>" // no comma here
+        },
+        { 
+            "q": "Artist Adrian Franks has produced a series of images based on the deaths of black men at the hands of law enforcement – a series that now includes Michael Brown. What is the series called?",
+            "a": [
+                {"option": "No Justice, No Peace",      "correct": false},
+                {"option": "Hands Up, Don’t Shoot",     "correct": false},
+                {"option": "The Whole World Is Watching",      "correct": false},
+                {"option": "My Hands Are Up",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/michael-brown-joins-brooklyn-artists-violence-against-black-men-portrait-series'>Franks’ series titled “My Hands Are Up” now includes a black and red image of Michael Brown. He told reporter Willis Ryder Arnold: “I got to put something out there to get people thinking about these serious issues of black males in particular meeting a lot of violence.” </a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/michael-brown-joins-brooklyn-artists-violence-against-black-men-portrait-series'>Franks’ series titled “My Hands Are Up” now includes a black and red image of Michael Brown. He told reporter Willis Ryder Arnold: “I got to put something out there to get people thinking about these serious issues of black males in particular meeting a lot of violence.” </a></p>" // no comma here
+        },
+        { 
+            "q": "What was the only subject in which Missouri students’ scores rose overall on this year’s MAP tests?",
+            "a": [
+                {"option": "English",      "correct": false},
+                {"option": "Math",     "correct": false},
+                {"option": "Social studies",      "correct": true},
+                {"option": "Science",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/statewide-map-test-scores-down-english-math-science-social-studies'>Scores on the social studies test rose overall while test scores for the other three subjects fell. Marshall Griffin reported that Missouri education officials are weighing several different reasons for the decline.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/statewide-map-test-scores-down-english-math-science-social-studies'>Scores on the social studies test rose overall while test scores for the other three subjects fell. Marshall Griffin reported that Missouri education officials are weighing several different reasons for the decline.</a></p>" // no comma here
+        },
+        { 
+            "q": "Rex Sinquefield is the primary backer of a group that contributed heavily to candidates in last week’s Missouri primary elections. What is the group’s name?",
+            "a": [
+                {"option": "Missourians for Prosperity",      "correct": false},
+                {"option": "Missouri Club for Growth",     "correct": true},
+                {"option": "Show-Me Success",      "correct": false},
+                {"option": "Forward Missouri",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/checking-score-how-well-did-sinquefield-backed-candidates-fare'>The Sinquefield-backed Missouri Club for Growth had a mixed record in primary races, Jason Rosenbaum reported. Four GOP challengers it supported over incumbents who did not favor Sinquefield’s tax cuts last year all lost, but other candidates he donated to won their races.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/checking-score-how-well-did-sinquefield-backed-candidates-fare'>The Sinquefield-backed Missouri Club for Growth had a mixed record in primary races, Jason Rosenbaum reported. Four GOP challengers it supported over incumbents who did not favor Sinquefield’s tax cuts last year all lost, but other candidates he donated to won their races.</a></p>" // no comma here
+        },
+        { 
+            "q": "How much money did state Rep. John Diehl, soon to become speaker of the Missouri House, contribute to the state House Republican Campaign Committee?",
+            "a": [
+                {"option": "$50,000",      "correct": false},
+                {"option": "$100,000",     "correct": false},
+                {"option": "$200,000",      "correct": true},
+                {"option": "$300,000",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/diehl-leads-800000-blitz-missouri-house-gop'>Diehl, R-Town and Country, donated $200,000 to the committee. Jo Mannies reported that the funds were part of a campaign finance blitz that amounted to at least $832,000 in large donations of more than $5,000 apiece as the GOP tries to maintain its veto-proof majority in the Missouri House.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/diehl-leads-800000-blitz-missouri-house-gop'>Diehl, R-Town and Country, donated $200,000 to the committee. Jo Mannies reported that the funds were part of a campaign finance blitz that amounted to at least $832,000 in large donations of more than $5,000 apiece as the GOP tries to maintain its veto-proof majority in the Missouri House.</a></p>" // no comma here
+        },
+        { 
+            "q": "A team of researchers at Washington University has found that babies born in what situation have very different gut microbes than those of babies carried to term?",
+            "a": [
+                {"option": "Born to mothers over 40",      "correct": false},
+                {"option": "Born to mothers in their teens",     "correct": false},
+                {"option": "Born prematurely",      "correct": true},
+                {"option": "Born via Caesarean section",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-preterm-babies-have-different-gut-microbes-which-could-affect-their-health'>The Washington University researchers found that babies born prematurely have very different gut microbes than those of babies carried to term. Veronique La Capra reported that premature infants are susceptible to life-long problems, and their immature gut bacteria may play a role in that.</a></p>",
+            "incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/study-preterm-babies-have-different-gut-microbes-which-could-affect-their-health'>The Washington University researchers found that babies born prematurely have very different gut microbes than those of babies carried to term. Veronique La Capra reported that premature infants are susceptible to life-long problems, and their immature gut bacteria may play a role in that.</a></p>" // no comma here
+        }
+]
+};

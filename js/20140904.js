6083eb1d8656460e2a11faa9ae431b7a46e0b76d
brentajones <bjones@bjones.net>
2014-09-04 18:18:14 -0500

    update for week

---
+++ js/20140904.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+	"info": {
+		"name":	"St. Louis Public Radio News Quiz for Sep. 05, 2014",
+		"main":	"<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+		"results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+		"level1":  "Nice work.",
+		"level2":  "Almost there.",
+		"level3":  "Try again next week." // no comma here
+	},
+	"questions": [
+		{
+			"q": "What is the name of the execution drug that Missouri used even though officials said it would not?",
+			"a": [
+				{"option": "Midazolam",	"correct": true},
+				{"option": "Pentobarbital",	"correct": false},
+				{"option": "Hydromorphone",	"correct": false},
+				{"option": "Pavulon",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-swore-it-wouldn-t-use-controversial-execution-drug-it-did'>The state had said it would not use Midazolam in executions, but an investigation by Chris McDaniel showed it did. He found that despite botched executions in other states that used the drug, Missouri has used Midazolam in every execution since November of last year. </a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/missouri-swore-it-wouldn-t-use-controversial-execution-drug-it-did'>The state had said it would not use Midazolam in executions, but an investigation by Chris McDaniel showed it did. He found that despite botched executions in other states that used the drug, Missouri has used Midazolam in every execution since November of last year. </a></p>"
+		},
+		{
+			"q": "In the wake of disturbances in Ferguson, a subcommittee headed by Sen. Claire McCaskill plans a hearing next week on what topic?",
+			"a": [
+				{"option": "Income inequality",	"correct": false},
+				{"option": "Militarization of local police ",	"correct": true},
+				{"option": "Racial profiling",	"correct": false},
+				{"option": "School desegregation",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/hearing-set-examine-militarization-local-police-departments'>McCaskill, D-Mo., called the hearing to look into the militarization of local law enforcement. Jim Howard reported that her Senate Subcommittee on Financial & Contracting Oversight will look into oversight of three federal programs that make it possible for local police to acquire military equipment.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/hearing-set-examine-militarization-local-police-departments'>McCaskill, D-Mo., called the hearing to look into the militarization of local law enforcement. Jim Howard reported that her Senate Subcommittee on Financial & Contracting Oversight will look into oversight of three federal programs that make it possible for local police to acquire military equipment.</a></p>"
+		},
+		{
+			"q": "Every year, the criminal section of the Civil Rights Division receives about 10,000 complaints alleging violations of civil rights, most involving law enforcement officials. In 2013, how many officers were prosecuted?",
+			"a": [
+				{"option": "150",	"correct": false},
+				{"option": "95",	"correct": false},
+				{"option": "60",	"correct": false},
+				{"option": "45",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/killing-michael-brown-limits-federal-and-civil-power'>In 2013, the last year for which figures are available, 45 officers were prosecuted by the federal government for alleged violations of civil rights. William Freivogel reported that a tiny handful of allegations of police brutality are prosecuted and the burden of proof is extremely high.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/killing-michael-brown-limits-federal-and-civil-power'>In 2013, the last year for which figures are available, 45 officers were prosecuted by the federal government for alleged violations of civil rights. William Freivogel reported that a tiny handful of allegations of police brutality are prosecuted and the burden of proof is extremely high.</a></p>"
+		},
+		{
+			"q": "Ferguson Mayor James Knowles III coaches what sport at McCluer South-Berkeley High School?",
+			"a": [
+				{"option": "Football",	"correct": false},
+				{"option": "Wrestling",	"correct": true},
+				{"option": "Tennis",	"correct": false},
+				{"option": "Soccer",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/amid-anger-over-browns-death-structurally-limited-mayor-vows-make-changes'>Knowles coaches wrestling at the high school. Jason Rosenbaum reported that when he was first elected to his post in 2011, Knowles became one of the youngest mayors in the state, and he is also one of the few Republicans who managed to win office in the heavily Democratic area.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/amid-anger-over-browns-death-structurally-limited-mayor-vows-make-changes'>Knowles coaches wrestling at the high school. Jason Rosenbaum reported that when he was first elected to his post in 2011, Knowles became one of the youngest mayors in the state, and he is also one of the few Republicans who managed to win office in the heavily Democratic area.</a></p>"
+		},
+		{
+			"q": "Former Ferguson-Florissant Superintendent Art McCoy has a new job as chief academic officer with what organization?",
+			"a": [
+				{"option": "StudentsFirst",	"correct": false},
+				{"option": "Council of the Great City Schools",	"correct": false},
+				{"option": "Education Commissioner of the States",	"correct": false},
+				{"option": "MIND Research Institute",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/former-ferguson-florissant-superintendent-gets-new-job-retains-local-involvement'>McCoy will be chief academic officer and superintendent in residence at the MIND Research Institute, based in California. Dale Singer reported that McCoy will remain in the St. Louis area and was active in helping students when Ferguson-Florissant delayed the opening of its school year because of unrest in Ferguson.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/former-ferguson-florissant-superintendent-gets-new-job-retains-local-involvement'>McCoy will be chief academic officer and superintendent in residence at the MIND Research Institute, based in California. Dale Singer reported that McCoy will remain in the St. Louis area and was active in helping students when Ferguson-Florissant delayed the opening of its school year because of unrest in Ferguson.</a></p>"
+		},
+		{
+			"q": "How did Noranda Aluminum of southeast Missouri respond to the Missouri Public Service Commission’s refusal to cut it electric bill?",
+			"a": [
+				{"option": "Filed suit",	"correct": false},
+				{"option": "Cut off political contributions",	"correct": false},
+				{"option": "Laid off employees",	"correct": true},
+				{"option": "All of the above",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/noranda-plans-layoffs-drops-expansion-after-psc-refuses-cut-electric-bill'>After the PSC refused to cut its electric bill, Noranda said it would lay off 200 employees. Jo Mannies reported that the company, which is the state’s largest user of electricity, also is suspending its planned $30 million expansion project.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/noranda-plans-layoffs-drops-expansion-after-psc-refuses-cut-electric-bill'>After the PSC refused to cut its electric bill, Noranda said it would lay off 200 employees. Jo Mannies reported that the company, which is the state’s largest user of electricity, also is suspending its planned $30 million expansion project.</a></p>"
+		},
+		{
+			"q": "The former CEO of Mamtek, Bruce Cole, pleaded guilty to charges arising from the company’s failure to build a plant in what Missouri city?",
+			"a": [
+				{"option": "Fulton",	"correct": false},
+				{"option": "Mexico",	"correct": false},
+				{"option": "Moberly",	"correct": true},
+				{"option": "Joplin",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/former-mamtek-ceo-bruce-cole-pleads-guilty-security-fraud-and-theft'>In 2010, Mamtek CEO Bruce Cole persuaded city leaders in Moberly to issue $39 million in bonds to build the plant, which was to employ 600 people. Marshall Griffin reported that the artificial sweetener plant never opened and Moberly's bond rating was downgraded as a result. Cole pleaded guilty to security fraud and theft.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/former-mamtek-ceo-bruce-cole-pleads-guilty-security-fraud-and-theft'>In 2010, Mamtek CEO Bruce Cole persuaded city leaders in Moberly to issue $39 million in bonds to build the plant, which was to employ 600 people. Marshall Griffin reported that the artificial sweetener plant never opened and Moberly's bond rating was downgraded as a result. Cole pleaded guilty to security fraud and theft.</a></p>"
+		},
+		{
+			"q": "Fifteen years ago, 70.8 percent of residences in the St. Louis area had earthquake insurance. What was that percentage last year?",
+			"a": [
+				{"option": "81.2 percent",	"correct": false},
+				{"option": "60.3 percent",	"correct": false},
+				{"option": "41.7 percent",	"correct": false},
+				{"option": "29.6 percent",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/after-napa-missourians-weigh-costs-earthquake-insurance'>The share of residences in the area covered by earthquake insurance fell to 29.6 percent in 2013. Durrie Bouscaren reported that a decline statewide is due in part to rising annual costs of coverage, particularly in areas like southeast Missouri, along the New Madrid Fault.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/after-napa-missourians-weigh-costs-earthquake-insurance'>The share of residences in the area covered by earthquake insurance fell to 29.6 percent in 2013. Durrie Bouscaren reported that a decline statewide is due in part to rising annual costs of coverage, particularly in areas like southeast Missouri, along the New Madrid Fault.</a></p>"
+		},
+		{
+			"q": "The Rise of the Rest Road Tour is coming to St. Louis this fall. What does it celebrate?",
+			"a": [
+				{"option": "Cycling",	"correct": false},
+				{"option": "Education",	"correct": false},
+				{"option": "Voter registration",	"correct": false},
+				{"option": "Entrepreneurship",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tour-celebrate-entrepreneurship-coming-st-louis'>The bus tour, spearheaded by Steve Case, former CEO of AOL, celebrates entrepreneurship. Maria Altman reported that it will feature a startup pitch competition in each city with the winners getting a $100,000 investment from Case. </a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tour-celebrate-entrepreneurship-coming-st-louis'>The bus tour, spearheaded by Steve Case, former CEO of AOL, celebrates entrepreneurship. Maria Altman reported that it will feature a startup pitch competition in each city with the winners getting a $100,000 investment from Case. </a></p>"
+		},
+		{
+			"q": "Piper Kerman, whose memoir “Orange is the New Black” inspired the Netflix series, was imprisoned for a year for what crime?",
+			"a": [
+				{"option": "Forgery",	"correct": false},
+				{"option": "Drug trafficking",	"correct": true},
+				{"option": "Computer hacking",	"correct": false},
+				{"option": "Prostitution",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/orange-new-black-author-hints-new-season-talks-reform-way-stl'>Kerman, who is coming to St. Louis for two appearances, spent a year in prison for drug trafficking. She told Nancy Fowler that too many non-violent offenders are in prison, and when they are released, they get no help returning to society.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/orange-new-black-author-hints-new-season-talks-reform-way-stl'>Kerman, who is coming to St. Louis for two appearances, spent a year in prison for drug trafficking. She told Nancy Fowler that too many non-violent offenders are in prison, and when they are released, they get no help returning to society.</a></p>"
+		}
+	]
+};

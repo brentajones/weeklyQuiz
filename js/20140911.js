dbd826ef9309b6049e0ccab299273851acc1c3e0
brentajones <bjones@bjones.net>
2014-09-11 17:40:26 -0500

    update for week

---
+++ js/20140911.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+	"info": {
+		"name":	"St. Louis Public Radio News Quiz for Sep. 12, 2014",
+		"main":	"<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+		"results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+		"level1":  "Nice work.",
+		"level2":  "Almost there.",
+		"level3":  "Try again next week." // no comma here
+	},
+	"questions": [
+		{
+			"q": "Protesters attempted to shut down Interstate 70 at what north St. Louis County exit on Wednesday?",
+			"a": [
+				{"option": "Natural Bridge",	"correct": false},
+				{"option": "I-170",	"correct": false},
+				{"option": "Hanley",	"correct": true},
+				{"option": "Florissant",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/protesters-attempt-highway-shutdown-north-st-louis-county'>The protesters wanted to shut down I-70 at Hanley, to emphasize their call for a special prosecutor in the death of Michael Brown. Camille Phillips reported that dozens were arrested, but a strong police presence prevented demonstrators from reaching their goal.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/protesters-attempt-highway-shutdown-north-st-louis-county'>The protesters wanted to shut down I-70 at Hanley, to emphasize their call for a special prosecutor in the death of Michael Brown. Camille Phillips reported that dozens were arrested, but a strong police presence prevented demonstrators from reaching their goal.</a></p>"
+		},
+		{
+			"q": "What changes were recommended to the Ferguson City Council in the wake of the Michael Brown shooting?",
+			"a": [
+				{"option": "A civilian review board for police activity",	"correct": false},
+				{"option": "A limit on court fine revenue",	"correct": false},
+				{"option": "Repealing the offense of failure to appear in municipal court",	"correct": false},
+				{"option": "All of the above",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/amid-national-scrutiny-ferguson-council-poised-change-courts-police-review'>In the wake of the shooting of Michael Brown and the unrest that followed, the city council heard recommendations to create a civilian review board, a limit on revenue from court fines, a repeal of the office of failure to appear in municipal court and more. Jason Rosenbaum reported that Brown’s death last month prompted intense examination of how the town polices itself and prosecutes people who break city laws.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/amid-national-scrutiny-ferguson-council-poised-change-courts-police-review'>In the wake of the shooting of Michael Brown and the unrest that followed, the city council heard recommendations to create a civilian review board, a limit on revenue from court fines, a repeal of the office of failure to appear in municipal court and more. Jason Rosenbaum reported that Brown’s death last month prompted intense examination of how the town polices itself and prosecutes people who break city laws.</a></p>"
+		},
+		{
+			"q": "What court heard arguments in two death penalty cases in Missouri?",
+			"a": [
+				{"option": "St. Louis County Circuit Court",	"correct": false},
+				{"option": "Missouri Court of Appeals",	"correct": false},
+				{"option": "U.S. District Court for Eastern Missouri",	"correct": false},
+				{"option": "8th U.S. Circuit Court of Appeals",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/appeals-court-hears-two-challenges-brought-death-row-inmates'>The federal appeals court heard the arguments in two Missouri death penalty cases. Chris McDaniel reported that plaintiffs claim that the state's lethal injection method amounts to cruel and unusual punishment.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/appeals-court-hears-two-challenges-brought-death-row-inmates'>The federal appeals court heard the arguments in two Missouri death penalty cases. Chris McDaniel reported that plaintiffs claim that the state's lethal injection method amounts to cruel and unusual punishment.</a></p>"
+		},
+		{
+			"q": "A recent state audit was critical of what practice by Gov. Jay Nixon?",
+			"a": [
+				{"option": "Use of state-owned airplane",	"correct": false},
+				{"option": "Public appearances for political causes",	"correct": false},
+				{"option": "Withholding of money from the Missouri budget",	"correct": true},
+				{"option": "Acceptance of gifts from lobbyists",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-releases-audit-critical-nixons-withholding-money-budget'>The report issued by Missouri Auditor Tom Schweich said Nixon violated the state constitution when he withheld money from two recent budgets. Marshall Griffin reported that the governor’s office disputed the conclusion in the report, which came out two days before lawmakers returned to Jefferson City for their veto session.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-releases-audit-critical-nixons-withholding-money-budget'>The report issued by Missouri Auditor Tom Schweich said Nixon violated the state constitution when he withheld money from two recent budgets. Marshall Griffin reported that the governor’s office disputed the conclusion in the report, which came out two days before lawmakers returned to Jefferson City for their veto session.</a></p>"
+		},
+		{
+			"q": "What is the name of the federal program that will help fund a new MetroLink station in the Cortex district?",
+			"a": [
+				{"option": "LION",	"correct": false},
+				{"option": "PUMA",	"correct": false},
+				{"option": "PANTHER",	"correct": false},
+				{"option": "TIGER",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/federal-tiger-grant-funds-new-metrolink-station'>The Transportation Investment Generating Economic Recovery, or TIGER, program will provide $10.3 million for the new station. Joseph Leahy reported that the federal grant covers nearly all of the project’s nearly $13 million cost.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/federal-tiger-grant-funds-new-metrolink-station'>The Transportation Investment Generating Economic Recovery, or TIGER, program will provide $10.3 million for the new station. Joseph Leahy reported that the federal grant covers nearly all of the project’s nearly $13 million cost.</a></p>"
+		},
+		{
+			"q": "State Rep. Paul Wieland, who objects to the state of Missouri providing contraception coverage as part of his health insurance, is from what town in Jefferson County?",
+			"a": [
+				{"option": "De Soto",	"correct": false},
+				{"option": "Herculaneum",	"correct": false},
+				{"option": "Festus",	"correct": false},
+				{"option": "Imperial",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/legislator-tells-federal-appeals-court-why-he-objects-birth-control-coverage'>Wieland, R-Imperial, who is Roman Catholic, says he doesn’t want the health insurance he gets as a legislator to coverage contraception for his three daughters. Jo Mannies reported that in an argument before a federal appeals court, his lawyer said that if businesses can drop contraceptive coverage on religious grounds, employees should be able to reject the coverage as well.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/legislator-tells-federal-appeals-court-why-he-objects-birth-control-coverage'>Wieland, R-Imperial, who is Roman Catholic, says he doesn’t want the health insurance he gets as a legislator to coverage contraception for his three daughters. Jo Mannies reported that in an argument before a federal appeals court, his lawyer said that if businesses can drop contraceptive coverage on religious grounds, employees should be able to reject the coverage as well.</a></p>"
+		},
+		{
+			"q": "What group got special attention when Southwest Airlines inaugurated new nonstop service from St. Louis to San Francisco on Tuesday?",
+			"a": [
+				{"option": "First responders",	"correct": false},
+				{"option": "Residents of Ferguson",	"correct": false},
+				{"option": "Entrepreneurs",	"correct": true},
+				{"option": "Teachers",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-entrepreneurs-take-new-san-francisco-flight'>Entrepreneurs were among the passengers on the inaugural flight to San Francisco, with the CEOs of some new companies aboard to give pitches for financial support. Maria Altman reported that the goal was to help St. Louis startups tap into the Bay Area’s booming startup and investment scene.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/st-louis-entrepreneurs-take-new-san-francisco-flight'>Entrepreneurs were among the passengers on the inaugural flight to San Francisco, with the CEOs of some new companies aboard to give pitches for financial support. Maria Altman reported that the goal was to help St. Louis startups tap into the Bay Area’s booming startup and investment scene.</a></p>"
+		},
+		{
+			"q": "Marsy's Law for Illinois, a state constitutional amendment on the Illinois ballot in November, is modeled after a law in what state?",
+			"a": [
+				{"option": "Texas",	"correct": false},
+				{"option": "New York",	"correct": false},
+				{"option": "Minnesota",	"correct": false},
+				{"option": "California",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/illinois-voters-consider-constitutional-amendment-crime-victims-rights'>Marsy’s Law for Illinois, which would amend the state constitution over rights for crime victims, is modeled after a similar effort in California. Stephanie Lecci reported that it would create language that would enforce crime victims' rights and give victims legal recourse to make the court aware of violations.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/illinois-voters-consider-constitutional-amendment-crime-victims-rights'>Marsy’s Law for Illinois, which would amend the state constitution over rights for crime victims, is modeled after a similar effort in California. Stephanie Lecci reported that it would create language that would enforce crime victims' rights and give victims legal recourse to make the court aware of violations.</a></p>"
+		},
+		{
+			"q": "A chess grandmaster from what country won the Sinquefield Cup?",
+			"a": [
+				{"option": "Russia",	"correct": false},
+				{"option": "Italy",	"correct": true},
+				{"option": "United States",	"correct": false},
+				{"option": "Norway",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/italian-fabiano-caruana-wins-sinquefield-cup'>Italian Grandmaster Fabiano Caruana won the cup in the competition in the Central West End. Willis Ryder Arnold reported that Caruana established himself quickly in the tournament, working methodically through a seven-game winning streak before an 8th round draw with the Norwegian reigning world chess champion, Magnus Carlsen. </a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/italian-fabiano-caruana-wins-sinquefield-cup'>Italian Grandmaster Fabiano Caruana won the cup in the competition in the Central West End. Willis Ryder Arnold reported that Caruana established himself quickly in the tournament, working methodically through a seven-game winning streak before an 8th round draw with the Norwegian reigning world chess champion, Magnus Carlsen. </a></p>"
+		},
+		{
+			"q": "Where was the inaugural alternative music event Pü Fest held last weekend?",
+			"a": [
+				{"option": "Cherokee Street",	"correct": true},
+				{"option": "Broadway",	"correct": false},
+				{"option": "Tower Grove Park",	"correct": false},
+				{"option": "Washington Avenue",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tale-two-festivals-loufests-record-attendance-and-birth-p-fest'>Pü Fest, staged at the same time LouFest was going on in Forest Park, was held in the Empire Hall basement at the intersection of California Avenue and Cherokee Street. Brian Villa reported that its founders were less interested in national tours and more focused on bands they say play shows in St. Louis every night of the week. </a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/tale-two-festivals-loufests-record-attendance-and-birth-p-fest'>Pü Fest, staged at the same time LouFest was going on in Forest Park, was held in the Empire Hall basement at the intersection of California Avenue and Cherokee Street. Brian Villa reported that its founders were less interested in national tours and more focused on bands they say play shows in St. Louis every night of the week. </a></p>"
+		}
+	]
+};

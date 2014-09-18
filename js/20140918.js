52ecb2244a98dd10d5db905488496fc3007f58ba
brentajones <bjones@bjones.net>
2014-09-18 14:13:17 -0500

    update for week

---
+++ js/20140918.js
@@ -0,0 +1,122 @@
+var quizJSON = {
+	"info": {
+		"name":	"St. Louis Public Radio News Quiz for Sep. 19, 2014",
+		"main":	"<p>How well were you paying attention to the news this week? Take our quiz and find out.</p>",
+		"results": "<h5>Thanks for playing. Keep listening and reading, then come back and take next week's quiz.</h5>",
+		"level1":  "Nice work.",
+		"level2":  "Almost there.",
+		"level3":  "Try again next week." // no comma here
+	},
+	"questions": [
+		{
+			"q": "If no indictments are returned in the fatal shooting of Michael Brown, what will become of the grand jury transcripts?",
+			"a": [
+				{"option": "They will be destroyed",	"correct": false},
+				{"option": "They will be sealed for 20 years",	"correct": false},
+				{"option": "They will be released to the public",	"correct": true},
+				{"option": "No one knows",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/if-officer-wilson-not-indicted-mcculloch-vows-release-grand-jury-transcripts-recordings'>St. Louis County Prosecutor Bob McCulloch says if the grand jury does not indict Officer Darren Wilson, transcripts and audio recordings of its proceedings will be made public. William Freivogel reported that grand jury proceedings often are not transcribed, but in this case, McCulloch had made a decision to transcribe and tape the proceedings.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/if-officer-wilson-not-indicted-mcculloch-vows-release-grand-jury-transcripts-recordings'>St. Louis County Prosecutor Bob McCulloch says if the grand jury does not indict Officer Darren Wilson, transcripts and audio recordings of its proceedings will be made public. William Freivogel reported that grand jury proceedings often are not transcribed, but in this case, McCulloch had made a decision to transcribe and tape the proceedings.</a></p>"
+		},
+		{
+			"q": "Before she became commissioner of education for Missouri, Chris Nicastro was superintendent in what two north St. Louis County school districts?",
+			"a": [
+				{"option": "Jennings and Hazelwood",	"correct": false},
+				{"option": "Hazelwood and Riverview Gardens",	"correct": true},
+				{"option": "Riverview Gardens and Ferguson-Florissant",	"correct": false},
+				{"option": "Ferguson-Florissant and Jennings",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nicastro-retire-head-missouri-schools'>Nicastro was superintendent in Hazelwood and Riverview Gardens before becoming Missouri’s commissioner of education in 2009. Dale Singer reported that her experience in those districts gave her tenure an emphasis on raising student achievement in urban schools.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/nicastro-retire-head-missouri-schools'>Nicastro was superintendent in Hazelwood and Riverview Gardens before becoming Missouri’s commissioner of education in 2009. Dale Singer reported that her experience in those districts gave her tenure an emphasis on raising student achievement in urban schools.</a></p>"
+		},
+		{
+			"q": "A new policy will allow what students who live in the Normandy school district to transfer to an accredited school?",
+			"a": [
+				{"option": "Any students at all",	"correct": false},
+				{"option": "Any students who transferred last year",	"correct": false},
+				{"option": "Any students who signed up to transfer by a February deadline",	"correct": true},
+				{"option": "Any students whose siblings transferred last year",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/restrictions-eased-normandy-school-transfers'>Under the latest policy change, any Normandy students who signed up to transfer by a February deadline are eligible to attend a nearby accredited district. Dale Singer reported that the change, which widens the pool of potential transfers, raises concerns about Normandy’s ability to survive financially.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/restrictions-eased-normandy-school-transfers'>Under the latest policy change, any Normandy students who signed up to transfer by a February deadline are eligible to attend a nearby accredited district. Dale Singer reported that the change, which widens the pool of potential transfers, raises concerns about Normandy’s ability to survive financially.</a></p>"
+		},
+		{
+			"q": "An organization started by St. Louis Alderman Antonio French, with a focus on voting and empowerment, is called what?",
+			"a": [
+				{"option": "Hands UP",	"correct": false},
+				{"option": "Better Ferguson",	"correct": false},
+				{"option": "Get Involved",	"correct": false},
+				{"option": "HealSTL",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/events-ferguson-spur-political-action-search-new-voters'>French, who has been active in Ferguson since the shooting death of Michael Brown, started the organization called HealSTL. Emanuele Berry reported that it is one of many efforts focused on helping people become more involved in improving their community.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/events-ferguson-spur-political-action-search-new-voters'>French, who has been active in Ferguson since the shooting death of Michael Brown, started the organization called HealSTL. Emanuele Berry reported that it is one of many efforts focused on helping people become more involved in improving their community.</a></p>"
+		},
+		{
+			"q": "A recount showed that the right to farm amendment in Missouri passed last month by how many votes?",
+			"a": [
+				{"option": "1,486",	"correct": false},
+				{"option": "2,375",	"correct": true},
+				{"option": "3,982",	"correct": false},
+				{"option": "4,873",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/recount-upholds-vote-approving-right-farm-amendment'>The recount showed the proposal passed by 2,375 votes out of nearly 1 million votes cast. Marshall Griffin reported that the difference between \"yes\" and \"no\" votes before the recount was 2,490.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/recount-upholds-vote-approving-right-farm-amendment'>The recount showed the proposal passed by 2,375 votes out of nearly 1 million votes cast. Marshall Griffin reported that the difference between \"yes\" and \"no\" votes before the recount was 2,490.</a></p>"
+		},
+		{
+			"q": "When was the last time teachers in Highland, Ill., went on strike?",
+			"a": [
+				{"option": "1999",	"correct": false},
+				{"option": "2003",	"correct": false},
+				{"option": "2009",	"correct": false},
+				{"option": "They’ve never gone on strike before",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/update-day-five-highland-teachers-strike'>Highland teachers walked off the job last week, going on strike for the first time ever. Camille Phillips reported that the teachers’ union wants their new contract to mirror their old one.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/update-day-five-highland-teachers-strike'>Highland teachers walked off the job last week, going on strike for the first time ever. Camille Phillips reported that the teachers’ union wants their new contract to mirror their old one.</a></p>"
+		},
+		{
+			"q": "Addition of a third shift at the General Motors plant at Wentzville is based in part on advance orders for a new Chevrolet pickup called what?",
+			"a": [
+				{"option": "Dakota",	"correct": false},
+				{"option": "Nevada",	"correct": false},
+				{"option": "Montana",	"correct": false},
+				{"option": "Colorado",	"correct": true}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/general-motors-plans-add-750-jobs-wentzille-plant'>GM said the addition of 750 workers on a third shift starting early next year is based in part on nearly 30,000 early orders from dealers for the Chevrolet Colorado. Stephanie Lecci and Rebecca Smith reported that in recent years, GM has ramped up its truck program, investing millions of dollars at the Wentzville plant.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/general-motors-plans-add-750-jobs-wentzille-plant'>GM said the addition of 750 workers on a third shift starting early next year is based in part on nearly 30,000 early orders from dealers for the Chevrolet Colorado. Stephanie Lecci and Rebecca Smith reported that in recent years, GM has ramped up its truck program, investing millions of dollars at the Wentzville plant.</a></p>"
+		},
+		{
+			"q": "What does Missouri’s “Macks Creek law” govern?",
+			"a": [
+				{"option": "Requiring homeowners to get flood insurance",	"correct": false},
+				{"option": "Making sure certain records are made public",	"correct": false},
+				{"option": "Limiting a town’s income from court fines and fees",	"correct": true},
+				{"option": "Defining what businesses may ban concealed weapons",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-will-check-whether-municipal-courts-are-collecting-too-much-fines'>The “Macks Creek law” limits a town’s income from court fines and fees. Jo Mannies reported that Missouri Auditor Tom Schweich plans to target municipal courts that he believes may be violating state law.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/schweich-will-check-whether-municipal-courts-are-collecting-too-much-fines'>The “Macks Creek law” limits a town’s income from court fines and fees. Jo Mannies reported that Missouri Auditor Tom Schweich plans to target municipal courts that he believes may be violating state law.</a></p>"
+		},
+		{
+			"q": "Boeing won a $4.2 billion contract to build what?",
+			"a": [
+				{"option": "Spacecraft to carry American astronauts",	"correct": true},
+				{"option": "A new generation of armed drones",	"correct": false},
+				{"option": "Enhanced satellite technology",	"correct": false},
+				{"option": "Tanker and transport planes",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/boeing-wins-nasa-contract-spacecraft'>The contract from NASA is to build new spacecraft to carry American astronauts to the International Space Station. Maria Altman reported that the agency has a goal of returning the launch of astronauts from U.S. soil by 2017.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/boeing-wins-nasa-contract-spacecraft'>The contract from NASA is to build new spacecraft to carry American astronauts to the International Space Station. Maria Altman reported that the agency has a goal of returning the launch of astronauts from U.S. soil by 2017.</a></p>"
+		},
+		{
+			"q": "New research at Washington University suggests that schizophrenia is actually a group of how many distinct disorders?",
+			"a": [
+				{"option": "Six",	"correct": false},
+				{"option": "Seven",	"correct": false},
+				{"option": "Eight",	"correct": true},
+				{"option": "Nine",	"correct": false}
+			],
+			"correct": "<p><span>That's right!</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wash-u-study-genetics-shows-schizophrenia-really-multiple-disorders'>The Washington U. study suggests that schizophrenia is actually a group of eight distinct disorders, each with a different genetic basis. Veronique LaCapra reported that previous research had found only weak or inconsistent links between schizophrenia and individual genes.</a></p>",
+			"incorrect": "<p><span>Incorrect.</span></p> <p><a target='_blank' href='http://news.stlpublicradio.org/post/wash-u-study-genetics-shows-schizophrenia-really-multiple-disorders'>The Washington U. study suggests that schizophrenia is actually a group of eight distinct disorders, each with a different genetic basis. Veronique LaCapra reported that previous research had found only weak or inconsistent links between schizophrenia and individual genes.</a></p>"
+		}
+	]
+};
